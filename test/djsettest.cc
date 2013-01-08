d9e86f8862607388581c2ae327f509ad1879eadc
Sander van Dijk <sgvandijk@gmail.com>
2013-01-09 00:57:26 +0100

    - New DisjointSet

--- test/djsettest.cc
+++ test/djsettest.cc
@@ -1,26 +1,54 @@
 #include "../DisjointSet/disjointset.hh"
 #include <iostream>
+#include <cmath>
 
 using namespace bats;
 using namespace std;
 
-#define N 100000
+#define N 1000000
+
+bool flipCoin()
+{
+  return (rand() % 20) == 0;
+}
 
 int main()
 {
   DisjointSet<int> set;
 
-  auto unionPred = [](int* a, int* b) { return (*a % 3) == (*b % 3); };
   vector<int> ints(N);
   iota(ints.begin(), ints.end(), 1);
   random_shuffle(ints.begin(), ints.end());
 
-  DisjointSet<int>::Element* elems[N];
+  int someSeenEven = -1;
+  int someSeenOdd = -1;
+  for (unsigned i = 0; i < N; ++i)
+  {
+    set.insert(ints[i]);
+    
+    if (ints[i] % 2 == 0)
+    {
+      if (someSeenEven != -1)
+	set.merge(ints[i], someSeenEven);
+      if (someSeenEven == -1 || flipCoin())
+	someSeenEven = ints[i];
+    }
 
-  for (unsigned i = 0; i < N; ++i) {
-    elems[i] = set.insert(&ints[i], unionPred);
+    if (ints[i] % 2 != 0)
+    {
+      if (someSeenOdd != -1)
+	set.merge(ints[i], someSeenOdd);
+      if (someSeenOdd == -1 || flipCoin())
+	someSeenOdd = ints[i];
+    }
   }
 
+  auto subSets = set.getSubSets();
+  cout << "Number of subsets: " << subSets.size() << endl;
+  for (auto subSet : subSets)
+    cout << "Size: " << subSet.size() << endl;
+
+/*
   
   auto elements = set.getElements();
   cout << "Number of elements: " << elements.size() << endl;
@@ -28,4 +56,5 @@ int main()
   auto subsets = set.getSubSets();
 
   cout << "Number of subsets: " << subsets.size() << endl;
+*/
 }
