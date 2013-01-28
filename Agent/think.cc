861f70677814703cd0ed42ff2266892946053fa4
Sander van Dijk <sgvandijk@gmail.com>
2013-01-28 11:54:59 +0000

    Gain ratio for looking at ball

--- Agent/think.cc
+++ Agent/think.cc
@@ -51,8 +51,9 @@ void Agent::think()
   }
   if (foundBall)
   {
+    float r = 0.9;
     Vector2f centerPx = Vector2f(raw.cols/2, raw.rows/2);
-    Vector2f offset = foundBallAtPx - centerPx;
+    Vector2f offset = (foundBallAtPx - centerPx) * r;
     offset.x() *= (Camera::VIEW_H_ANGLE / (double)raw.cols); // pixel per angle
     offset.y() *= (Camera::VIEW_V_ANGLE / (double)raw.rows); // pixel per angle
 //    std::cout << "Found ball at " << foundBallAtPx.x() << "," << foundBallAtPx.y() << " - "
