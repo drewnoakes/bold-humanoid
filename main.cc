ec39fddc6a7c8eafffa548fc3b9ca397cfb97fc2
Drew Noakes <git@drewnoakes.com>
2013-01-28 00:01:29 +0000

    Fixed bug where auto-get-up was not disableableableable.

--- main.cc
+++ main.cc
@@ -29,7 +29,7 @@ int main(int argc, char **argv)
     } else if (arg == "-j" || arg == "--joystick") {
       useJoystick = true;
     } else if (arg == "-g" || arg == "--no-get-up") {
-      autoGetUpFromFallen = true;
+      autoGetUpFromFallen = false;
     }
   }
 
