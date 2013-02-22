fa4fc46e339b88ae2037c6d1458e69690778f63b
Sander van Dijk <sgvandijk@mail.com>
2013-02-22 16:57:31 +0000

    Camera image streaming client JS

--- www/scripts/app/Camera.js
+++ www/scripts/app/Camera.js
@@ -4,8 +4,22 @@ define(
     ],
     function(WebSocketFactory)
     {
-        var protocol = 'camera-protocol';
+	var protocol = 'camera-protocol';
 
         var socket = WebSocketFactory.open(protocol);
+
+	var nImg = 0;
+        socket.onmessage = function (msg)
+        {
+	    var imgBlob = new Blob([msg.data], {type: "image/jpgeg"});
+	    var objectURL = window.URL.createObjectURL(imgBlob);
+	    $('#camera-container').append($('<img>').attr('src',objectURL).css({position: 'absolute', left: '0px', top: '0px'}));
+
+	    var images = $('#camera-container img');
+	    if (images.length > 5)
+		images.first().remove();
+
+        }
+
     }
 );
