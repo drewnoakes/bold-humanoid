cea9ae5ccacba7779fa54a2b083d1f149cd1c2fa
Drew Noakes <git@drewnoakes.com>
2013-02-22 23:30:39 +0000

    Fixed typo in MIME type. More efficient use of jQuery selector. Tabs-to-spaces.

--- www/scripts/app/Camera.js
+++ www/scripts/app/Camera.js
@@ -4,22 +4,19 @@ define(
     ],
     function(WebSocketFactory)
     {
-	var protocol = 'camera-protocol';
+        var protocol = 'camera-protocol',
+            socket = WebSocketFactory.open(protocol),
+		    container = $('#camera-container');
 
-        var socket = WebSocketFactory.open(protocol);
-
-	var nImg = 0;
-        socket.onmessage = function (msg)
+		socket.onmessage = function(msg)
         {
-	    var imgBlob = new Blob([msg.data], {type: "image/jpgeg"});
-	    var objectURL = window.URL.createObjectURL(imgBlob);
-	    $('#camera-container').append($('<img>').attr('src',objectURL).css({position: 'absolute', left: '0px', top: '0px'}));
+            var imgBlob = new Blob([msg.data], {type: "image/jpeg"});
+            var objectURL = window.URL.createObjectURL(imgBlob);
+            container.append($('<img>').attr('src', objectURL).css({position: 'absolute', left: '0px', top: '0px'}));
 
-	    var images = $('#camera-container img');
-	    if (images.length > 5)
-		images.first().remove();
-
+            var images = container.find('img');
+            if (images.length > 5)
+                images.first().remove();
         }
-
     }
 );
