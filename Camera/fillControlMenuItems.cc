df629d4cc8aa626158319462437c5a3712a74979
Sander van Dijk <sgvandijk@gmail.com>
2013-02-24 13:18:43 +0000

    Comment out fillControlMenuItems content for now

--- Camera/fillControlMenuItems.cc
+++ Camera/fillControlMenuItems.cc
@@ -2,14 +2,17 @@
 
 void Camera::fillControlMenuItems(Control& control)
 {
+  /*
   struct v4l2_querymenu querymenu;
   memset (&querymenu, 0, sizeof (querymenu));
-  querymenu.id = queryctrl.id;
+  querymenu.id = control.id;
 
-  for (querymenu.index = queryctrl.minimum;
-       querymenu.index <= queryctrl.maximum;
+  for (querymenu.index = control.minimum;
+       querymenu.index <= control.maximum;
        querymenu.index++) {
     if (0 == ioctl (fd, VIDIOC_QUERYMENU, &querymenu)) {
       printf ("  %s\n", querymenu.name);
     }
-  }}
+  }
+  */
+}
