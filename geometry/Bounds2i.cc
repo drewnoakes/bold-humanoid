7b83655cdb3a60669a703d0029f2187b233fa8cd
Drew Noakes <git@drewnoakes.com>
2013-03-05 17:49:17 +0000

    Code simplification.

--- geometry/Bounds2i.cc
+++ geometry/Bounds2i.cc
@@ -16,22 +16,23 @@
 using namespace bold;
 using namespace cv;
 using namespace Eigen;
+using namespace std;
 
 Bounds2i::Bounds2i(int minX, int minY, int maxX, int maxY)
-: d_min(Eigen::Vector2i(minX, minY)),
-  d_max(Eigen::Vector2i(maxX, maxY))
+: d_min(Vector2i(minX, minY)),
+  d_max(Vector2i(maxX, maxY))
 {
   assert(d_min.x() <= d_max.x() && d_min.y() <= d_max.y());
 }
 
-Bounds2i::Bounds2i(Eigen::Vector2i min, Eigen::Vector2i max)
+Bounds2i::Bounds2i(Vector2i min, Vector2i max)
 : d_min(min),
   d_max(max)
 {
   assert(d_min.x() <= d_max.x() && d_min.y() <= d_max.y());
 }
 
-bool Bounds2i::contains(Eigen::Vector2i const& v) const
+bool Bounds2i::contains(Vector2i const& v) const
 {
   return v.x() >= d_min.x()
       && v.x() <= d_max.x()
@@ -54,25 +55,24 @@ bool Bounds2i::isEmpty() const
   return width() != 0 && height() != 0;
 }
 
-std::vector<Eigen::Vector2i> Bounds2i::getCorners() const
+vector<Vector2i> Bounds2i::getCorners() const
 {
-  std::vector<Eigen::Vector2i> corners = {
-    d_min, Eigen::Vector2i(d_min.x(), d_max.y()),
-    d_max, Eigen::Vector2i(d_max.x(), d_min.y())
+  vector<Vector2i> corners = {
+    d_min, Vector2i(d_min.x(), d_max.y()),
+    d_max, Vector2i(d_max.x(), d_min.y())
   };
 
   return corners;
 }
 
-std::vector<LineSegment2i> Bounds2i::getEdges() const
+vector<LineSegment2i> Bounds2i::getEdges() const
 {
   auto corners = getCorners();
-  std::vector<LineSegment2i> edges;
-  for (unsigned i = 0, lastIndex = 3; i < 4; i++)
+  vector<LineSegment2i> edges;
+  for (unsigned i = 0, lastIndex = 3; i < 4; lastIndex = i++)
   {
     if (corners[lastIndex] != corners[i])
       edges.push_back(LineSegment2i(corners[lastIndex], corners[i]));
-    lastIndex = i;
   }
 
   return edges;
