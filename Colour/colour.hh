65bd7669db1305da31fcb818b08cbf24aba62ce2
Drew Noakes <git@drewnoakes.com>
2013-03-10 01:30:52 +0000

    Moved hsvRange code from .hh to .cc. Made arg const&.

--- Colour/colour.hh
+++ Colour/colour.hh
@@ -75,50 +75,20 @@ namespace bold
       int v;
       int vRange;
 
-      hsvRange()
-      {};
+      hsvRange();
 
       hsvRange(
         int hue,        int hueRange,
         int saturation, int saturationRange,
-        int value,      int valueRange)
-      : h(hue),         hRange(hueRange),
-        s(saturation),  sRange(saturationRange),
-        v(value),       vRange(valueRange)
-      {}
+        int value,      int valueRange);
 
       /** Obtain the center colour for the range, in BGR format. */
-      Colour::bgr toBgr() const
-      {
-        return Colour::hsv2bgr(Colour::hsv(h, s, v));
-      }
+      Colour::bgr toBgr() const;
 
-      inline bool contains(hsv hsv) const
-      {
-        int hDiff = abs(hsv.h - h);
-        hDiff = std::min(hDiff, 192 - hDiff);
+      bool contains(hsv const& hsv) const;
 
-        return (hDiff <= hRange &&
-                hsv.s >= s - sRange && hsv.s <= s + sRange &&
-                hsv.v >= v - vRange && hsv.v <= v + vRange);
-      }
-
-      static hsvRange fromBytes(uchar h, uchar hRange, uchar s, uchar sRange, uchar v, uchar vRange)
-      {
-        return hsvRange(h, hRange, s, sRange, v, vRange);
-      }
-
-      static hsvRange fromDoubles(double h, double hRange, double s, double sRange, double v, double vRange)
-      {
-        assert(h >= 0.0 && h <= 360.0);
-        assert(hRange >= 0.0 && hRange <= 360.0);
-        assert(s >= 0.0 && sRange <= 1.0);
-        assert(sRange >= 0.0 && sRange <= 1.0);
-        assert(v >= 0.0 && vRange <= 1.0);
-        assert(vRange >= 0.0 && vRange <= 1.0);
-
-        return hsvRange((h/360.0)*255, (hRange/360)*255, s*255, sRange*255, v*255, vRange*255);
-      }
+      static hsvRange fromBytes(uchar h, uchar hRange, uchar s, uchar sRange, uchar v, uchar vRange);
+      static hsvRange fromDoubles(double h, double hRange, double s, double sRange, double v, double vRange);
     };
 
     static hsv bgr2hsv(bgr const& in);
