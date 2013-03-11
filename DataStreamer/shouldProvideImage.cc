8cd96698aae8ccbb37862347cb92f6722c7b1a4f
Drew Noakes <git@drewnoakes.com>
2013-03-11 10:43:55 +0000

    DataStreamer::shouldProvideImage returns false if no clients connected.

--- DataStreamer/shouldProvideImage.cc
+++ DataStreamer/shouldProvideImage.cc
@@ -5,5 +5,6 @@ bool DataStreamer::shouldProvideImage()
   // Current logic is just to provide an image every N cycles.
   // With experience we may find a better solution.
 
-  return AgentModel::getInstance().getCycleNumber() % d_streamFramePeriod == 0;
+  return d_cameraSessions.size() != 0
+      && AgentModel::getInstance().getCycleNumber() % d_streamFramePeriod == 0;
 }
