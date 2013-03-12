932102a710458bba9241910e950be7bc6655e546
Sander van Dijk <sgvandijk@mail.com>
2013-03-12 16:01:04 +0000

    internal header and constructor in separate file

---
+++ ImagePassHandler/BlobDetectPass/BlobDetectPass.cc
@@ -0,0 +1,22 @@
+#include "blobdetectpass.ih"
+
+BlobDetectPass::BlobDetectPass(int imageWidth, int imageHeight, std::vector<BlobType> const& blobTypes)
+  : d_blobTypes(blobTypes),
+    d_imageHeight(imageHeight),
+    d_imageWidth(imageWidth),
+    d_runsPerRowPerLabel(),
+    d_currentRun(0, 0)
+{
+  // Create a run length code for each label
+  for (BlobType const& blobType : blobTypes)
+  {
+    uchar pixelLabelId = blobType.pixelLabel.id();
+    
+    // A RunLengthCode is a vector of vectors of runs
+    d_runsPerRowPerLabel[pixelLabelId] = RunLengthCode();
+    
+    // Initialise a vector of Runs for each row in the image
+    for (unsigned y = 0; y < d_imageHeight; ++y)
+      d_runsPerRowPerLabel[pixelLabelId].push_back(std::vector<bold::Run>());
+  }
+}
