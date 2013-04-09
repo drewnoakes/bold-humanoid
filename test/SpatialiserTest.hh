1b28f7c74633b97bc0ffe858fa59de25c8f5b632
Sander van Dijk <sgvandijk@mail.com>
2013-04-09 11:18:42 +0100

    Aptialiser tests expect y is up and z is forward looking up is negative angle along x axis, down is positive

--- test/SpatialiserTest.hh
+++ test/SpatialiserTest.hh
@@ -30,12 +30,12 @@ TEST (SpatialiserTests, findGroundPointForPixelLookingStraightDown)
 {
   Spatialiser spatialiser = createTestSpatialiser();
   // Look straight down at the ground
-  Affine3d cameraTorsoTransform(AngleAxisd(-M_PI/2, Vector3d::UnitX()));
+  Affine3d cameraTorsoTransform(AngleAxisd(M_PI/2, Vector3d::UnitX()));
 
   Maybe<Vector3d> groundPoint = spatialiser.findGroundPointForPixel(Vector2i(5,5), /*torsoHeight*/0.4, cameraTorsoTransform);
 
   ASSERT_TRUE ( groundPoint.hasValue() );
-  EXPECT_TRUE ( VectorsEqual(Vector3d(0,0,-0.4), *groundPoint.value()) );
+  EXPECT_TRUE ( VectorsEqual(Vector3d(0,-0.4,0), *groundPoint.value()) );
 }
 
 TEST (SpatialiserTests, findGroundPointForPixelEmptyIfSkybound)
@@ -43,7 +43,7 @@ TEST (SpatialiserTests, findGroundPointForPixelEmptyIfSkybound)
   Spatialiser spatialiser = createTestSpatialiser();
 
   // Look straight up at the sky
-  Affine3d cameraTorsoTransform(AngleAxisd(M_PI/2, Vector3d::UnitX()));
+  Affine3d cameraTorsoTransform(AngleAxisd(-M_PI/2, Vector3d::UnitX()));
 
   Maybe<Vector3d> groundPoint = spatialiser.findGroundPointForPixel(Vector2i(5,5), /*torsoHeight*/0.4, cameraTorsoTransform);
 
