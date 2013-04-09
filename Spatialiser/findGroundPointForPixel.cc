25df9099a78ff8708f163d989b7e55fd359ec760
Sander van Dijk <sgvandijk@mail.com>
2013-04-09 17:12:04 +0100

    remove comment and indentation

--- Spatialiser/findGroundPointForPixel.cc
+++ Spatialiser/findGroundPointForPixel.cc
@@ -5,6 +5,7 @@ Maybe<Vector3d> Spatialiser::findGroundPointForPixel(Vector2i const& pixel,
                                                      Affine3d const& cameraTorsoTransform) const
 {
   Vector3d direction = cameraTorsoTransform.rotation() * d_cameraModel->directionForPixel(pixel);
-  Vector3d position = cameraTorsoTransform.translation();// * Vector3d::Zero();
+  Vector3d position = cameraTorsoTransform.translation();
+  
   return Math::intersectRayWithGroundPlane(position, direction, -torsoHeight);
 }
