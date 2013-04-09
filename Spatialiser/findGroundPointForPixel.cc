6d5f7ac4ee4663c1eb735ed18a8f4092ca5b00a9
Sander van Dijk <sgvandijk@mail.com>
2013-04-09 11:21:04 +0100

    only use rotation part of transform to rotate take out transformations to make z point upwards

--- Spatialiser/findGroundPointForPixel.cc
+++ Spatialiser/findGroundPointForPixel.cc
@@ -4,14 +4,7 @@ Maybe<Vector3d> Spatialiser::findGroundPointForPixel(Vector2i const& pixel,
                                                      double const torsoHeight,
                                                      Affine3d const& cameraTorsoTransform) const
 {
-  // The torso and world use different orientations of the axes, so we include that into the transform here
-  // TODO simplify this
-  Affine3d cameraAgentTransform = cameraTorsoTransform
-    * AngleAxisd(-M_PI/2, Vector3d::UnitX()) // rotate z-axis to point upwards
-    * AngleAxisd(M_PI, Vector3d::UnitZ());   // rotate y-axis to point forwards, with x-axis to right
-
-  Vector3d direction = cameraAgentTransform * d_cameraModel->directionForPixel(pixel);
-  Vector3d position = cameraAgentTransform * Vector3d::Zero();
-
+  Vector3d direction = cameraTorsoTransform.rotation() * d_cameraModel->directionForPixel(pixel);
+  Vector3d position = cameraTorsoTransform.translation();// * Vector3d::Zero();
   return Math::intersectRayWithGroundPlane(position, direction, -torsoHeight);
 }
