073110f01109badd56d5778c34cdb43807a7b8b0
Drew Noakes <git@drewnoakes.com>
2013-04-12 00:13:58 +0100

    Addressed compiler warnings (reorder initialiser.)

--- VisualCortex/VisualCortex.cc
+++ VisualCortex/VisualCortex.cc
@@ -1,6 +1,6 @@
 #include "visualcortex.ih"
 
 VisualCortex::VisualCortex(shared_ptr<CameraModel> cameraModel)
-  : d_minBallArea(8*8),
-    d_cameraModel(cameraModel)
+  : d_cameraModel(cameraModel),
+    d_minBallArea(8*8)
 {}
