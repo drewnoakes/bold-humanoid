d5f39a1e24f82c61776cfe91d69f7884ac384924
Sander van Dijk <sgvandijk@gmail.com>
2013-04-14 16:44:37 +0100

    Stand only controls body, and runs Ambulator::step

--- Option/Stand/runPolicy.cc
+++ Option/Stand/runPolicy.cc
@@ -2,10 +2,11 @@
 
 OptionList Stand::runPolicy()
 {
-  Robot::Action::GetInstance()->m_Joint.SetEnableBody(true, true);
+  Robot::Action::GetInstance()->m_Joint.SetEnableBodyWithoutHead(true, true);
 
   d_ambulator->setMoveDir(Eigen::Vector2d(0,0));
   d_ambulator->setTurnAngle(0);
-  
+  d_ambulator->step();
+
   return OptionList();
 }
