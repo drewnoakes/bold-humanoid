eb6c7824900c7396d481eb486f05e9e8b32027f2
Sander van Dijk <sgvandijk@gmail.com>
2013-04-15 20:18:56 +0100

    Made StateObject purely virtual; no need for protected ctor anymore

--- StateObject/stateobject.hh
+++ StateObject/stateobject.hh
@@ -3,21 +3,13 @@
 
 #include <rapidjson/writer.h>
 #include <rapidjson/stringbuffer.h>
-#include <string>
 
 namespace bold
 {
   class StateObject
   {
   public:
-    virtual void writeJson(rapidjson::Writer<rapidjson::StringBuffer>& writer) const {};
-
-  protected:
-    StateObject()
-    {};
-
-    ~StateObject()
-    {};
+    virtual void writeJson(rapidjson::Writer<rapidjson::StringBuffer>& writer) const = 0;
   };
 }
 
