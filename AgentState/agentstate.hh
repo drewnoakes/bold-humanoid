2d2ddff7a45833e2b9c2e9d36246c6b4ee6c1277
Drew Noakes <git@drewnoakes.com>
2013-04-15 13:14:49 +0100

    Fixed implementation of AgentState::allStateObjects. Removed unused fields and forward declarations.

--- AgentState/agentstate.hh
+++ AgentState/agentstate.hh
@@ -10,31 +10,17 @@
 
 namespace bold
 {
-  class AgentFrameState;
-  class AlarmState;
-  class BodyState;
-  class CameraFrameState;
-  class GameState;
-  class HardwareState;
-  class WorldFrameState;
-
   class AgentState
   {
   public:
     AgentState()
-    : d_cameraFrameNumber(0),
-      d_agentFrame(),
-      d_alarmState(),
-      d_bodyState(),
-      d_cameraFrame(),
-      d_gameState(),
-      d_hardwareState(),
-      d_worldFrameState()
+    : d_cameraFrameNumber(0)
     {}
 
     unsigned long long getCameraFrameNumber() const { return d_cameraFrameNumber; }
 
     std::vector<std::shared_ptr<StateObject>> allStateObjects() const;
+
     std::vector<StateType> allStateTypes() const
     {
       std::vector<StateType> types = {
@@ -61,32 +47,25 @@ namespace bold
     void set(std::shared_ptr<T> state)
     {
       // Todo: check whether this is atomic
-      d_states[&typeid(T)] = state;
+      d_stateByTypeId[&typeid(T)] = state;
     }
 
     template <typename T>
     std::shared_ptr<T const> get() const
     {
       // Todo: this definitely is not atomic
-      auto s = d_states.find(&typeid(T));
-      return s != d_states.end() ? std::static_pointer_cast<T const>(s->second) : 0;
+      auto s = d_stateByTypeId.find(&typeid(T));
+      return s != d_stateByTypeId.end() ? std::static_pointer_cast<T const>(s->second) : 0;
     }
 
     static AgentState& getInstance();
 
   private:
     unsigned long d_cameraFrameNumber;
-    std::shared_ptr<AgentFrameState> d_agentFrame;
-    std::shared_ptr<AlarmState> d_alarmState;
-    std::shared_ptr<BodyState> d_bodyState;
-    std::shared_ptr<CameraFrameState> d_cameraFrame;
-    std::shared_ptr<GameState> d_gameState;
-    std::shared_ptr<HardwareState> d_hardwareState;
-    std::shared_ptr<WorldFrameState> d_worldFrameState;
 
     struct TypeInfoCompare { bool operator()(std::type_info const* a, std::type_info const* b) const { return a->before(*b); }; };
 
-    std::map<std::type_info const*, std::shared_ptr<StateObject>,TypeInfoCompare> d_states;
+    std::map<std::type_info const*, std::shared_ptr<StateObject>, TypeInfoCompare> d_stateByTypeId;
   };
 
   inline AgentState& AgentState::getInstance()
--- AgentState/allStateObjects.cc
+++ AgentState/allStateObjects.cc
@@ -1,10 +1,3 @@
-#include "../StateObject/AgentFrameState/agentframestate.hh"
-#include "../StateObject/AlarmState/alarmstate.hh"
-#include "../StateObject/BodyState/bodystate.hh"
-#include "../StateObject/CameraFrameState/cameraframestate.hh"
-#include "../StateObject/GameState/gamestate.hh"
-#include "../StateObject/HardwareState/hardwarestate.hh"
-
 #include "agentstate.hh"
 
 using namespace bold;
@@ -12,12 +5,9 @@ using namespace std;
 
 vector<shared_ptr<StateObject>> AgentState::allStateObjects() const
 {
-  return {
-    dynamic_pointer_cast<StateObject>(d_cameraFrame),
-    dynamic_pointer_cast<StateObject>(d_agentFrame),
-    dynamic_pointer_cast<StateObject>(d_gameState),
-    dynamic_pointer_cast<StateObject>(d_hardwareState),
-    dynamic_pointer_cast<StateObject>(d_bodyState),
-    dynamic_pointer_cast<StateObject>(d_alarmState)
-  };
+  vector<shared_ptr<StateObject>> stateObjects;
+  for (auto const& pair : d_stateByTypeId) {
+    stateObjects.push_back(pair.second);
+  }
+  return stateObjects;
 }
