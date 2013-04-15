68fd0af1d8f4da4a620233cebe2f83b07a2ac211
Sander van Dijk <sgvandijk@gmail.com>
2013-04-15 20:17:59 +0100

    OptionTreeState added

---
+++ StateObject/OptionTreeState/optiontreestate.hh
@@ -0,0 +1,26 @@
+#ifndef BOLD_OPTIONTREESTATE_HH
+#define BOLD_OPTIONTREESTATE_HH
+
+#include "../stateobject.hh"
+#include "../../Option/option.hh"
+
+namespace bold
+{
+  class OptionTreeState : public StateObject
+  {
+  public:
+    OptionTreeState(OptionList const& ranOptions)
+    : d_ranOptions(ranOptions)
+    {}
+
+    OptionList getRanOptions() const { return d_ranOptions; };
+
+    void writeJson(rapidjson::Writer<rapidjson::StringBuffer>& writer) const override;
+  private:
+    OptionList d_ranOptions;
+    
+  };
+
+}
+
+#endif
---
+++ StateObject/OptionTreeState/writeJson.cc
@@ -0,0 +1,14 @@
+#include "optiontreestate.ih"
+
+void OptionTreeState::writeJson(rapidjson::Writer<rapidjson::StringBuffer>& writer) const
+{
+  writer.StartObject();
+  {
+    writer.String("ranoptions");
+    writer.StartArray();
+    for (auto option : d_ranOptions)
+      writer.String(option->getID().c_str());
+    writer.EndArray();
+  }
+  writer.EndObject();
+}
