4c1ad1aa2ed0744dce565679faa3d466e47d92ea
Sander van Dijk <sgvandijk@gmail.com>
2013-04-20 11:36:13 +0100

    Take out debug output

--- CameraModel/directionForPixel.cc
+++ CameraModel/directionForPixel.cc
@@ -6,7 +6,6 @@ Vector3d CameraModel::directionForPixel(Vector2i const& pixel) const
   p << (pixel -Vector2i(d_imageWidth / 2, d_imageHeight / 2)).cast<double>(), 1;
 
   auto ptInv = getProjectionTransform().inverse();
-  cout << ptInv.matrix() << endl;
   Vector3d dir(getProjectionTransform().inverse() * p);
   return dir.normalized();
 }
