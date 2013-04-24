d5a081d0a5b60211dfb7cf5f127779e325eac37f
Drew Noakes <git@drewnoakes.com>
2013-04-24 07:13:47 +0100

    Added control to return head to home position.

--- DataStreamer/getDebugControls.cc
+++ DataStreamer/getDebugControls.cc
@@ -34,6 +34,11 @@ vector<Control> DataStreamer::getDebugControls()
   controls.push_back(Control::createAction("&blacktriangle;",      [&moveHead](){ moveHead( 0, 5); }));
   controls.push_back(Control::createAction("&blacktriangledown;",  [&moveHead](){ moveHead( 0,-5); }));
   controls.push_back(Control::createAction("&blacktriangleright;", [&moveHead](){ moveHead(-5, 0); }));
+  controls.push_back(Control::createAction("home", [](){
+    auto head = Head::GetInstance();
+    head->m_Joint.SetEnableHeadOnly(true, true);
+    head->MoveToHome();
+  }));
 
   // Layers
   // TODO: should lambdas be declared mutable?
@@ -41,7 +46,7 @@ vector<Control> DataStreamer::getDebugControls()
   controls.push_back(Control::createBool("Line dots",        d_shouldDrawLineDots,      [this](bool const& value) { d_shouldDrawLineDots = value; }));
   controls.push_back(Control::createBool("Lines (observed)", d_shouldDrawObservedLines, [this](bool const& value) { d_shouldDrawObservedLines = value; }));
   controls.push_back(Control::createBool("Lines (expected)", d_shouldDrawExpectedLines, [this](bool const& value) { d_shouldDrawExpectedLines = value; }));
-  controls.push_back(Control::createBool("Horizon", d_shouldDrawHorizon, [this](bool const& value) { d_shouldDrawHorizon = value; }));
+  controls.push_back(Control::createBool("Horizon",          d_shouldDrawHorizon,       [this](bool const& value) { d_shouldDrawHorizon = value; }));
 
   return controls;
 }
