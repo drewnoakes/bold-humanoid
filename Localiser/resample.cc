5684b6eb5cdc4d4d003c7a26c265eeaaad7a429f
Drew Noakes <git@drewnoakes.com>
2013-04-25 17:12:43 +0200

    Don't generate random particles in cycles where we do not see any fixed landmarks.

--- Localiser/resample.cc
+++ Localiser/resample.cc
@@ -4,10 +4,15 @@ shared_ptr<vector<Particle>> Localiser::resample(shared_ptr<vector<Particle>> co
 {
   auto newParticles = make_shared<vector<Particle>>();
 
-  unsigned randomizeCount = particleCount * d_randomizeRatio;
-  unsigned drawCount = particleCount - randomizeCount;
+  auto cameraState = AgentState::get<CameraFrameState>();
 
-  assert(randomizeCount + drawCount == particleCount);
+  bool seenLandmark = cameraState->getGoalObservations().size() >= d_minGoalsNeeded || (d_useLines && cameraState->getObservedLineSegments().size() != 0);
+
+  unsigned randomizeCount = seenLandmark
+    ? particleCount * d_randomizeRatio
+    : 0; // don't randomize if we haven't seen any fixed landmarks this cycle
+
+  unsigned drawCount = particleCount - randomizeCount;
 
   for (int i = 0; i < randomizeCount; i++)
   {
@@ -22,6 +27,8 @@ shared_ptr<vector<Particle>> Localiser::resample(shared_ptr<vector<Particle>> co
     // TODO apply noise to the generated samples? probably do that in the motion model, even if no motion
     newParticles->push_back(drawSample());
   }
+
+  assert(newParticles->size() == particleCount);
 
   return newParticles;
 }
