9da6244d975ba965567e8f661bbdce8b16614ab3
Drew Noakes <git@drewnoakes.com>
2013-04-26 11:01:25 +0200

    Added Math::clamp, Math::lerp. Needs unit tests, but compo coming!

--- Math/math.hh
+++ Math/math.hh
@@ -34,6 +34,40 @@ namespace bold
 
     static double smallestAngleBetween(Eigen::Vector2d v1, Eigen::Vector2d v2);
 
+    template<typename T>
+    static double clamp(T val, T min, T max)
+    {
+      if (val < min)
+        return min;
+      if (val > max)
+        return max;
+      return val;
+    }
+
+    /**
+      * Maps @link input to a value in the range from @link lowerOutput to @link upperOutput.
+      * If @link input is outside the range from @link lower to @link upper, then @link lowerOutput or @link upperOutput are returned,
+      * otherwise the value is linearly interpolated.
+      * Note that @link lower must be less than @link upper, but that there is no restriction on values of @link lowerOutput and @link upperOutput.
+      */
+    template<typename T>
+    static T lerp(double const& input, double const& lower, double const& upper, T const& lowerOutput, T const& upperOutput)
+    {
+      if (upper <= lower)
+        throw std::runtime_error("lower must be less than upper");
+
+      double ratio = (input - lower) / (upper - lower);
+      ratio = clamp(ratio, 0.0, 1.0);
+
+      return lowerOutput + (upperOutput - lowerOutput) * ratio;
+    };
+
+    template<typename T>
+    static T lerp(double const& ratio, T const& lowerOutput, T const& upperOutput)
+    {
+      return lowerOutput + (upperOutput - lowerOutput) * ratio;
+    };
+
   private:
     Math() {}
   };
