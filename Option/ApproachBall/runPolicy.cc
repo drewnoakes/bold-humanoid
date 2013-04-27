e548964287c56a3b820817eb44689ceed9366482
Drew Noakes <git@drewnoakes.com>
2013-04-27 09:12:57 +0200

    Refactor ApproachBall::runPolicy using Math class.

--- Option/ApproachBall/runPolicy.cc
+++ Option/ApproachBall/runPolicy.cc
@@ -2,35 +2,29 @@
 
 OptionList ApproachBall::runPolicy()
 {
-  auto agentFrameState = AgentState::get<AgentFrameState>();
+  auto ballPos = AgentState::get<AgentFrameState>()->getBallObservation();
 
-  auto ballPos = agentFrameState->getBallObservation();
   if (!ballPos)
   {
-    cerr << "[ApproachBall::runPolicy] No ball observation! " << endl;
+    cerr << "[ApproachBall::runPolicy] No ball observation!" << endl;
     return OptionList();
   }
 
   double dist = ballPos->head<2>().norm();
   double breakDist = 0.5;
-  double alpha = dist/breakDist;
-  if (alpha > 1)
-    alpha = 1;
-  
-  Vector2d minMove(5.0, 0);
-  Vector2d maxMove(30.0, 0);
-  Vector2d move = minMove + alpha * (maxMove - minMove);
 
+  double alpha = Math::clamp(dist/breakDist, 0.0, 1.0);
+
+  Vector2d move = Math::lerp(alpha, Vector2d(5.0, 0), Vector2d(30.0, 0));
   cout << "Move: " << move.transpose() << endl;
 
   d_ambulator->setMoveDir(move);
-  
+
   double turnAngle = -atan2(ballPos->x(), ballPos->y()) * 35.0;
   double turnGain = 0.5;
 
   d_ambulator->setTurnAngle(turnGain * turnAngle);
   d_ambulator->step();
-
 
   return OptionList();
 }
