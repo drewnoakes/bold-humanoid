e5955585baf44181d182e723e63480f018ba8b1b
Drew Noakes <git@drewnoakes.com>
2013-05-18 15:24:16 +0100

    Extended CppTests with experiments using the shared_ptr API.

--- test/CppTests.hh
+++ test/CppTests.hh
@@ -36,3 +36,92 @@ TEST (CppTests, castingSharedPointers)
   // RTTI doesn't provide the derived type when upcast
   EXPECT_NE( &typeid(base), &typeid(derived) );
 }
+
+TEST (CppTests, reassigningSharedPointers)
+{
+  auto a = make_shared<int>(1);
+  
+  EXPECT_EQ( 1, *a );
+  
+  auto b = a;
+  
+  EXPECT_EQ( 1, *a );
+  EXPECT_EQ( 1, *b );
+  
+  // Reassigning the object pointed to affects all copies of the shared_ptr
+  *b = 2;
+  
+  EXPECT_EQ( 2, *a );
+  EXPECT_EQ( 2, *b );
+  
+  // Reassigning an instance of shared_ptr with another shared_ptr affects only the copy be assigned to
+  a = make_shared<int>(3);
+
+  EXPECT_EQ( 3, *a );
+  EXPECT_EQ( 2, *b );
+}
+
+TEST (CppTests, resettingSharedPointers)
+{
+  auto a = make_shared<int>(1);
+  
+  EXPECT_EQ( 1, *a );
+  
+  auto b = a;
+  
+  EXPECT_EQ( a, b );
+  EXPECT_EQ( 1, *a );
+  EXPECT_EQ( 1, *b );
+ 
+  a.reset(new int);
+  *a = 2;
+  
+  EXPECT_EQ( 2, *a );
+  EXPECT_EQ( 1, *b );
+ 
+  a.reset();
+
+  EXPECT_EQ( nullptr, a );
+  EXPECT_EQ( 1, *b );
+}
+
+TEST (CppTests, referenceCounting)
+{
+  auto a = make_shared<int>(1);
+  
+  EXPECT_EQ( 1, a.use_count() );
+  EXPECT_TRUE( a.unique() );
+  
+  auto b = a;
+  
+  EXPECT_EQ( 2, a.use_count() );
+  EXPECT_EQ( 2, b.use_count() );
+  EXPECT_FALSE( a.unique() );
+  EXPECT_FALSE( b.unique() );
+ 
+  a.reset();
+  
+  EXPECT_EQ( 1, b.use_count() );
+  EXPECT_TRUE( b.unique() );
+}
+
+TEST (CppTests, swappingSharedPointers)
+{
+  auto a = make_shared<int>(1);
+  auto b = make_shared<int>(2);
+
+  EXPECT_EQ( 1, *a );
+  EXPECT_EQ( 2, *b );
+
+  b.swap(a);
+  
+  EXPECT_EQ( 2, *a );
+  EXPECT_EQ( 1, *b );
+}
+
+///
+/// ATOMIC OPERATIONS
+///
+/// See http://en.cppreference.com/w/cpp/memory/shared_ptr/atomic
+///
+
