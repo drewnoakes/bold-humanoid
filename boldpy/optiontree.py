93f3f08dc720951f2d182ae70d4e688988b57c82
Sander van Dijk <sgvandijk@gmail.com>
2013-05-31 21:12:11 +0100

    ActionOption implementation in Python

--- boldpy/optiontree.py
+++ boldpy/optiontree.py
@@ -1,10 +1,38 @@
 import bold
 from boldpy.agent import agent
+import logging
+
+class ActionOption(bold.Option):
+    def __init__(self, id, actionName):
+        bold.Option.__init__(self, id)
+
+        self.actionName = actionName
+        self.started = False
+
+        
+    def hasTerminated(self):
+        if not self.started:
+            return 0.0
+
+        if (agent.getActionModule().isRunning()):
+            return 0.0
+
+        return 1.0;
+
+
+    def runPolicy(self):
+        if not self.started and not agent.getActionModule().isRunning:
+            logging.info('Sarting action: ' + str(getID()))
+            agent.getActionModule().start(self.actionName)
+            self.started = true
+
+        return [];
+
 
 class PyOptionTreeBuilder:
     def createActionOptions(self, tree, namesScripts):
         for ns in namesScripts:
-            tree.addOption(bold.ActionOption(ns[0], ns[1], agent.getActionModule()))
+            tree.addOption(ActionOption(ns[0], ns[1]))
 
     def createOptions(self, tree):
         actionOptions = [
@@ -16,11 +44,11 @@ class PyOptionTreeBuilder:
 
         self.createActionOptions(tree, actionOptions)
         
-        hm = agent.getHeadModule()
-        wm = agent.getWalkModule()
-        amb = agent.getAmbulator()
+        #hm = agent.getHeadModule()
+        #wm = agent.getWalkModule()
+        #amb = agent.getAmbulator()
 
-        tree.addOption(bold.StopWalking("stopwalking", amb))
+        #tree.addOption(bold.StopWalking("stopwalking", amb))
 
     def buildTree(self):
         tree = bold.OptionTree()
