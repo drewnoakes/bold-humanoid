2bbb8d78dff3e85481ee346fe06726a18bb40ee8
Drew Noakes <git@drewnoakes.com>
2013-06-06 13:44:39 +0100

    Fixed a few bugs in FieldEdgePass.

--- ImagePassHandler/FieldEdgePass/fieldedgepass.hh
+++ ImagePassHandler/FieldEdgePass/fieldedgepass.hh
@@ -14,33 +14,33 @@ namespace bold
     : d_fieldLabel(fieldLabel),
       d_pixelWidth(pixelWidth),
       d_pixelHeight(pixelHeight),
-      d_maxRowByCol(pixelWidth)
+      d_maxYByX(pixelWidth)
     {}
 
     void onImageStarting() override
     {
-      assert(d_fieldLabel.size() == d_pixelWidth);
+      assert(d_maxYByX.size() == d_pixelWidth);
       
       for (ushort x = 0; x < d_pixelWidth; x++)
-        d_fieldLabel[x] = d_pixelHeight;
+        d_maxYByX[x] = d_pixelHeight;
     }
 
-    void onPixel(uchar value, ushort x, ushort y) override
+    void onPixel(uchar labelId, ushort x, ushort y) override
     {
       assert(x >=0 && x < d_pixelWidth);
       
-      if (value == d_fieldLabel->id())
+      if (labelId == d_fieldLabel->id())
       {
-        // Increment count in bucket
-        if (y < d_maxRowByCol[x])
-          d_maxRowByCol[x] = y;
+        assert(y > d_maxYByX[x]);
+        
+        d_maxYByX[x] = y;
       }
     }
     
-    std::vector<uchar>& getEdgeYValues() const { return d_maxRowByCol; }
+    std::vector<uchar>& getEdgeYValues() const { return d_maxYByX; }
 
   private:
-    std::vector<uchar> d_maxRowByCol;
+    std::vector<uchar> d_maxYByX;
     std::shared_ptr<PixelLabel> d_fieldLabel;
     ushort d_pixelWidth;
     ushort d_pixelHeight;
