9c65ae640168e11215d4bf8458532ff3d946d84f
Drew Noakes <git@drewnoakes.com>
2013-06-11 00:13:30 +0100

    Added UI control to toggle auto-balance of walk.
    Extracted Ambulator ctor to own .cc file.

--- Ambulator/ambulator.hh
+++ Ambulator/ambulator.hh
@@ -14,27 +14,7 @@ namespace bold
   class Ambulator : public Configurable
   {
   public:
-    Ambulator(std::shared_ptr<WalkModule> walkModule)
-      : Configurable("ambulator"),
-        d_walkModule(walkModule),
-        d_xAmp(0.0, getParam("xAmpDelta", 3.0)),
-        d_yAmp(0.0, getParam("yAmpDelta", 3.0)),
-        d_turnAmp(0.0, getParam("turnDelta", 1.0)),
-        d_maxHipPitchAtSpeed(getParam("maxHipPitchAtSpeed", 15.0)),
-        d_minHipPitch(getParam("minHipPitch", 13.0)),
-        d_maxHipPitch(getParam("maxHipPitch", 17.0)),
-        d_turnAngleSet(false),
-        d_moveDirSet(false),
-        d_controls()
-    {
-      // TODO these should be double controls
-      d_controls.push_back(Control::createInt("Min hip pitch", d_minHipPitch, [this](int value) { d_minHipPitch = value; }));
-      d_controls.push_back(Control::createInt("Max hip pitch", d_maxHipPitch, [this](int value) { d_maxHipPitch = value; }));
-      d_controls.push_back(Control::createInt("Max hip pitch @ speed", d_maxHipPitchAtSpeed, [this](int value) { d_maxHipPitchAtSpeed = value; }));
-      d_controls.push_back(Control::createInt("X smoothing delta", d_xAmp.getDelta(), [this](int value) { d_xAmp.setDelta(value); }));
-      d_controls.push_back(Control::createInt("Y smoothing delta", d_yAmp.getDelta(), [this](int value) { d_yAmp.setDelta(value); }));
-      d_controls.push_back(Control::createInt("Turn smoothing delta", d_turnAmp.getDelta(), [this](int value) { d_turnAmp.setDelta(value); }));
-    }
+    Ambulator(std::shared_ptr<WalkModule> walkModule);
 
     void step();
 
