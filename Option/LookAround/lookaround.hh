c4b3a9079cf16f894cbcbc428aef9dfaa2cd1451
Drew Noakes <git@drewnoakes.com>
2013-06-18 14:42:49 +0100

    LookAround has more sensible default param values.

--- Option/LookAround/lookaround.hh
+++ Option/LookAround/lookaround.hh
@@ -13,21 +13,28 @@ namespace bold
     : Option(id),
       d_headModule(headModule)
     {
-      d_topAngle      = getParam("topAngle", -30.0);
-      d_bottomAngle   = getParam("bottomAngle", 15.0);
-      d_sideAngle     = getParam("sideAngle", 100.0);
-      d_durationHoriz = getParam("durationHoriz", 1.3);
-      d_durationVert  = getParam("durationVert", 0.4);
+      d_topAngle      = getParam("topAngle",    -20.0);
+      d_bottomAngle   = getParam("bottomAngle",  30.0);
+      d_sideAngle     = getParam("sideAngle",   100.0);
+      d_durationHoriz = getParam("durationHoriz", 1.5);
+      d_durationVert  = getParam("durationVert",  0.2);
     }
 
     virtual std::vector<std::shared_ptr<Option>> runPolicy() override;
 
   private:
     std::shared_ptr<HeadModule> d_headModule;
+
+    /// The head's upwards tilt angle
     double d_topAngle;
+    /// The head's downwards tilt angle
     double d_bottomAngle;
+    /// The head's maximum pan angle (negated for left side)
     double d_sideAngle;
+    
+    /// The time spent in each horizontal movement
     double d_durationHoriz;
+    /// The time spent in each vertical movement
     double d_durationVert;
   };
 }
