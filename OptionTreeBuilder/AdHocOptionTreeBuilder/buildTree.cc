b6e934a36ce2ae7ca0936d4d426f015a104674eb
Drew Noakes <git@drewnoakes.com>
2013-06-19 16:40:51 +0100

    Debug output.

--- OptionTreeBuilder/AdHocOptionTreeBuilder/buildTree.cc
+++ OptionTreeBuilder/AdHocOptionTreeBuilder/buildTree.cc
@@ -477,8 +477,10 @@ unique_ptr<OptionTree> AdHocOptionTreeBuilder::buildTree(unsigned teamNumber,
         double panAngleRange = headModule->getLeftLimitRads();
         double panRatio = panAngle / panAngleRange;
         double circleDurationSeconds = fabs(panRatio) * 3;
-        //cout << "circleDuration: " << circleDurationSeconds << endl;
-        //cout << "Seconds since start: " << circleBallState->secondsSinceStart() << endl;
+        cout << "[circleBallState] circleDurationSeconds=" << circleDurationSeconds
+             << " secondsSinceStart=" << circleBallState->secondsSinceStart()
+             << " panRatio=" << panRatio
+             << " leftLimitDegs=" << headModule->getLeftLimitDegs() << endl;
         return circleBallState->secondsSinceStart() > circleDurationSeconds;
       });
 
