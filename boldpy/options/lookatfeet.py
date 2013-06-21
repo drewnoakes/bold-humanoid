21222e7c532ce7c7aa4d01be45e6da9d880c8ede
Sander van Dijk <sgvandijk@gmail.com>
2013-06-21 15:38:33 +0100

    LookAtFeet Python Option working

--- boldpy/options/lookatfeet.py
+++ boldpy/options/lookatfeet.py
@@ -1,12 +1,16 @@
 import bold
+from boldpy.agent import *
 
 class LookAtFeet(bold.Option):
     def __init__(self, id):
         super(LookAtFeet, self).__init__(id)
 
-        self.feetX = self.getParamDbl("feetX", 1.0)
-        self.feetY = self.getParamDbl("feetY", 1.0)
+        self.feetX = self.getParamDbl("feetX", 0.0)
+        self.feetY = self.getParamDbl("feetY", -67.5)
 
     def runPolicy(self):
+        hm = getAgent().getHeadModule()
+        hm.moveToDegs(self.feetX, self.feetY)
+
         return []
 
