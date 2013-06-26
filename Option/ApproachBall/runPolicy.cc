79eb8e1f047439473e603078046bc75da752be34
Drew Noakes <git@drewnoakes.com>
2013-06-26 21:40:33 +0200

    Simplify turn logic.

--- Option/ApproachBall/runPolicy.cc
+++ Option/ApproachBall/runPolicy.cc
@@ -20,24 +20,7 @@ std::vector<std::shared_ptr<Option>> ApproachBall::runPolicy()
   // NOTE atan has flipped x/y on purpose
   double ballAngleRads = -atan2(ballPos->x(), ballPos->y());
 
-  double speedScaleDueToAngle;
-
-//   if (ballAngleRads > d_upperTurnLimitRads)
-//   {
-    // We have a long way to turn, so don't walk forwards
-//     speedScaleDueToAngle = 0.0;
-//   }
-//   else if (ballAngleRads < d_lowerTurnLimitRads)
-//   {
-    // We are roughly facing the ball, so just walk forwards with full speed
-//     speedScaleDueToAngle = 1.0;
-//   }
-//   else
-  {
-    speedScaleDueToAngle = Math::lerp(ballAngleRads, d_lowerTurnLimitRads, d_upperTurnLimitRads, 1.0, 0.0);
-//     speedScaleDueToAngle = Math::clamp(fabs((ballAngleRads - d_lowerTurnLimitRads) /
-//                                             (d_upperTurnLimitRads - d_lowerTurnLimitRads)), 0.0, 1.0);
-  }
+  double speedScaleDueToAngle = Math::lerp(ballAngleRads, d_lowerTurnLimitRads, d_upperTurnLimitRads, 1.0, 0.0);
 
   cout << "speedScaleDueToAngle=" << speedScaleDueToAngle << " ballAngleRads=" << ballAngleRads << endl;
 
