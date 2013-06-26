e2eb221374433e0a6f37eabf1f21d41ded6ab7d5
Drew Noakes <git@drewnoakes.com>
2013-06-26 21:03:14 +0200

    Further debugging of turn logic.

--- Option/ApproachBall/runPolicy.cc
+++ Option/ApproachBall/runPolicy.cc
@@ -2,6 +2,9 @@
 
 std::vector<std::shared_ptr<Option>> ApproachBall::runPolicy()
 {
+  assert(d_upperTurnLimitRads > d_lowerTurnLimitRads);
+  assert(d_brakeDistance != 0);
+
   auto ballPos = AgentState::get<AgentFrameState>()->getBallObservation();
 
   if (!ballPos)
@@ -17,8 +20,26 @@ std::vector<std::shared_ptr<Option>> ApproachBall::runPolicy()
   // NOTE atan has flipped x/y on purpose
   double ballAngleRads = -atan2(ballPos->x(), ballPos->y());
 
-  double speedScaleDueToAngle = Math::clamp(fabs((ballAngleRads - d_lowerTurnLimitRads) /
-                                                 (d_upperTurnLimitRads - d_lowerTurnLimitRads)), 1.0, 0.0);
+  double speedScaleDueToAngle;
+
+//   if (ballAngleRads > d_upperTurnLimitRads)
+//   {
+    // We have a long way to turn, so don't walk forwards
+//     speedScaleDueToAngle = 0.0;
+//   }
+//   else if (ballAngleRads < d_lowerTurnLimitRads)
+//   {
+    // We are roughly facing the ball, so just walk forwards with full speed
+//     speedScaleDueToAngle = 1.0;
+//   }
+//   else
+  {
+    speedScaleDueToAngle = Math::lerp(ballAngleRads, d_lowerTurnLimitRads, d_upperTurnLimitRads, 1.0, 0.0);
+//     speedScaleDueToAngle = Math::clamp(fabs((ballAngleRads - d_lowerTurnLimitRads) /
+//                                             (d_upperTurnLimitRads - d_lowerTurnLimitRads)), 0.0, 1.0);
+  }
+
+  cout << "speedScaleDueToAngle=" << speedScaleDueToAngle << " ballAngleRads=" << ballAngleRads << endl;
 
   Vector2d moveDir = Math::lerp(speedDueToDistance * speedScaleDueToAngle,
                                 Vector2d(d_minForwardSpeed, 0),
