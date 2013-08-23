555033e98e6bcfd5664d92aba4dc8c44b44834ef
Sander van Dijk <sgvandijk@gmail.com>
2013-08-23 13:28:44 +0200

    Initialize sum variable in moving average stddev calculation

---
+++ CameraModel/expectedPixelSize.cc
@@ -0,0 +1,15 @@
+#include "cameramodel.ih"
+
+unsigned CameraModel::expectedPixelSize(Vector3d const& position, double diameter)
+{
+  // Up in camera fram
+  Vector3d up(0, 0, 1);
+  
+  // Perpendicular to object direction
+  Vector3d perp = position.cross(up).normalized();
+
+  double s = (getProjectionTransform() * (diameter * perp)).norm();
+  cout << "s: " << s << endl;
+  return (s + 0.5);
+}
+
