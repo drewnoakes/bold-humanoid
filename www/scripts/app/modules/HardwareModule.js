c8fc678a20a80aa5bc358b98bb37879d53f1b55c
Drew Noakes <git@drewnoakes.com>
2013-11-09 13:12:05 +0000

    Made the body figure of the HardwareModule reusable.

--- www/scripts/app/modules/HardwareModule.js
+++ www/scripts/app/modules/HardwareModule.js
@@ -5,9 +5,10 @@ define(
     [
         'DataProxy',
         'Constants',
-        'Protocols'
+        'Protocols',
+        'BodyFigure'
     ],
-    function (DataProxy, Constants, Protocols)
+    function (DataProxy, Constants, Protocols, BodyFigure)
     {
         'use strict';
 
@@ -28,8 +29,6 @@ define(
                 precision: 0
             }
         };
-
-        var bodyTemplate = Handlebars.compile($('#hardware-body-model-template').html());
 
         var chartHeight = 150,
             chartWidth = 430,
@@ -99,7 +98,8 @@ define(
                 this.temperatureSeriesById.push(series);
             }
 
-            this.buildJointBlocks();
+            this.bodyFigure = new BodyFigure();
+            this.container.append(this.bodyFigure.element);
 
             this.subscription = DataProxy.subscribe(
                 Protocols.hardwareState,
@@ -108,80 +108,6 @@ define(
                     onmessage: _.bind(this.onData, this)
                 }
             );
-        };
-
-        HardwareModule.prototype.buildJointBlocks = function()
-        {
-            var shoulderOffset = 60,
-                elbowOffset = 65,
-                hipOffset = 40,
-                kneeOffset = 45,
-                footOffset = 50;
-
-            // TODO rename Constants.jointIds to use consistent pitch/roll/yaw naming
-
-            var bodyData = {
-                joints: [
-                    { id: Constants.jointIds.headTilt, name: 'Head Tilt', row: 0, group: 0, xOffset: 0 },
-                    { id: Constants.jointIds.headPan,  name: 'Head Pan', row: 1, group: 0, xOffset: 0 },
-
-                    { id: Constants.jointIds.shoulderForwardLeft,  name: 'Left Shoulder Pitch', row: 2, group: 1, xOffset: shoulderOffset },
-                    { id: Constants.jointIds.shoulderForwardRight, name: 'Right Shoulder Pitch', row: 2, group: 1, xOffset: -shoulderOffset },
-                    { id: Constants.jointIds.shoulderOutwardLeft,  name: 'Left Shoulder Roll', row: 3, group: 1, xOffset: shoulderOffset },
-                    { id: Constants.jointIds.shoulderOutwardRight, name: 'Right Shoulder Roll', row: 3, group: 1, xOffset: -shoulderOffset },
-
-                    { id: Constants.jointIds.elbowLeft,  name: 'Left Elbow',  row: 4, group: 2, xOffset: elbowOffset },
-                    { id: Constants.jointIds.elbowRight, name: 'Right Elbow', row: 4, group: 2, xOffset: -elbowOffset },
-
-                    { id: Constants.jointIds.legTurnLeft,     name: 'Left Hip Yaw', row: 5, group: 3, xOffset: hipOffset },
-                    { id: Constants.jointIds.legTurnRight,    name: 'Right Hip Yaw', row: 5, group: 3, xOffset: -hipOffset },
-                    { id: Constants.jointIds.legOutLeft,      name: 'Left Hip Roll', row: 6, group: 3, xOffset: hipOffset },
-                    { id: Constants.jointIds.legOutRight,     name: 'Right Hip Roll', row: 6, group: 3, xOffset: -hipOffset },
-                    { id: Constants.jointIds.legForwardLeft,  name: 'Left Hip Pitch', row: 7, group: 3, xOffset: hipOffset },
-                    { id: Constants.jointIds.legForwardRight, name: 'Right Hip Pitch', row: 7, group: 3, xOffset: -hipOffset },
-
-                    { id: Constants.jointIds.kneeLeft,  name: 'Left Knee', row: 8, group: 4, xOffset: kneeOffset },
-                    { id: Constants.jointIds.kneeRight, name: 'Right Knee', row: 8, group: 4, xOffset: -kneeOffset },
-
-                    { id: Constants.jointIds.footForwardLeft,  name: 'Left Ankle Pitch', row: 9, group: 5, xOffset: footOffset },
-                    { id: Constants.jointIds.footForwardRight, name: 'Right Ankle Pitch', row: 9, group: 5, xOffset: -footOffset },
-                    { id: Constants.jointIds.footOutLeft,      name: 'Left Ankle Roll', row: 10, group: 5, xOffset: footOffset },
-                    { id: Constants.jointIds.footOutRight,     name: 'Right Ankle Roll', row: 10, group: 5, xOffset: -footOffset }
-                ],
-                containerHeight: 0,
-                containerWidth: 0
-            };
-
-            // layout constants
-            var blockHeight = 20;
-            var blockWidth = 70;
-            var blockSpacing = 25; // from top-to-top, not bottom-to-top
-            var groupSpacing = 6;
-            var center = 70;
-
-            _.each(bodyData.joints, function (joint)
-            {
-                joint.y = groupSpacing * joint.group + blockSpacing * joint.row;
-                joint.x = joint.xOffset >= 0
-                    ? center + joint.xOffset
-                    : center + joint.xOffset;// - blockWidth;
-                joint.height = blockHeight;
-                joint.width = blockWidth;
-
-                bodyData.containerWidth = Math.max(bodyData.containerWidth, joint.x + joint.width);
-                bodyData.containerHeight = Math.max(bodyData.containerHeight, joint.y + joint.height);
-            });
-
-            var bodyHtml = bodyTemplate(bodyData);
-            var $bodyContainer = $('<div></div>').html(bodyHtml).children().appendTo(this.container);
-
-            this.jointBlockById = [undefined];
-
-            for (var i = 1; i <= 20; i++) {
-                var jointBlock = $bodyContainer.find('div.joint[data-joint-id=' + i + ']').get(0);
-                jointBlock.temperature = $(jointBlock).find('div.temperature');
-                this.jointBlockById.push(jointBlock);
-            }
         };
 
         HardwareModule.prototype.unload = function ()
@@ -210,14 +136,14 @@ define(
             {
                 this.temperatureSeriesById[joint.id].append(time, joint.temp);
 
-                this.jointBlockById[joint.id].temperature.text(joint.temp);
-
-                var t = Math.max(Math.min(highTemperature, joint.temp), lowTemperature),
+                var jointElement = this.bodyFigure.getJointElement(joint.id),
+                    t = Math.max(Math.min(highTemperature, joint.temp), lowTemperature),
                     ratio = (t - lowTemperature) / (highTemperature - lowTemperature),
                     hue = 140 * (1 - ratio),
                     hsl = 'hsl('  + hue + ', 100%, 50%)';
 
-                this.jointBlockById[joint.id].style.background = hsl;
+                jointElement.textContent = joint.temp;
+                jointElement.style.background = hsl;
             }.bind(this));
         };
 
