0ec18495e7d15b77a536d5ac5100238d2db33809
Drew Noakes <git@drewnoakes.com>
2013-12-11 21:51:14 +0000

    New logging framework added.

---
+++ util/log.cc
@@ -0,0 +1,41 @@
+#include "log.hh"
+
+#include "ccolor.hh"
+
+using namespace bold;
+using namespace std;
+
+LogLevel log::minLevel = LogLevel::Verbose;
+
+log::~log()
+{
+  if (d_level < minLevel)
+    return;
+
+  int fgColor = 39;
+  int bgColor = 49;
+
+  switch (d_level) {
+    case LogLevel::Verbose:
+      fgColor = 37;
+      break;
+    case LogLevel::Info:
+      break;
+    case LogLevel::Warning:
+      fgColor = 95;
+      break;
+    case LogLevel::Error:
+      fgColor = 37;
+      bgColor = 41;
+      break;
+  }
+
+  auto& ostream = d_level == LogLevel::Error ? cerr : cout;
+
+  if (d_scope.size())
+    ostream << ccolor::fore::lightblue << "[" << d_scope << "] ";
+
+  ostream << "\033[" << fgColor << ";" << bgColor << "m"
+          << d_message->str()
+          << "\033[00m" << endl;
+}
---
+++ util/log.hh
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <iostream>
+#include <sstream>
+#include <memory>
+
+namespace bold
+{
+  enum class LogLevel
+  {
+    Verbose,
+    Info,
+    Warning,
+    Error
+  };
+
+  /** A very minimal logging framework to control console output.
+   */
+  class log
+  {
+  private:
+    log(LogLevel level)
+    : d_scope(),
+      d_level(level),
+      d_message(new std::ostringstream())
+    {}
+
+    log(std::string scope, LogLevel level)
+    : d_scope(scope),
+      d_level(level),
+      d_message(new std::ostringstream())
+    {}
+
+  public:
+    static LogLevel minLevel;
+
+    static log info()    { return log(LogLevel::Info); }
+    static log verbose() { return log(LogLevel::Verbose); }
+    static log warning() { return log(LogLevel::Warning); }
+    static log error()   { return log(LogLevel::Error); }
+
+    static log info(std::string scope)    { return log(scope, LogLevel::Info); }
+    static log verbose(std::string scope) { return log(scope, LogLevel::Verbose); }
+    static log warning(std::string scope) { return log(scope, LogLevel::Warning); }
+    static log error(std::string scope)   { return log(scope, LogLevel::Error); }
+
+    log(log&& log)
+    : d_scope(log.d_scope),
+      d_level(log.d_level),
+      d_message(std::move(log.d_message))
+    {}
+
+    ~log();
+
+    template<typename T>
+    std::ostringstream& operator<<(T value)
+    {
+      *d_message << value;
+      return *d_message;
+    }
+
+  private:
+    std::string d_scope;
+    LogLevel d_level;
+    std::unique_ptr<std::ostringstream> d_message;
+  };
+}
