eab00ce74d0e01e5ea60b3ca1907bd7302e22b2d
Drew Noakes <git@drewnoakes.com>
2013-12-15 21:11:23 +0100

    Modify body 'zero' state so that camera spans horizon (a more natural pose.)
    This zero state is used during testing on a machine where no body is actually available.

--- StateObject/BodyState/zero.cc
+++ StateObject/BodyState/zero.cc
@@ -1,7 +1,13 @@
 #include "bodystate.ih"
 
+#include "../MX28/mx28.hh"
+
 shared_ptr<BodyState const> BodyState::zero(ulong thinkCycleNumber)
 {
   double angles[(int)JointId::MAX + 1] = {0,};
+
+  // Tilt the head up slightly, so that we can see the horizon in the image (better for testing)
+  angles[(int)JointId::HEAD_TILT] = MX28::degs2Value(20.0);
+
   return make_shared<BodyState>(angles, thinkCycleNumber);
 }
