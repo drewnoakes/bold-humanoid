076dbffe88ddb30805a510162d2c1ae885634938
Drew Noakes <git@drewnoakes.com>
2013-12-19 11:32:54 +0000

    Fully qualify symbols.

--- test/helpers.hh
+++ test/helpers.hh
@@ -4,7 +4,7 @@
 
 #include <Eigen/Core>
 
-::testing::AssertionResult VectorsEqual(Vector3d const& a, Vector3d const& b, const float delta = 0.00001) {
+::testing::AssertionResult VectorsEqual(Eigen::Vector3d const& a, Eigen::Vector3d const& b, const float delta = 0.00001) {
   double d = (a-b).norm();
   if (d < delta)
     return ::testing::AssertionSuccess();
@@ -12,7 +12,7 @@
     return ::testing::AssertionFailure() << "Actual: " << b << ", expected: " << a << " d = " << d;
 }
 
-::testing::AssertionResult VectorsEqual(Vector2d const& a, Vector2d const& b, const float delta = 0.00001) {
+::testing::AssertionResult VectorsEqual(Eigen::Vector2d const& a, Eigen::Vector2d const& b, const float delta = 0.00001) {
   double d = (a-b).norm();
   if (d < delta)
     return ::testing::AssertionSuccess();
