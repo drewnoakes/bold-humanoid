e7d2616d5761d29e0b9e4d9176376abd3c322471
Drew Noakes <git@drewnoakes.com>
2013-12-24 18:41:59 +0000

    Made timing-sensitive test more robust across runtime environments.

--- test/ConsumerQueueThreadTests.hh
+++ test/ConsumerQueueThreadTests.hh
@@ -4,6 +4,7 @@
 
 #include "helpers.hh"
 #include "../util/consumerqueuethread.hh"
+#include "../Clock/clock.hh"
 
 using namespace bold;
 using namespace std;
@@ -57,9 +58,18 @@ TEST (ConsumerQueueThreadTests, doesntBlockPusher)
 
   ASSERT_TRUE ( callCount < loopCount );
   this_thread::sleep_for(chrono::milliseconds(loopCount / 2));
-  ASSERT_TRUE ( callCount > 0 && callCount < loopCount ) << "Invalid callCount=" << callCount;
-  this_thread::sleep_for(chrono::milliseconds(loopCount * 2));
-  ASSERT_TRUE ( callCount == loopCount ) << "Invalid loopCount=" << loopCount;
+  ASSERT_GT ( callCount, 0 );
+  ASSERT_LT ( callCount, loopCount );
+
+  auto start = Clock::getTimestamp();
+  while (callCount < loopCount)
+  {
+    if (Clock::getSecondsSince(start) > 1.0)
+      FAIL() << "Timed out waiting for all items to complete";
+    this_thread::sleep_for(chrono::milliseconds(1));
+  }
+
+  ASSERT_EQ ( loopCount, callCount );
 
   queue.stop(); // joins
 }
