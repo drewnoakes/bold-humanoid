f3a03fcd47137110159494da25694fcefb85746b
Drew Noakes <git@drewnoakes.com>
2013-12-24 15:49:08 +0000

    Bring back the 'Randomize' button in the localiser module.

--- www/scripts/app/modules/LocaliserModule.js
+++ www/scripts/app/modules/LocaliserModule.js
@@ -52,7 +52,9 @@ define(
             this.particleSubscription = DataProxy.subscribe(Protocols.particleState,    { json: true, onmessage: _.bind(this.onParticleData, this) });
             this.cameraSubscription   = DataProxy.subscribe(Protocols.cameraFrameState, { json: true, onmessage: _.bind(this.onCameraData, this) });
 
-            ControlBuilder.buildAll('localiser', $('<div></div>', {'class': 'control-container localiser-controls flow'}).appendTo(this.$container).get(0));
+            var container = $('<div></div>', {'class': 'control-container localiser-controls flow'}).appendTo(this.$container).get(0);
+            ControlBuilder.actions('localiser', container);
+            ControlBuilder.buildAll('localiser', container);
 
             // TODO show what is in the camera frame (ball, goals, lines...)
             this.$ballVisibleMarker = $('<div></div>').addClass('marker ball').appendTo(this.$container);
