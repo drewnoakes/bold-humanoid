114e43c1e0a994cb8efdce5f9bf6ec682ed1d121
Sander van Dijk <sgvandijk@gmail.com>
2013-12-26 18:23:36 +0000

    get<> template to get tuple element by type; will be standard in C++14

--- util/meta.hh
+++ util/meta.hh
@@ -17,4 +17,24 @@ namespace bold
   template <int A, int B>
   struct MIN : IF<A < B, A, B>
   {};
- }
+
+  template<int Index, class Search, class First, class... Types>
+  struct get_internal
+  {
+    typedef typename get_internal<Index + 1, Search, Types...>::type type;
+    static constexpr int index = Index;
+  };
+
+  template<int Index, class Search, class... Types>
+  struct get_internal<Index, Search, Search, Types...>
+  {
+    typedef get_internal type;
+    static const int index = Index;
+  };
+
+  template<class T, class... Types>
+  T get(std::tuple<Types...> const& tuple)
+  {
+    return std::get<get_internal<0,T,Types...>::type::index>(tuple);
+  }
+}
