d0dd17af25c98910b9f9ac323833084d12702263
Drew Noakes <git@drewnoakes.com>
2014-01-17 16:44:36 +0000

    Add TODO.

--- VisualCortex/shouldMergeBallBlobs.cc
+++ VisualCortex/shouldMergeBallBlobs.cc
@@ -4,6 +4,8 @@ bool VisualCortex::shouldMergeBallBlobs(Bounds2i const& larger, Bounds2i const& 
 {
   // Merge if the union would be more square, and the new maxDimension is not far off the original one
 
+  // TODO VISION do not merge blobs if the union would be too large to be considered as the ball
+
   Bounds2i combined = Bounds2i::merge(larger, smaller);
 
   double largerAspect = (double)larger.minDimension() / larger.maxDimension();
