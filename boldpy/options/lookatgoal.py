fff0e10c6245a3fb8fa454aa46a571e0eb0c4b82
Sander van Dijk <sgvandijk@gmail.com>
2014-01-25 22:07:54 +0000

    lookatgoal.py operational

--- boldpy/options/lookatgoal.py
+++ boldpy/options/lookatgoal.py
@@ -6,14 +6,9 @@ class LookAtGoal(bold.Option):
     def __init__(self, id):
         super(LookAtGoal, self).__init__(id)
 
-        self.gain = self.getParamDbl("gain", 0.85)
-        self.maxOffset = self.getParamDbl("maxOffset", 20.0)
-
     def runPolicy(self):
         cameraState = bold.AgentState.getCameraFrameState()
         goalObs = cameraState.getGoalObservations()
-
-        print(goalObs)
 
         if len(goalObs) < 2:
             print("[LookAtGoal::runPolicy] Couldn't see both goal posts!")
@@ -21,11 +16,8 @@ class LookAtGoal(bold.Option):
 
         middle = (goalObs[0] + goalObs[1]) / 2
 
-        print("Middle: " + str(middle))
-
         # TODO: copied from LookAtBall; make into general function, or put getXenterPx on CameraModel
         # Or give HeadModule a method moveTrackPixel
-
         cm = getAgent().getCameraModel();
         w = cm.imageWidth()
         h = cm.imageHeight()
@@ -37,14 +29,10 @@ class LookAtGoal(bold.Option):
         happ = cm.rangeHorizontalDegs() / w
         vapp = cm.rangeVerticalDegs() / h
 
-        offset = (middle - centerPx) * self.gain
+        offset = (middle - centerPx);
         # multiplication is elementwise
         offset *= array([[happ],
                          [vapp]])
-
-        # TODO: find or create saturate function
-        offset = minimum(offset, array([[self.maxOffset],[self.maxOffset]]))
-        offset = maximum(offset, array([[-self.maxOffset],[-self.maxOffset]]))
 
         hm = getAgent().getHeadModule()
         hm.moveTracking(offset[0,0], offset[1,0])
