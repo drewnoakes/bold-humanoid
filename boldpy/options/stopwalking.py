57f7835c3aac871a941618245c77da18f608c316
Sander van Dijk <sgvandijk@gmail.com>
2014-01-25 22:02:26 +0000

    stopwalking.py operational

--- boldpy/options/stopwalking.py
+++ boldpy/options/stopwalking.py
@@ -7,16 +7,12 @@ class StopWalking(bold.Option):
         bold.Option.__init__(self, id)
     
     def hasTerminated(self):
-        print("StopWalking.hasTerminated()")
         return 1.0 if getAgent().getAmbulator().isRunning() else 0.0
 
     def runPolicy(self):
-        print("StopWalking.runPolicy()")
         amb = getAgent().getAmbulator()
         moveDir = numpy.array([[0.0],
                                [0.0]])
-        print(moveDir)
-        print(amb.setMoveDir)
         amb.setMoveDir(moveDir)
         amb.setTurnAngle(0);
         return []
