7f5b2c43d8019715a59cb506f74cb59c1533cf0d
Sander van Dijk <sgvandijk@gmail.com>
2014-01-30 23:52:19 +0000

    New unit tests for new ParticleFilter

--- test/ParticleFilterTests.hh
+++ test/ParticleFilterTests.hh
@@ -6,75 +6,71 @@
 #include <stdexcept>
 
 #include "helpers.hh"
-#include "../Filter/ParticleFilter/particlefilter.hh"
-#include "../ParticleSamplerFactory/WheelSamplerFactory/wheelsamplerfactory.hh"
+#include "../filters/Filter/ParticleFilter/particlefilter.hh"
 
 using namespace std;
 using namespace bold;
 using namespace Eigen;
 
-TEST (ParticleFilterTests, basicOperation)
+TEST (ParticleFilterTests, ESSCheck)
 {
-  unsigned resampleCallCount = 0;
-  ParticleFilter<2>::ParticleResampler resampler
-   = [&resampleCallCount](shared_ptr<vector<ParticleFilter<2>::Particle>> particles, unsigned particleCount)
-    {
-      resampleCallCount++;
-      auto newParticles = make_shared<vector<ParticleFilter<2>::Particle>>();
-      for (int i = particleCount - 1; i >= 0; i--)
-      {
-        newParticles->push_back((*particles)[i]);
-      }
-      return newParticles;
-    };
+  // With uniform weights, ESS should be number of non-zero weights
+  ESSCheck<1, 100, 50> essCheck;
 
-  ParticleFilter<2>::ParticleExtractor extractor =
-    [this](shared_ptr<vector<ParticleFilter<2>::Particle>> particles) { return (*particles)[0].first; };
+  VectorXd weights(100);
 
-  EXPECT_EQ ( 0, resampleCallCount );
+  weights.fill(1.0 / 100);
+  EXPECT_EQ(100, essCheck.ESS(weights));
 
-  int particleCount = 10;
-  int i = 0;
-  ParticleFilter<2> filter(particleCount, [&](){ i++; return Vector2d(i, i); }, resampler, extractor);
+  weights.fill(0);
+  weights.head<50>().fill(1.0 / 50);
+  EXPECT_EQ(50, essCheck.ESS(weights));
+}
 
-  EXPECT_EQ(particleCount, i);
+TEST (ParticleFilterTests, SystematicResample)
+{
+  constexpr int N = 6;
+  SystematicResample<1, N> resample;
+  MatrixXd particles = Matrix<double, 1, N>();
+  for (unsigned i = 0; i < N; ++i)
+    particles(i) = i;
 
-  i = 1;
-  shared_ptr<vector<ParticleFilter<2>::Particle> const> particles = filter.getParticles();
-  for (auto const& p : *particles)
-  {
-    EXPECT_TRUE( VectorsEqual(p.first, Vector2d(i, i)) );
-    EXPECT_EQ( 0.0, p.second );
-    i++;
-  }
+  // With uniform weights, we should get the same samples
+  VectorXd weights = Matrix<double, N, 1>::Constant(1.0 / N);
+  auto newParticles = resample.resample(particles, weights, N);
+  EXPECT_TRUE( MatricesEqual(particles, newParticles) );
 
-  EXPECT_EQ ( i, particleCount + 1 );
+  // Sampling half the samples should give either all odd or all even samples
+  newParticles = resample.resample(particles, weights, N / 2);
+  for (unsigned i = 0; i < N / 2; ++i)
+    EXPECT_TRUE( newParticles(i) == particles(2 * i) || newParticles(i) == particles(2 * i + 1) );
 
-  int predictCallCount = 0;
-  filter.predict([&](Vector2d const& state) { predictCallCount++; return Vector2d(state.x() + 1, state.y() + 1); });
+  // A lone particle with non-zero weight should be sampled always
+  weights.fill(0);
+  weights(0) = 1;
+  newParticles = resample.resample(particles, weights, N);
+  for (unsigned i = 0 ; i < N; ++i)
+    EXPECT_TRUE( VectorsEqual(VectorXd(newParticles.col(i)), VectorXd(particles.col(0))) );
 
-  EXPECT_EQ(particleCount, predictCallCount);
+  weights.fill(0);
+  weights(N / 2) = 1;
+  newParticles = resample.resample(particles, weights, N);
+  for (unsigned i = 0 ; i < N; ++i)
+    EXPECT_TRUE( VectorsEqual(VectorXd(newParticles.col(i)), VectorXd(particles.col(N / 2))) );
 
-  i = 1;
-  for (auto const& p : *(filter.getParticles()))
-  {
-    EXPECT_TRUE( VectorsEqual(p.first, Vector2d(i + 1, i + 1)) );
-    i++;
-  }
+  weights.fill(0);
+  weights(N - 1) = 1;
+  newParticles = resample.resample(particles, weights, N);
+  for (unsigned i = 0 ; i < N; ++i)
+    EXPECT_TRUE( VectorsEqual(VectorXd(newParticles.col(i)), VectorXd(particles.col(N - 1))) );
 
-  EXPECT_EQ ( i, particleCount + 1 );
-
-  // Set weights as [1,2,3,...]
-  // The sampling function will return them in reverse order
-  i = 0;
-  filter.update([&i](Vector2d const& state) { i++; return i; });
-
-  EXPECT_EQ ( 1, resampleCallCount );
-
-  i = particleCount;
-  for (auto const& p : *(filter.getParticles()))
-  {
-    EXPECT_EQ( double(i), p.second );
-    i--;
-  }
+  // A particle with twice the weight should be sampled twice as often
+  // (in this test case where N is even, and weights are larger than 1.0 / N)
+  weights.fill(0);
+  weights(0) = 1.0 / 3.0;
+  weights(1) = 2.0 / 3.0;
+  newParticles = resample.resample(particles, weights, N);
+  unsigned count1 = (newParticles.array() == 0).count();
+  unsigned count2 = (newParticles.array() == 1).count();
+  EXPECT_EQ( count2, 2 * count1 );
 }
