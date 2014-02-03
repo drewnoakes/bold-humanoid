c0b527ae80a9784b936dd2fe858a43ac2b2a937b
Drew Noakes <git@drewnoakes.com>
2014-02-03 23:05:08 +0000

    Code comments.

--- StateObject/BodyState/BodyState.cc
+++ StateObject/BodyState/BodyState.cc
@@ -19,10 +19,15 @@ BodyState::BodyState(shared_ptr<HardwareState const> const& hardwareState, ulong
   d_motionCycleNumber(cycleNumber),
   d_cameraTilt(Config::getSetting<double>("camera.vertical-angle-degrees"))
 {
+  // Add two extra as:
+  // - we don't use index 0
+  // - we add an extra joint for the camera's tilt within the head.
   double angles[(uchar)JointId::MAX + 2];
 
   for (uchar jointId = (uchar)JointId::MIN; jointId <= (uchar)JointId::MAX; jointId++)
     angles[jointId] = hardwareState->getMX28State(jointId).presentPosition;
+
+  // Set the camera head tilt according to the configured angle
   angles[(uchar)JointId::CAMERA_TILT] = Math::degToRad(d_cameraTilt->getValue());
 
   initialise(angles);
