e9d8943883e4c954e95e57bdfdf75f2b981e5098
Drew Noakes <git@drewnoakes.com>
2014-02-06 23:07:33 +0000

    Add JointName::getName.

--- JointId/jointid.hh
+++ JointId/jointid.hh
@@ -1,7 +1,11 @@
 #pragma once
 
+#include <string>
+
 namespace bold
 {
+  typedef unsigned char uchar;
+
   enum class JointId : unsigned char
   {
     R_SHOULDER_PITCH = 1,
@@ -38,8 +42,47 @@ namespace bold
 
     HEAD_START = HEAD_PAN,
     HEAD_END = HEAD_TILT,
-    
+
     MIN = R_SHOULDER_PITCH,
     MAX = HEAD_TILT
+  };
+
+  class JointName
+  {
+  public:
+    JointName() = delete;
+
+    static std::string getName(uchar jointId)
+    {
+      return getName((JointId)jointId);
+    }
+
+    static std::string getName(JointId jointId)
+    {
+      switch (jointId)
+      {
+        case JointId::R_SHOULDER_PITCH: return "R_SHOULDER_PITCH";
+        case JointId::L_SHOULDER_PITCH: return "L_SHOULDER_PITCH";
+        case JointId::R_SHOULDER_ROLL:  return "R_SHOULDER_ROLL";
+        case JointId::L_SHOULDER_ROLL:  return "L_SHOULDER_ROLL";
+        case JointId::R_ELBOW:          return "R_ELBOW";
+        case JointId::L_ELBOW:          return "L_ELBOW";
+        case JointId::R_HIP_YAW:        return "R_HIP_YAW";
+        case JointId::L_HIP_YAW:        return "L_HIP_YAW";
+        case JointId::R_HIP_ROLL:       return "R_HIP_ROLL";
+        case JointId::L_HIP_ROLL:       return "L_HIP_ROLL";
+        case JointId::R_HIP_PITCH:      return "R_HIP_PITCH";
+        case JointId::L_HIP_PITCH:      return "L_HIP_PITCH";
+        case JointId::R_KNEE:           return "R_KNEE";
+        case JointId::L_KNEE:           return "L_KNEE";
+        case JointId::R_ANKLE_PITCH:    return "R_ANKLE_PITCH";
+        case JointId::L_ANKLE_PITCH:    return "L_ANKLE_PITCH";
+        case JointId::R_ANKLE_ROLL:     return "R_ANKLE_ROLL";
+        case JointId::L_ANKLE_ROLL:     return "L_ANKLE_ROLL";
+        case JointId::HEAD_PAN:         return "HEAD_PAN";
+        case JointId::HEAD_TILT:        return "HEAD_TILT";
+        default:                        return "Unknown";
+      }
+    }
   };
 }
