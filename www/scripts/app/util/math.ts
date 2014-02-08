6e69f49ddfe8eaf29465a84d138e3e998471c659
Drew Noakes <git@drewnoakes.com>
2014-02-08 00:32:29 +0000

    New math module, with MovingAverage.

---
+++ www/scripts/app/util/math.ts
@@ -0,0 +1,41 @@
+/**
+ * @author Drew Noakes http://drewnoakes.com
+ */
+
+/// <reference path="../../libs/lodash.d.ts"/>
+
+export class MovingAverage
+{
+    private values: number[] = [];
+    private count: number;
+    private nextIndex: number;
+    private sum: number;
+
+    constructor(private windowSize: number)
+    {
+        this.nextIndex = 0;
+        this.sum = 0;
+        this.count = 0;
+    }
+
+    public next(value: number)
+    {
+        console.assert(!isNaN(value));
+
+        if (this.count !== this.windowSize)
+        {
+            this.values.push(value);
+            this.count++;
+        }
+        else
+        {
+            this.sum -= this.values[this.nextIndex];
+            this.values[this.nextIndex] = value;
+        }
+        this.nextIndex++;
+        if (this.nextIndex === this.windowSize)
+            this.nextIndex = 0;
+        this.sum += value;
+        return this.sum/this.count;
+    }
+}
