9d85bf6d1e9d03bf452639596305bdba82746254
Sander van Dijk <sgvandijk@gmail.com>
2014-02-08 16:24:15 +0000

    Take out debug message

--- cmake/FindNumPy.cmake
+++ cmake/FindNumPy.cmake
@@ -50,8 +50,6 @@ if(NOT PYTHONINTERP_FOUND)
     return()
 endif()
 
-message("${PYTHON_EXECUTABLE}")
-
 execute_process(COMMAND "${PYTHON_EXECUTABLE}" "-c"
     "import numpy as n; print(n.__version__); print(n.get_include());"
     RESULT_VARIABLE _NUMPY_SEARCH_SUCCESS
