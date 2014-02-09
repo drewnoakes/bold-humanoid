ae8ebaa031f61538454be56b8e7ab820a242f3e2
Drew Noakes <git@drewnoakes.com>
2014-02-09 21:09:40 +0000

    Actually fix periodic field-edge column zero bug.

--- ImagePassHandler/FieldEdgePass/PeriodicFieldEdgePass/periodicfieldedgepass.cc
+++ ImagePassHandler/FieldEdgePass/PeriodicFieldEdgePass/periodicfieldedgepass.cc
@@ -26,7 +26,7 @@ void PeriodicFieldEdgePass::onPixel(uchar labelId, ushort x, ushort y)
 {
 //   assert(x >= 0 && x < d_pixelWidth);
 
-  if (!x && x % d_period != 0)
+  if (x % d_period != 0)
     return;
 
   ushort c = x / d_period;
@@ -53,15 +53,23 @@ ushort PeriodicFieldEdgePass::getEdgeYValue(ushort x) const
 {
   assert(x < d_pixelWidth);
 
+  // Map from the x-postiion to the periodic samples.
+
   ushort rem = x % d_period;
   ushort c = x / d_period;
 
   if (rem == 0 || c == d_runByC.size() - 1)
   {
+    // x is an exact multiple of the period, so return the value directly.
     assert(d_maxYByC[c] < d_pixelHeight);
     return d_maxYByC[c];
   }
 
+  // Pixels at the far edge of the image may be beyond the last sampled column.
+  if (c == d_maxYByC.size())
+    return d_maxYByC[c - 1];
+
+  // Interpolate between the two closest samples.
   return Math::lerp((double)rem/d_period, d_maxYByC[c], d_maxYByC[c + 1]);
 }
 
