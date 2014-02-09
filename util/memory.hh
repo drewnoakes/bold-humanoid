6d472ac1ddf5ec8a1fcc6271871e9986fd16cbc4
Sander van Dijk <sgvandijk@gmail.com>
2014-02-09 23:36:50 +0000

    Make EIGEN_VECTORIZE=NO not align and allocate_aligned_shared default to make_shared (even though still everybody should use EIGEN_VECTORIZE=YES)

--- util/memory.hh
+++ util/memory.hh
@@ -6,10 +6,18 @@
 
 namespace bold
 {
+#if EIGEN_ALIGN
   template<typename T, typename... Args>
   std::shared_ptr<T> allocate_aligned_shared(Args... args)
   {
     typedef std::remove_const<T> NonConst;
     return std::allocate_shared<T>(Eigen::aligned_allocator<NonConst>(), args...);
   }
+#else
+  template<typename T, typename... Args>
+  std::shared_ptr<T> allocate_aligned_shared(Args... args)
+  {
+    return std::make_shared<T>(args...);
+  }
+#endif
 }
