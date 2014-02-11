adeffc6d3baca827f573ae3dde222c9f3448aa38
Drew Noakes <git@drewnoakes.com>
2014-02-11 20:48:01 +0000

    Extract some variables.

--- test/BodyStateTests.hh
+++ test/BodyStateTests.hh
@@ -20,11 +20,17 @@ TEST (BodyStateTests, posture)
   shared_ptr<Limb const> leftFoot = am1.getLimb("lFoot");
   shared_ptr<Limb const> rightFoot = am1.getLimb("rFoot");
 
-  EXPECT_EQ( Vector3d(-0.074/2, -0.005, -0.1222 - 0.093 - 0.093 - 0.0335),
+  // See DARwIn-OP_Kinematics.pdf
+
+  double footSide = 0.074/2;
+  double footForward = -0.005;
+  double footDownward = 0.1222 + 0.093 + 0.093 + 0.0335;
+
+  EXPECT_EQ( Vector3d(-footSide, footForward, -footDownward),
              Vector3d(leftFoot->transform.translation()) );
   EXPECT_TRUE( (leftFoot->transform.rotation() - Matrix3d::Identity()).isZero() );
 
-  EXPECT_EQ( Vector3d(0.074/2, -0.005, -0.1222 - 0.093 - 0.093 - 0.0335),
+  EXPECT_EQ( Vector3d(footSide, footForward, -footDownward),
              Vector3d(rightFoot->transform.translation()) );
   EXPECT_TRUE( (rightFoot->transform.rotation() - Matrix3d::Identity()).isZero() );
 
@@ -39,12 +45,12 @@ TEST (BodyStateTests, posture)
   leftFoot = am2.getLimb("lFoot");
   rightFoot = am2.getLimb("rFoot");
 
-  EXPECT_EQ( Vector3d(-0.074/2 - 0.093 - 0.093 - 0.0335, -0.005, -0.1222),
+  EXPECT_EQ( Vector3d(-0.074/2 - 0.093 - 0.093 - 0.0335, footForward, -0.1222),
              Vector3d(leftFoot->transform.translation()) );
   EXPECT_EQ( AngleAxisd(M_PI/2, Vector3d::UnitY()).matrix(),
              leftFoot->transform.rotation().matrix() );
 
-  EXPECT_EQ( Vector3d(0.074/2 + 0.093 + 0.093 + 0.0335, -0.005, -0.1222),
+  EXPECT_EQ( Vector3d(0.074/2 + 0.093 + 0.093 + 0.0335, footForward, -0.1222),
              Vector3d(rightFoot->transform.translation()) );
   EXPECT_EQ( AngleAxisd(-M_PI/2, Vector3d::UnitY()).matrix(),
              rightFoot->transform.rotation().matrix() );
