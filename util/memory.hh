c5ea754c7b9e6a2611b77ffd0c37af0e9e65788a
Drew Noakes <git@drewnoakes.com>
2014-02-17 22:21:46 +0000

    Use perfect forwarding.

--- util/memory.hh
+++ util/memory.hh
@@ -3,21 +3,22 @@
 #include <Eigen/Core>
 #include <memory>
 #include <type_traits>
+#include <utility>
 
 namespace bold
 {
 #if EIGEN_ALIGN
   template<typename T, typename... Args>
-  std::shared_ptr<T> allocate_aligned_shared(Args... args)
+  std::shared_ptr<T> allocate_aligned_shared(Args&&... args)
   {
     typedef std::remove_const<T> NonConst;
-    return std::allocate_shared<T>(Eigen::aligned_allocator<NonConst>(), args...);
+    return std::allocate_shared<T>(Eigen::aligned_allocator<NonConst>(), std::forward<Args>(args)...);
   }
 #else
   template<typename T, typename... Args>
-  std::shared_ptr<T> allocate_aligned_shared(Args... args)
+  std::shared_ptr<T> allocate_aligned_shared(Args&&... args)
   {
-    return std::make_shared<T>(args...);
+    return std::make_shared<T>(std::forward<Args>(args)...);
   }
 #endif
 }
