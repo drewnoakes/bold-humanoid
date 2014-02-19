8199eb257bf561ae57f3ba3b4b8924c2e60620bd
Drew Noakes <git@drewnoakes.com>
2014-02-19 15:55:13 +0000

    Log info about ball position in verbose mode when looking at feet.

--- Option/LookAtFeet/runPolicy.cc
+++ Option/LookAtFeet/runPolicy.cc
@@ -1,8 +1,15 @@
 #include "lookatfeet.ih"
 
+#include "../../StateObject/AgentFrameState/agentframestate.hh"
+
 std::vector<std::shared_ptr<Option>> LookAtFeet::runPolicy()
 {
   d_headModule->moveToDegs(d_panDegs->getValue(), d_tiltDegs->getValue());
+
+  auto ballObs = AgentState::get<AgentFrameState>()->getBallObservation();
+
+  if (ballObs)
+    log::verbose("LookAtFeet::runPolicy") << "Ball pos in agent frame " << ballObs->transpose();
 
   return std::vector<std::shared_ptr<Option>>();
 }
