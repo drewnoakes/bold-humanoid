54f34cbd4ee795f100f4f788805132928da2573e
Drew Noakes <git@drewnoakes.com>
2014-02-25 21:07:11 +0000

    DOMTemplate converted to TypeScript.

---
+++ www/scripts/app/DOMTemplate.ts
@@ -0,0 +1,45 @@
+/**
+ * @author Drew Noakes http://drewnoakes.com
+ */
+
+var parser: DOMParser = new DOMParser();
+
+class DOMTemplate
+{
+    private template: any;
+
+    constructor(templateId: string, public contentType?: string)
+    {
+        this.contentType = contentType || "text/html";
+
+        var templateElement = document.getElementById(templateId);
+
+        if (!templateElement) {
+            console.error('No element found with id', templateId);
+            return;
+        }
+
+        var templateText = templateElement.textContent;
+
+        console.assert(!!templateText);
+
+        this.template = Handlebars.compile(templateText);
+    }
+
+    public create(data)
+    {
+        var obj = parser.parseFromString(this.template(data), this.contentType);
+
+        console.assert(!!obj);
+
+        if (this.contentType === "text/html") {
+            console.assert(!!obj.body);
+            console.assert(!!obj.body.childNodes.length);
+            return obj.body.firstChild;
+        }
+        else {
+            return obj;
+        }
+    }
+}
+export = DOMTemplate;
