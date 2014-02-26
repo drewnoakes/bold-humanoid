4d429100af720f70b479c3e2c85dddfa4de788e7
Drew Noakes <git@drewnoakes.com>
2014-02-26 21:00:35 +0000

    Reorder state registration.

--- Agent/registerStateTypes.cc
+++ Agent/registerStateTypes.cc
@@ -4,20 +4,20 @@ void Agent::registerStateTypes()
 {
   AgentState::registerStateType<AgentFrameState>("AgentFrame");
   AgentState::registerStateType<AmbulatorState>("Ambulator");
-  AgentState::registerStateType<BodyState>("Body");
   AgentState::registerStateType<BodyControlState>("BodyControl");
+  AgentState::registerStateType<BodyState>("Body");
   AgentState::registerStateType<CameraFrameState>("CameraFrame");
   AgentState::registerStateType<DebugState>("Debug");
-  AgentState::registerStateType<MotionTimingState>("MotionTiming");
-  AgentState::registerStateType<LabelCountState>("LabelCount");
-  AgentState::registerStateType<ThinkTimingState>("ThinkTiming");
   AgentState::registerStateType<GameState>("Game");
   AgentState::registerStateType<HardwareState>("Hardware");
+  AgentState::registerStateType<LabelCountState>("LabelCount");
   AgentState::registerStateType<MotionTaskState>("MotionTask");
+  AgentState::registerStateType<MotionTimingState>("MotionTiming");
   AgentState::registerStateType<OdometryState>("Odometry");
   AgentState::registerStateType<OptionTreeState>("OptionTree");
   AgentState::registerStateType<OrientationState>("Orientation");
   AgentState::registerStateType<ParticleState>("Particle");
   AgentState::registerStateType<StaticHardwareState>("StaticHardware");
+  AgentState::registerStateType<ThinkTimingState>("ThinkTiming");
   AgentState::registerStateType<WorldFrameState>("WorldFrame");
 }
