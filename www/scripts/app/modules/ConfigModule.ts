cf1fb44805c70d54a356d8bdb75b663aaa418ee4
Drew Noakes <git@drewnoakes.com>
2014-02-26 21:37:42 +0000

    Migrate ConfigModule.

--- www/scripts/app/modules/ConfigModule.ts
+++ www/scripts/app/modules/ConfigModule.ts
@@ -1,68 +1,56 @@
 /**
  * @author Drew Noakes http://drewnoakes.com
  */
-define(
-    [
-        'ControlClient',
-        'ControlBuilder',
-        'DOMTemplate'
-    ],
-    function(ControlClient, ControlBuilder, DOMTemplate)
+
+import ControlClient = require('ControlClient');
+import ControlBuilder = require('ControlBuilder');
+import DOMTemplate = require('DOMTemplate');
+import Module = require('Module');
+
+var moduleTemplate = new DOMTemplate('config-module-template');
+
+class ConfigModule extends Module
+{
+    private textElement: HTMLDivElement;
+    private filter: HTMLInputElement;
+
+    constructor()
     {
-        'use strict';
-
-        var moduleTemplate = new DOMTemplate('config-module-template');
-
-        var ConfigModule = function()
-        {
-            this.container = document.createElement('div');
-
-            this.title = 'config';
-            this.id = 'config';
-            this.element = this.container;
-        };
-
-        ConfigModule.prototype.load = function()
-        {
-            var templateRoot = moduleTemplate.create();
-
-            this.container.appendChild(templateRoot);
-
-            this.textElement = this.container.querySelector('div.json-text');
-            console.assert(this.textElement);
-
-            this.subscription = ControlClient.onSettingChange(this.updateText.bind(this));
-
-            var header = templateRoot.querySelector('div.header');
-            console.assert(header);
-            ControlBuilder.actions('config', header);
-
-            this.filter = document.createElement('input');
-            this.filter.type = 'text';
-            this.filter.placeholder = 'Type to filter...';
-            this.filter.addEventListener('input', this.updateText.bind(this));
-            header.appendChild(this.filter);
-
-            this.updateText();
-        };
-
-        ConfigModule.prototype.unload = function()
-        {
-            while (this.container.hasChildNodes())
-                this.container.removeChild(this.container.lastChild);
-
-            delete this.textElement;
-
-            if (this.subscription)
-                this.subscription.close();
-        };
-
-        ConfigModule.prototype.updateText = function()
-        {
-            var matching = this.filter.value;
-            this.textElement.textContent = ControlClient.getConfigText(matching);
-        };
-
-        return ConfigModule;
+        super('config', 'config');
     }
-);
+
+    public load(element: HTMLDivElement)
+    {
+        var templateRoot = <HTMLElement>moduleTemplate.create();
+
+        element.appendChild(templateRoot);
+
+        this.textElement = <HTMLDivElement>templateRoot.querySelector('div.json-text');
+
+        this.closeables.add(ControlClient.onSettingChange(this.updateText.bind(this)));
+
+        var header = templateRoot.querySelector('div.header');
+        ControlBuilder.actions('config', header);
+
+        this.filter = document.createElement('input');
+        this.filter.type = 'text';
+        this.filter.placeholder = 'Type to filter...';
+        this.filter.addEventListener('input', this.updateText.bind(this));
+        header.appendChild(this.filter);
+
+        this.updateText();
+    }
+
+    public unload()
+    {
+        delete this.textElement;
+    }
+
+    private updateText()
+    {
+        var matching = this.filter.value;
+        this.textElement.textContent = ControlClient.getConfigText(matching);
+    }
+}
+
+export = ConfigModule;
