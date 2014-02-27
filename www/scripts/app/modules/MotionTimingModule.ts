0d6f42da12dcbdeaf6468fd3573392b356d4fde5
Drew Noakes <git@drewnoakes.com>
2014-02-27 00:34:01 +0000

    Migrate MotionTimingModule.

--- www/scripts/app/modules/MotionTimingModule.ts
+++ www/scripts/app/modules/MotionTimingModule.ts
@@ -1,41 +1,35 @@
 /**
  * @author Drew Noakes http://drewnoakes.com
  */
-define(
-    [
-        'modules/TimingPane',
-        'constants'
-    ],
-    function(TimingPane, constants)
+
+import constants = require('constants');
+import TimingPane = require('modules/TimingPane');
+import Module = require('Module');
+
+class MotionTimingModule extends Module
+{
+    private pane: TimingPane;
+
+    constructor()
     {
-        'use strict';
-
-        var MotionTimingModule = function()
-        {
-            this.pane = new TimingPane(constants.protocols.motionTiming, 125/*fps*/);
-
-            /////
-
-            this.title = 'motion timing';
-            this.id = 'motion-timing';
-            this.element = this.pane.container;
-        };
-
-        MotionTimingModule.prototype.load = function()
-        {
-            this.pane.load();
-        };
-
-        MotionTimingModule.prototype.unload = function()
-        {
-            this.pane.unload();
-        };
-
-        MotionTimingModule.prototype.onResized = function(width, height)
-        {
-            this.pane.onResized(width, height);
-        };
-
-        return MotionTimingModule;
+        super('motion-timing', 'motion timing');
+        this.pane = new TimingPane(constants.protocols.motionTiming, 125/*fps*/);
     }
-);
+
+    public load(element: HTMLDivElement)
+    {
+        this.pane.load(element);
+    }
+
+    public unload()
+    {
+        this.pane.unload();
+    }
+
+    public onResized(width, height)
+    {
+        this.pane.onResized(width, height);
+    }
+}
+
+export = MotionTimingModule;
