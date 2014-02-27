3181e54c8b767a57ad67ac310be50436b9f9276d
Drew Noakes <git@drewnoakes.com>
2014-02-27 00:33:54 +0000

    Migrate TimingPane.

--- www/scripts/app/modules/TimingPane.ts
+++ www/scripts/app/modules/TimingPane.ts
@@ -1,243 +1,283 @@
 /**
  * @author Drew Noakes http://drewnoakes.com
  */
-define(
-    [
-        'DataProxy',
-        'util/math'
-    ],
-    function (DataProxy, math)
+
+/// <reference path="../../libs/lodash.d.ts" />
+/// <reference path="../../libs/smoothie.d.ts" />
+
+import constants = require('constants');
+import DataProxy = require('DataProxy');
+import Closeable = require('util/Closeable');
+import math = require('util/math');
+import state = require('state');
+
+var seriesOptions = {
+    strokeStyle: 'rgb(0, 255, 0)',
+    fillStyle: 'rgba(0, 255, 0, 0.3)',
+    lineWidth: 1
+};
+
+var chartHeight = 150;
+
+interface ITimingEntry
+{
+    label: string;
+    avg: math.MovingAverage;
+    update: (timestamp: number, millis: number)=>void;
+    row: HTMLTableRowElement;
+    children: ITimingEntry[];
+    isExpanded: boolean;
+    time?: number;
+    millis?: number;
+    maxMillis?: number;
+}
+
+class TimingPane
+{
+    private container: HTMLDivElement;
+    private thresholdMillis: number;
+    private chart: SmoothieChart;
+    private canvas: HTMLCanvasElement;
+    private series: TimeSeries;
+    private table: HTMLTableElement;
+    private fpsInterval: number;
+    private fpsCount: number;
+    private lastCycleNumber: number;
+    private closeables: Closeable = new Closeable();
+    private entryByLabel: {[label:string]:ITimingEntry} = {};
+
+    constructor(private protocol: string, private targetFps: number)
     {
-        var seriesOptions = {
-            strokeStyle: 'rgb(0, 255, 0)',
-            fillStyle: 'rgba(0, 255, 0, 0.3)',
-            lineWidth: 1
+        this.container = document.createElement('div');
+        this.thresholdMillis = 1000 / targetFps;
+    }
+
+    public load(element: HTMLDivElement)
+    {
+        var chartOptions = {
+            interpolation: 'step',
+            grid: {
+                strokeStyle: 'rgb(40, 40, 40)',
+                fillStyle: 'rgb(0, 0, 0)',
+                lineWidth: 1,
+                millisPerLine: 250,
+                verticalSections: 6,
+                sharpLines: true,
+                borderVisible: false
+            },
+            labels: {
+                fillStyle: '#ffffff'
+            },
+            yRangeFunction: range => ({min: 0, max: Math.max(this.thresholdMillis, range.max)}),
+            horizontalLines: [
+                {color: '#FF0000', lineWidth: 1, value: this.thresholdMillis}
+            ]
         };
 
-        var chartHeight = 150;
+        this.chart = new SmoothieChart(chartOptions);
+        this.canvas = document.createElement('canvas');
+        this.canvas.height = chartHeight;
+        element.appendChild(this.canvas);
 
-        var TimingPane = function (protocol, targetFps)
-        {
-            this.$container = $('<div></div>');
-            this.container = this.$container.get(0);
-            this.protocol = protocol;
-            this.targetFps = targetFps;
-            this.thresholdMillis = 1000 / targetFps;
-        };
+        this.series = new TimeSeries();
+        this.chart.addTimeSeries(this.series, seriesOptions);
+        this.chart.streamTo(this.canvas, /*delayMs*/ 0);
 
-        TimingPane.prototype.load = function ()
+        var fps = document.createElement('div');
+        fps.className = 'fps';
+        element.appendChild(fps);
+
+        var reset = document.createElement('a');
+        reset.href = '#';
+        reset.className = 'reset';
+        reset.textContent = 'reset';
+        reset.addEventListener('click', () =>
         {
-            var chartOptions = {
-                interpolation: 'step',
-                grid: {
-                    strokeStyle: 'rgb(40, 40, 40)',
-                    fillStyle: 'rgb(0, 0, 0)',
-                    lineWidth: 1,
-                    millisPerLine: 250,
-                    verticalSections: 6,
-                    sharpLines: true,
-                    borderVisible: false
-                },
-                labels: {
-                    fillStyle: '#ffffff'
-                },
-                yRangeFunction: function (range)
+            this.resetMaximums();
+            return false;
+        });
+        element.appendChild(reset);
+
+        this.table = document.createElement('table');
+        this.table.className = 'timing-details';
+        element.appendChild(this.table);
+
+        this.entryByLabel = {};
+
+        this.closeables.add(DataProxy.subscribe(
+            this.protocol,
+            {
+                json: true,
+                onmessage: _.bind(this.onData, this)
+            }
+        ));
+
+        this.fpsInterval = setInterval(() =>
+        {
+            // TODO change colour based on performance vs this.targetFps
+            fps.textContent = this.fpsCount ? this.fpsCount + ' FPS' : '';
+            this.fpsCount = 0;
+        }, 1000);
+    }
+
+    public unload()
+    {
+        this.chart.stop();
+        clearInterval(this.fpsInterval);
+
+        this.closeables.closeAll();
+        delete this.fpsInterval;
+
+        delete this.series;
+        delete this.chart;
+        delete this.table;
+        delete this.canvas;
+        delete this.entryByLabel;
+    }
+
+    public onResized(width:number, height:number)
+    {
+        this.canvas.width = width;
+    }
+
+    private onData(data: state.Timing)
+    {
+        if (this.lastCycleNumber) {
+            this.fpsCount += data.cycle - this.lastCycleNumber;
+        }
+        this.lastCycleNumber = data.cycle;
+        var time = new Date().getTime();
+        var timings = data.timings;
+
+        _.each(_.keys(timings), key =>
+        {
+            var millis = <number>timings[key];
+            this.getOrCreateEntry(key).update(time, millis);
+        });
+
+        this.updateChart(time);
+    }
+
+    private updateChart(time)
+    {
+        var totalTime = 0;
+
+        _.each(_.values(this.entryByLabel), entry =>
+        {
+            if (entry.time === time && entry.label.indexOf('/') === -1)
+                totalTime += entry.millis;
+        });
+
+        this.series.append(time, totalTime);
+    }
+
+    private resetMaximums()
+    {
+        _.each(_.values(this.entryByLabel), entry => entry.maxMillis = 0);
+    }
+
+    private getOrCreateEntry(label: string): ITimingEntry
+    {
+        var entry = this.entryByLabel[label];
+
+        if (!entry) {
+            // If this entry is a child of another entry, try to find it's parent
+            var parts = label.split('/'),
+                hasParent = parts.length !== 1,
+                parent;
+
+            // Some messing around to get paths in a nice order, where parents appear above children
+            // Image Processing
+            // Image Processing/Pixel Label
+            // Image Processing/Pixel Label/Find Horizon
+            // Image Processing/Pixel Label/Pixels Above
+            for (var i = 1; i < parts.length; i++) {
+                var parentPath = parts.slice(0, i).join('/');
+                parent = this.getOrCreateEntry(parentPath);
+                parent.row.classList.add('parent');
+                if (i === 1)
+                    parent.row.classList.add('root');
+            }
+
+            var row = document.createElement('tr');
+
+            var cellExpand = document.createElement('td'),     // {'class': 'expander'}).appendTo(row).get(0),
+                cellLabel = document.createElement('td'),      //.text(label).appendTo(row).get(0),
+                cellMillis = document.createElement('td'),     // {'class': 'duration'}).appendTo(row).get(0),
+                cellMAvgMillis = document.createElement('td'), // {'class': 'avg-duration'}).appendTo(row).get(0),
+                cellMaxMillis = document.createElement('td');  // {'class': 'max-duration'}).appendTo(row).get(0);
+
+            cellExpand.className = 'expander';
+            cellLabel.textContent = label;
+            cellMillis.className = 'duration';
+            cellMAvgMillis.className = 'avg-duration';
+            cellMaxMillis.className = 'max-duration';
+
+            row.appendChild(cellExpand);
+            row.appendChild(cellLabel);
+            row.appendChild(cellMillis);
+            row.appendChild(cellMAvgMillis);
+            row.appendChild(cellMaxMillis);
+
+            entry = {
+                label: label,
+                avg: new math.MovingAverage(this.targetFps * 4), // four second moving average
+                update: (timestamp: number, millis: number) =>
                 {
-                    return {min: 0, max: Math.max(this.thresholdMillis, range.max)};
-                }.bind(this),
-                horizontalLines: [
-                    {color: '#FF0000', lineWidth: 1, value: this.thresholdMillis}
-                ]
+                    entry.time = timestamp;
+                    entry.millis = millis;
+                    if (!entry.maxMillis || entry.maxMillis < millis) {
+                        entry.maxMillis = millis;
+                        cellMaxMillis.textContent = millis.toFixed(3);
+                    }
+                    cellMillis.textContent = millis.toFixed(3);
+                    cellMAvgMillis.textContent = entry.avg.next(millis).toFixed(3);
+                },
+                row: row,
+                children: [],
+                isExpanded: false
             };
 
-            this.chart = new SmoothieChart(chartOptions);
-            this.canvas = document.createElement('canvas');
-            this.canvas.height = chartHeight;
-            this.$container.append(this.canvas);
-
-            this.series = new TimeSeries();
-            this.chart.addTimeSeries(this.series, seriesOptions);
-            this.chart.streamTo(this.canvas, /*delayMs*/ 0);
-
-            this.$fps = $('<div></div>', {'class': 'fps'}).appendTo(this.$container);
-            $('<a></a>', {'class': 'reset', href: '#'}).text('reset').click(function ()
+            var setChildRowDisplay = (e: ITimingEntry) =>
             {
-                this.resetMaximums();
-                return false;
-            }.bind(this)).appendTo(this.$container);
-
-            this.table = $('<table></table>', {'class': 'timing-details'}).appendTo(this.$container);
-
-            this.entryByLabel = {};
-
-            this.subscription = DataProxy.subscribe(
-                this.protocol,
+                var display = e.isExpanded ? 'table-row' : 'none';
+                _.each(e.children, child =>
                 {
-                    json: true,
-                    onmessage: _.bind(this.onData, this)
-                }
-            );
-
-            this.fpsInterval = setInterval(function ()
-            {
-                // TODO change colour based on performance vs this.targetFps
-                this.$fps.text(this.fpsCount ? this.fpsCount + ' FPS' : '');
-                this.fpsCount = 0;
-            }.bind(this), 1000);
-        };
-
-        TimingPane.prototype.unload = function ()
-        {
-            this.chart.stop();
-            this.$container.empty();
-            this.subscription.close();
-
-            clearInterval(this.fpsInterval);
-            delete this.fpsInterval;
-
-            delete this.series;
-            delete this.chart;
-            delete this.table;
-            delete this.canvas;
-            delete this.entryByLabel;
-            delete this.subscription;
-        };
-
-        TimingPane.prototype.onData = function (data)
-        {
-            if (this.lastCycleNumber) {
-                this.fpsCount += data.cycle - this.lastCycleNumber;
-            }
-            this.lastCycleNumber = data.cycle;
-            var time = new Date().getTime();
-            var timings = data.timings;
-
-            _.each(_.keys(timings), function (key)
-            {
-                var millis = timings[key];
-                this.getOrCreateEntry(key).update(time, millis);
-            }.bind(this));
-
-            this.updateChart(time);
-        };
-
-        TimingPane.prototype.onResized = function (width, height)
-        {
-            this.canvas.width = width;
-        };
-
-        TimingPane.prototype.updateChart = function (time)
-        {
-            var totalTime = 0;
-
-            _.each(_.values(this.entryByLabel), function (entry)
-            {
-                if (entry.time === time && entry.label.indexOf('/') === -1)
-                    totalTime += entry.millis;
-            });
-
-            this.series.append(time, totalTime);
-        };
-
-        TimingPane.prototype.resetMaximums = function ()
-        {
-            _.each(_.values(this.entryByLabel), function (entry)
-            {
-                entry.maxMillis = 0;
-            });
-        };
-
-        TimingPane.prototype.getOrCreateEntry = function (label)
-        {
-            var entry = this.entryByLabel[label];
-
-            if (!entry) {
-                // If this entry is a child of another entry, try to find it's parent
-                var parts = label.split('/'),
-                    hasParent = parts.length !== 1,
-                    parent;
-
-                // Some messing around to get paths in a nice order, where parents appear above children
-                // Image Processing
-                // Image Processing/Pixel Label
-                // Image Processing/Pixel Label/Find Horizon
-                // Image Processing/Pixel Label/Pixels Above
-                for (var i = 1; i < parts.length; i++) {
-                    var parentPath = parts.slice(0, i).join('/');
-                    parent = this.getOrCreateEntry(parentPath);
-                    parent.row.classList.add('parent');
-                    if (i === 1)
-                        parent.row.classList.add('root');
-                }
-
-                var row = $('<tr></tr>');
-
-                var cellExpand = $('<td></td>', {'class': 'expander'}).appendTo(row).get(0),
-                    cellLabel = $('<td></td>').text(label).appendTo(row).get(0),
-                    cellMillis = $('<td></td>', {'class': 'duration'}).appendTo(row).get(0),
-                    cellMAvgMillis = $('<td></td>', {'class': 'avg-duration'}).appendTo(row).get(0),
-                    cellMaxMillis = $('<td></td>', {'class': 'max-duration'}).appendTo(row).get(0);
-
-                entry = {
-                    label: label,
-                    avg: new math.MovingAverage(this.targetFps * 4), // four second moving average
-                    update: function (timestamp, millis)
-                    {
-                        entry.time = timestamp;
-                        entry.millis = millis;
-                        if (!entry.maxMillis || entry.maxMillis < millis) {
-                            entry.maxMillis = millis;
-                            cellMaxMillis.textContent = millis.toFixed(3);
-                        }
-                        cellMillis.textContent = millis.toFixed(3);
-                        cellMAvgMillis.textContent = entry.avg.next(millis).toFixed(3);
-                    },
-                    row: row.get(0),
-                    children: [],
-                    isExpanded: false
-                };
-
-                var setChildRowDisplay = function (e)
-                {
-                    var display = e.isExpanded ? 'table-row' : 'none';
-                    _.each(e.children, function (child)
-                    {
-                        child.row.style.display = display;
-                        setChildRowDisplay(child);
-                    });
-                };
-
-                var setRowExpansion = function (isExpanded)
-                {
-                    entry.isExpanded = isExpanded;
-
-                    if (isExpanded)
-                        entry.row.classList.add("expanded");
-                    else
-                        entry.row.classList.remove("expanded");
-                };
-
-                entry.row.addEventListener('click', function ()
-                {
-                    setRowExpansion(!entry.isExpanded);
-                    setChildRowDisplay(entry);
+                    child.row.style.display = display;
+                    setChildRowDisplay(child);
                 });
+            };
 
-                this.entryByLabel[label] = entry;
+            var setRowExpansion = isExpanded =>
+            {
+                entry.isExpanded = isExpanded;
 
-                if (hasParent) {
-                    parent.children.push(entry);
-                    entry.row.style.display = parent.isExpanded ? 'table-row' : 'none';
-                }
+                if (isExpanded)
+                    entry.row.classList.add("expanded");
+                else
+                    entry.row.classList.remove("expanded");
+            };
 
-                setRowExpansion(entry.isExpanded);
+            entry.row.addEventListener('click', () =>
+            {
+                setRowExpansion(!entry.isExpanded);
+                setChildRowDisplay(entry);
+            });
 
-                row.appendTo(this.table)
+            this.entryByLabel[label] = entry;
+
+            if (hasParent) {
+                parent.children.push(entry);
+                entry.row.style.display = parent.isExpanded ? 'table-row' : 'none';
             }
 
-            return entry;
-        };
+            setRowExpansion(entry.isExpanded);
 
-        return TimingPane;
+            this.table.appendChild(row);
+        }
+
+        return entry;
     }
-);
+}
+
+export = TimingPane;
