47b887bc52a587d09a121b92d5456ce88887f5a2
Drew Noakes <git@drewnoakes.com>
2014-03-03 22:16:15 +0000

    Formatting.

--- StateObject/ParticleState/writeJson.cc
+++ StateObject/ParticleState/writeJson.cc
@@ -6,22 +6,26 @@ using namespace rapidjson;
 void ParticleState::writeJson(Writer<StringBuffer>& writer) const
 {
   writer.StartObject();
-  writer.String("particles").StartArray();
   {
-    for (unsigned i = 0; i < d_particles.cols(); ++i)
+    writer.String("particles");
+    writer.StartArray();
     {
-      writer.StartArray();
+      for (unsigned i = 0; i < d_particles.cols(); ++i)
+      {
+        writer.StartArray();
 
-      auto particle = d_particles.col(i);
-      writer.Double(particle.x(), "%.3f"); // x
-      writer.Double(particle.y(), "%.3f"); // y
-      writer.Double(particle.z(), "%.3f"); // theta
-      writer.Double(isnan(particle.w()) ? 0 : particle.w()); // weight
+        auto particle = d_particles.col(i);
+        writer.Double(particle.x(), "%.3f"); // x
+        writer.Double(particle.y(), "%.3f"); // y
+        writer.Double(particle.z(), "%.3f"); // theta
+        writer.Double(isnan(particle.w()) ? 0 : particle.w()); // weight
 
-      writer.EndArray();
+        writer.EndArray();
+      }
     }
+    writer.EndArray();
+
+    writer.String("pnwsum").Double(d_preNormWeightSum);
   }
-  writer.EndArray();
-  writer.String("pnwsum").Double(d_preNormWeightSum);
   writer.EndObject();
 }
