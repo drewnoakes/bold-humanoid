edfb3ea9ce21b3ea923f10083e41d82a67aa2758
Drew Noakes <git@drewnoakes.com>
2014-03-03 20:43:17 +0000

    Use emplace_back instead of push_back.

--- Spatialiser/updateCameraToAgent.cc
+++ Spatialiser/updateCameraToAgent.cc
@@ -21,9 +21,7 @@ void Spatialiser::updateCameraToAgent()
   {
     auto const& pos3d = findGroundPointForPixel(goal);
     if (pos3d.hasValue())
-    {
-      goals.push_back(*pos3d);
-    }
+      goals.emplace_back(*pos3d);
   }
 
   for (LineSegment2i const& lineSegment : cameraFrame->getObservedLineSegments())
@@ -31,9 +29,7 @@ void Spatialiser::updateCameraToAgent()
     auto const& p1 = findGroundPointForPixel(lineSegment.p1().cast<double>() + Vector2d(0.5,0.5));
     auto const& p2 = findGroundPointForPixel(lineSegment.p2().cast<double>() + Vector2d(0.5,0.5));
     if (p1.hasValue() && p2.hasValue())
-    {
-      lineSegments.push_back(LineSegment3d(*p1, *p2));
-    }
+      lineSegments.emplace_back(*p1, *p2);
   }
 
   //
@@ -58,24 +54,24 @@ void Spatialiser::updateCameraToAgent()
 
   auto const& p1 = findGroundPointForPixel(Vector2d(0, 0) + Vector2d(0.5,0.5));
   if (p1)
-    vertices.push_back(p1->head<2>());
+    vertices.emplace_back(p1->head<2>());
 
   auto const& p2 = findGroundPointForPixel(Vector2d(width - 3, 0) + Vector2d(0.5,0.5));
   if (p2)
-    vertices.push_back(p2->head<2>());
+    vertices.emplace_back(p2->head<2>());
 
   if (horiz2 >= 0 && horiz2 < height)
   {
     auto const& p3 = findGroundPointForPixel(Vector2d(width - 3, horiz2) + Vector2d(0.5,0.5));
     if (p3)
-      vertices.push_back(p3->head<2>());
+      vertices.emplace_back(p3->head<2>());
   }
 
   if (horiz1 >= 0 && horiz1 < height)
   {
     auto const& p4 = findGroundPointForPixel(Vector2d(0, horiz1) + Vector2d(0.5,0.5));
     if (p4)
-      vertices.push_back(p4->head<2>());
+      vertices.emplace_back(p4->head<2>());
   }
 
   Maybe<Polygon2d> visibleFieldPoly = vertices.size() == 4 ? Maybe<Polygon2d>(Polygon2d(vertices)) : Maybe<Polygon2d>::empty();
