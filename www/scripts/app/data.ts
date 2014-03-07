e6a899ec522faa0bd2ff8c0820f0438685796909
Drew Noakes <git@drewnoakes.com>
2014-03-07 10:17:38 +0000

    Refactor code around WebSockets and subscriptions.
    Much clearer now. Have an object for the Protocol instead of many maps and string keys.

--- www/scripts/app/data.ts
+++ www/scripts/app/data.ts
@@ -4,59 +4,17 @@
 
 import Settings = require('Settings');
 
-var indicatorByProtocol = {};
-var socketByProtocol: {[protocol:string]:WebSocket} = {};
-
 declare class MozWebSocket
 {
     constructor(url: string, protocol: string);
 }
 
+// GLOBAL STATE
+
 var elementContainer = document.querySelector('#connections .indicators');
+var protocolByName: {[name: string]: Protocol} = {};
 
-function openWebSocket(protocol: string) : WebSocket
-{
-    if (!protocol || protocol === '')
-        throw new Error("Invalid protocol: " + protocol);
-    if (socketByProtocol[protocol] !== undefined)
-        throw new Error("WebSocket already open for protocol: " + protocol);
-
-    var socket: WebSocket = typeof MozWebSocket !== 'undefined'
-        ? <WebSocket>new MozWebSocket(Settings.webSocketUrl, protocol)
-        : new WebSocket(Settings.webSocketUrl, protocol);
-
-    socketByProtocol[protocol] = socket;
-
-    // Reuse the indicator, in case we are re-connecting
-    var connectionIndicator = indicatorByProtocol[protocol];
-    if (!connectionIndicator) {
-        connectionIndicator = document.createElement('div');
-        connectionIndicator.title = protocol;
-        elementContainer.appendChild(connectionIndicator);
-        indicatorByProtocol[protocol] = connectionIndicator;
-    }
-
-    connectionIndicator.className = 'connection-indicator connecting';
-
-    socket.onopen = () => connectionIndicator.className = 'connection-indicator connected';
-    socket.onclose = () => connectionIndicator.className = 'connection-indicator disconnected';
-    socket.onerror = e => connectionIndicator.className = 'connection-indicator error';
-
-    return socket;
-}
-
-function closeWebSocket(protocol: string): void
-{
-    socketByProtocol[protocol].close();
-    delete socketByProtocol[protocol];
-    indicatorByProtocol[protocol].remove();
-    delete indicatorByProtocol[protocol];
-}
-
-export function disconnectAll()
-{
-    _.each(_.values(socketByProtocol), socket => socket.close());
-}
+// CLIENT
 
 interface IClient
 {
@@ -65,42 +23,135 @@ interface IClient
     onerror?: (err: ErrorEvent)=>void;
 }
 
-interface IProtocol
-{
-    name: string;
-    clients: IClient[];
-    socket: WebSocket;
-}
+// PROTOCOL
 
-var proxyEvent = (protocol: IProtocol, eventName: string, unsubscribe: ()=>void) =>
+class Protocol
 {
-    return msg =>
+    public clients: IClient[] = [];
+    public socket: WebSocket;
+    private indicator: HTMLDivElement;
+
+    constructor(public protocolName: string)
     {
-        var parsed;
-        for (var i = 0; i < protocol.clients.length; i++) {
-            var client = protocol.clients[i];
-            var callback = client[eventName];
-            console.assert(typeof(callback) === 'function');
-            try {
-                if (client.parseJson) {
-                    if (!parsed && msg.data) {
-                        parsed = JSON.parse(msg.data);
-                    }
-                    callback(parsed);
-                } else {
-                    // TODO use arguments instead of 'msg'?
-                    callback(msg);
+        console.assert(!protocolName[protocolName]);
+
+        this.indicator = document.createElement('div');
+        this.indicator.title = protocolName;
+        this.indicator.className = 'connection-indicator connecting';
+
+        elementContainer.appendChild(this.indicator);
+    }
+
+    public addClient(client: IClient)
+    {
+        if (this.clients.length === 0)
+        {
+            // This is the first client, so open the socket
+            this.createSocket();
+        }
+
+        this.clients.push(client);
+        this.indicator.title = this.protocolName + ' (' + this.clients.length + ' client' + (this.clients.length==1?')':'s)');
+    }
+
+    private createSocket()
+    {
+        if (this.socket)
+        {
+            console.assert(this.socket.readyState === WebSocket.CLOSED);
+            this.socket.close();
+        }
+
+        this.socket = typeof MozWebSocket !== 'undefined'
+            ? <WebSocket>new MozWebSocket(Settings.webSocketUrl, this.protocolName)
+            : new WebSocket(Settings.webSocketUrl, this.protocolName);
+
+        // Wire up the indicator
+        this.indicator.className = 'connection-indicator connecting';
+        this.socket.onopen = () => this.indicator.className = 'connection-indicator connected';
+        this.socket.onclose = () => this.indicator.className = 'connection-indicator disconnected';
+        this.socket.onerror = e => this.indicator.className = 'connection-indicator error';
+
+        // The callback logic for errors and messages is very similar.
+        // In both cases, just proxy the argument onwards.
+        this.socket.onerror = this.proxyEventToClients('onerror');
+        this.socket.onmessage = this.proxyEventToClients('onmessage');
+    }
+
+    public removeClient(client: IClient)
+    {
+        // Find the client
+        for (var i = 0; i < this.clients.length; i++)
+        {
+            if (this.clients[i] === client)
+            {
+                // Remove the client
+                this.clients.splice(i, 1);
+
+                if (this.clients.length === 0)
+                {
+                    // No one is using this socket any more, so close it down
+                    this.socket.close();
+                    delete this.socket;
                 }
-            } catch(ex) {
-                console.error(protocol.name, 'subscription handler raised an error. Forcing unsubscribe.', ex);
-                unsubscribe();
-                throw ex;
+
+                break;
             }
         }
     }
-};
+    public disconnect()
+    {
+        this.socket.close();
+    }
 
-var dataByProtocol: {[name: string]: IProtocol} = {};
+    public reconnect()
+    {
+        if (this.socket.readyState === WebSocket.CLOSED)
+        {
+           this.createSocket();
+        }
+    }
+
+    private proxyEventToClients(eventName: string): (arg:any)=>void
+    {
+        return msg =>
+        {
+            var parsed;
+            for (var i = 0; i < this.clients.length; i++) {
+                var client = this.clients[i];
+                var callback = client[eventName];
+                console.assert(typeof(callback) === 'function');
+                try {
+                    if (client.parseJson) {
+                        if (!parsed && msg.data) {
+                            parsed = JSON.parse(msg.data);
+                        }
+                        callback(parsed);
+                    } else {
+                        // TODO use arguments instead of 'msg'?
+                        callback(msg);
+                    }
+                } catch(ex) {
+                    console.error(this.protocolName, 'subscription handler raised an error. Closing WebSocket.', ex);
+                    this.socket.close();
+                    throw ex;
+                }
+            }
+        }
+    }
+}
+
+// EXPORTS
+
+export function disconnectAll()
+{
+    _.each<Protocol>(_.values(protocolByName), protocol => protocol.disconnect());
+}
+
+export function reconnectAll()
+{
+    _.each<Protocol>(_.values(protocolByName), protocol => protocol.reconnect());
+}
 
 export interface ISubscriptionOptions<TData>
 {
@@ -109,25 +160,13 @@ export interface ISubscriptionOptions<TData>
     onerror?: (data: ErrorEvent)=>void;
     parseJson?: boolean;
 }
-
 export class Subscription<TData>
 {
-    private protocol: IProtocol;
+    private protocol: Protocol;
     private client: IClient;
 
-    constructor(private protocolName: string, private options: ISubscriptionOptions<TData>)
+    constructor(protocolName: string, options: ISubscriptionOptions<TData>)
     {
-        this.protocol = dataByProtocol[protocolName];
-
-        if (!this.protocol) {
-            // First client of this protocol
-            var socket = openWebSocket(protocolName);
-            this.protocol = { name: protocolName, clients: [], socket: socket };
-            dataByProtocol[protocolName] = this.protocol;
-            this.protocol.socket.onmessage = proxyEvent(this.protocol, 'onmessage', () => this.close());
-            this.protocol.socket.onerror = proxyEvent(this.protocol, 'onerror', () => this.close());
-        }
-
         this.client = {
             // default to 'true'
             parseJson: typeof(options.parseJson) === 'boolean' ? options.parseJson : true,
@@ -135,7 +174,13 @@ export class Subscription<TData>
             onerror: options.onerror
         };
 
-        this.protocol.clients.push(this.client);
+        // Look up or create Protocol
+        this.protocol = protocolByName[protocolName];
+        if (!this.protocol)
+            this.protocol = protocolByName[protocolName] = new Protocol(protocolName);
+
+        // Add another client
+        this.protocol.addClient(this.client);
 
         // Ensure our functions are always called with this instance.
         this.send = this.send.bind(this);
@@ -149,24 +194,6 @@ export class Subscription<TData>
 
     public close()
     {
-        // Search for our client
-        for (var i = 0; i < this.protocol.clients.length; i++)
-        {
-            if (this.protocol.clients[i] === this.client)
-            {
-                // Remove this client
-                this.protocol.clients.splice(i, 1);
-
-                if (this.protocol.clients.length === 0)
-                {
-                    // No one is using this socket any more, so close it down
-                    closeWebSocket(this.protocol.name);
-
-                    delete dataByProtocol[this.protocol.name];
-                }
-
-                break;
-            }
-        }
+        this.protocol.removeClient(this.client);
     }
 }
