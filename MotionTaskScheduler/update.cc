6062c67a49d26babd4aee256d9cfbffdd7f858db
Drew Noakes <git@drewnoakes.com>
2014-03-15 16:36:50 +0000

    Whitespace.

--- MotionTaskScheduler/update.cc
+++ MotionTaskScheduler/update.cc
@@ -12,13 +12,13 @@ void MotionTaskScheduler::update()
       // Remove any committed tasks for which the corresponding module has completed
       auto it1 = d_tasks.erase(
         remove_if(
-          d_tasks.begin(), 
+          d_tasks.begin(),
           d_tasks.end(),
           [module](shared_ptr<MotionTask> const& task) { return task->isCommitted() && task->getModule() == module; }
         ),
         d_tasks.end()
       );
-      
+
       if (it1 != d_tasks.end())
       {
         // Something was removed
@@ -26,17 +26,17 @@ void MotionTaskScheduler::update()
       }
     }
   }
-  
+
   if (!d_hasChange)
     return;
 
   // Determine which tasks are assigned which body sections
   auto moduleJointSelection = make_shared<vector<pair<MotionModule*, shared_ptr<JointSelection>>>>();
-  
+
   vector<shared_ptr<MotionTask>> headTasks;
   vector<shared_ptr<MotionTask>> armTasks;
   vector<shared_ptr<MotionTask>> legTasks;
-  
+
   for (shared_ptr<MotionTask> task : d_tasks)
   {
     if (task->getSection() == SectionId::Head)
@@ -46,36 +46,36 @@ void MotionTaskScheduler::update()
     if (task->getSection() == SectionId::Legs)
       legTasks.push_back(task);
   }
-  
+
   auto sortTasks = [](vector<shared_ptr<MotionTask>>& tasks)
   {
     std::stable_sort(
-      tasks.begin(), 
+      tasks.begin(),
       tasks.end(),
       [](shared_ptr<MotionTask> a, shared_ptr<MotionTask> b)
       {
         // Committed first
         if (a->isCommitted() && !b->isCommitted())
           return true;
-        
+
         // Sort larger priorities first
         return a->getPriority() >= b->getPriority();
       }
     );
   };
-  
+
   sortTasks(headTasks);
   sortTasks(armTasks);
   sortTasks(legTasks);
-  
+
   shared_ptr<MotionTask> headTask = headTasks.size() ? headTasks[0] : nullptr;
   shared_ptr<MotionTask> armTask  = armTasks.size()  ? armTasks[0] : nullptr;
   shared_ptr<MotionTask> legTask  = legTasks.size()  ? legTasks[0] : nullptr;
-  
+
   MotionModule* headModule = headTask ? headTask->getModule() : nullptr;
   MotionModule* armModule = armTask ? armTask->getModule() : nullptr;
   MotionModule* legModule = legTask ? legTask->getModule() : nullptr;
-  
+
   // This is a bit ugly, but I cannot think of a simpler way of grouping them in c++
   if (headModule == armModule && armModule == legModule)
   {
@@ -107,12 +107,12 @@ void MotionTaskScheduler::update()
     if (armModule)  moduleJointSelection->push_back(make_pair(armModule,  JointSelection::arms()));
     if (legModule)  moduleJointSelection->push_back(make_pair(legModule,  JointSelection::legs()));
   }
-  
+
   // Commit selected tasks if they require it
   if (headTask && headTask->isCommitRequested()) headTask->setCommitted();
   if (armTask  && armTask->isCommitRequested())  armTask->setCommitted();
   if (legTask  && legTask->isCommitRequested())  legTask->setCommitted();
-  
+
   // Generate motion task state
   State::make<MotionTaskState>(moduleJointSelection, headTasks, armTasks, legTasks);
 
@@ -123,12 +123,12 @@ void MotionTaskScheduler::update()
   // for the next think cycle.
   auto it2 = d_tasks.erase(
     remove_if(
-      d_tasks.begin(), 
+      d_tasks.begin(),
       d_tasks.end(),
       [](shared_ptr<MotionTask> task) { return !task->isCommitted(); }
     ),
     d_tasks.end()
   );
-  
+
   d_hasChange = it2 == d_tasks.end();
 }
