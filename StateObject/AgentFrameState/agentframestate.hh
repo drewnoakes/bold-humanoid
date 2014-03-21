54adb954abb4b429bc98ba8fa16255f7a48a3b53
Sander van Dijk <sgvandijk@gmail.com>
2014-03-21 08:43:59 +0000

    AgentFrameState hold junctions

--- StateObject/AgentFrameState/agentframestate.hh
+++ StateObject/AgentFrameState/agentframestate.hh
@@ -8,6 +8,7 @@
 #include "../../util/Maybe.hh"
 #include "../../geometry/LineSegment/linesegment.hh"
 #include "../../geometry/Polygon2.hh"
+#include "../../LineJunctionFinder/linejunctionfinder.hh"
 
 namespace bold
 {
@@ -17,6 +18,7 @@ namespace bold
     AgentFrameState(Maybe<Eigen::Vector3d> ballObservation,
                     std::vector<Eigen::Vector3d> goalObservations,
                     std::vector<LineSegment3d> observedLineSegments,
+                    std::vector<std::pair<Eigen::Vector2d, LineJunctionFinder::JunctionType>> observedLineJunctions,
                     Maybe<Polygon2d> visibleFieldPoly,
                     std::vector<std::pair<Eigen::Vector3d,Eigen::Vector3d>> occlusionRays,
                     ulong thinkCycleNumber)
@@ -43,6 +45,7 @@ namespace bold
     Maybe<Eigen::Vector3d> d_ballObservation;
     std::vector<Eigen::Vector3d> d_goalObservations;
     std::vector<LineSegment3d> d_observedLineSegments;
+    std::vector<std::pair<Eigen::Vector2d,LineJunctionFinder::JunctionType>> d_observerdLineJunctions;
     Maybe<Polygon2d> d_visibleFieldPoly;
     std::vector<std::pair<Eigen::Vector3d,Eigen::Vector3d>> d_occlusionRays;
     ulong d_thinkCycleNumber;
