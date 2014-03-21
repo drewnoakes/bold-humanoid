f9f0484308f728d74ffef237a94bb264ef113666
Sander van Dijk <sgvandijk@gmail.com>
2014-03-21 17:51:24 +0000

    Fix aligned allocate shared using wrong type for allocator template param

--- util/memory.hh
+++ util/memory.hh
@@ -11,7 +11,7 @@ namespace bold
   template<typename T, typename... Args>
   std::shared_ptr<T> allocate_aligned_shared(Args&&... args)
   {
-    typedef std::remove_const<T> NonConst;
+    typedef typename std::remove_const<T>::type NonConst;
     return std::allocate_shared<T>(Eigen::aligned_allocator<NonConst>(), std::forward<Args>(args)...);
   }
 #else
