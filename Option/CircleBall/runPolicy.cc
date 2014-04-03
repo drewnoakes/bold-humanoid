9e6b6abc836ebef5b358ceb7bc31abc4508577de
Drew Noakes <git@drewnoakes.com>
2014-04-03 10:45:27 +0200

    Circle ball improvements.
    - Modifies turn speed based upon translational error
    - Adds x/y speed needed when turning to convert outward turn to inward turn

--- Option/CircleBall/runPolicy.cc
+++ Option/CircleBall/runPolicy.cc
@@ -15,30 +15,53 @@ vector<shared_ptr<Option>> CircleBall::runPolicy(Writer<StringBuffer>& writer)
     return { d_lookAtFeet };
   }
 
-  // bias towards the foot that will be closest in turn
-  // value represents the bias in the foot
+  static Setting<double>* maxSpeedX = Config::getSetting<double>("options.circle-ball.max-speed-x");
+  static Setting<double>* maxSpeedY = Config::getSetting<double>("options.circle-ball.max-speed-y");
+  static Setting<double>* pGainX = Config::getSetting<double>("options.circle-ball.p-gain-x");
+  static Setting<double>* pGainY = Config::getSetting<double>("options.circle-ball.p-gain-y");
+  static Setting<double>* turnSpeedX = Config::getSetting<double>("options.circle-ball.turn-speed-x");
+  static Setting<double>* turnSpeedY = Config::getSetting<double>("options.circle-ball.turn-speed-y");
+  static Setting<double>* turnSpeedA = Config::getSetting<double>("options.circle-ball.turn-speed-a");
+  static Setting<double>* brakeDistance = Config::getSetting<double>("options.circle-ball.brake-distance");
+
+  // TODO set this position based upon which foot is closest to the ball when commencing
   auto targetBallPos = Vector2d(0.1, 0.25);
   auto observedBallPos = agentFrame->getBallObservation()->head<2>();
   Vector2d error = observedBallPos - targetBallPos;
 
-  static Setting<double>* maxSpeedX = Config::getSetting<double>("options.circle-ball.max-speed-x");
-  static Setting<double>* maxSpeedY = Config::getSetting<double>("options.circle-ball.max-speed-y");
-  static Setting<double>* turnSpeed = Config::getSetting<double>("options.circle-ball.turn-speed");
-  static Setting<double>* pGainX = Config::getSetting<double>("options.circle-ball.p-gain-x");
-  static Setting<double>* pGainY = Config::getSetting<double>("options.circle-ball.p-gain-y");
+  // Alpha controls how much turning is attempted. Max turn occurs when we have
+  // no positional error for the ball.
+  // If the error is greater than the brake distance, then turning is disabled
+  // to allow the position to be corrected.
+  // Value is linearly interpolated as a ratio of error length to brake distance.
+  double alpha = Math::clamp(1.0 - (error.norm()/brakeDistance->getValue()), 0.0, 1.0);
 
-  Vector2d control(
-    Math::clamp(error.x() * pGainX->getValue(), -maxSpeedX->getValue(), maxSpeedX->getValue()),
-    Math::clamp(error.y() * pGainY->getValue(), -maxSpeedY->getValue(), maxSpeedY->getValue())
-  );
+  // Set movement speed in x/y based on error distance
+  double x = error.x() * pGainX->getValue();
+  double y = error.y() * pGainY->getValue();
 
-  double a = d_isLeftTurn ? -turnSpeed->getValue() : turnSpeed->getValue();
+  // Add turn movement, based upon ratio of error
+  x += alpha * turnSpeedX->getValue();
+  y += alpha * turnSpeedY->getValue();
 
-  d_ambulator->setMoveDir(Vector2d(control.y(), control.x())); //  NOTE x and y intentionally swapped
+  // Blend turn speed
+  double a = alpha * (d_isLeftTurn ? -turnSpeedA->getValue() : turnSpeedA->getValue());
+
+  // Clamp movement direction to within maximums
+  x = Math::clamp(x, -maxSpeedX->getValue(), maxSpeedX->getValue());
+  y = Math::clamp(y, -maxSpeedY->getValue(), maxSpeedY->getValue());
+
+  cout << "ERR " << error.x() << ", " << error.y()
+       << " CTL " << x << ", " << y
+       << " ALPHA " << alpha
+       << endl;
+
+  d_ambulator->setMoveDir(Vector2d(y, x)); // NOTE x and y intentionally swapped
   d_ambulator->setTurnAngle(a);
 
-//   writer.String("moveDir").StartArray().Double(x).Double(y).EndArray(2);
-//   writer.String("turn").Double(a);
+  writer.String("error").StartArray().Double(error.x()).Double(error.y()).EndArray(2);
+  writer.String("moveDir").StartArray().Double(x).Double(y).EndArray(2);
+  writer.String("turn").Double(a);
 
   // Look at ball to make sure we don't lose track of it
   return { d_lookAtBall };
