0f14141289581822f7e8cfaa84489e83348f6bdd
Sander van Dijk <sgvandijk@gmail.com>
2014-04-04 12:19:08 +0100

    Write odometry state matrix json nicer

--- StateObject/OdometryState/writeJson.cc
+++ StateObject/OdometryState/writeJson.cc
@@ -8,11 +8,14 @@ void OdometryState::writeJson(Writer<StringBuffer>& writer) const
 {
   writer.StartObject();
   {
-    writer.String("translation");
+    writer.String("tr");
     writer.StartArray();
-    for (unsigned i = 0; i < 4; ++i)
-      for (unsigned j = 0; j < 4; ++j)
-        writer.Double(d_transform.matrix()(i, j));
+    for (unsigned j = 0; j < 4; ++j)
+    {
+      for (unsigned i = 0; i < 4; ++i)
+        writer.Double(d_transform.matrix()(i, j), "%.3f");
+      writer.String(" ");
+    }
     writer.EndArray();
   }
   writer.EndObject();
