36d60b805ab7ab217ac17eb0951e85e5c085a94c
Drew Noakes <git@drewnoakes.com>
2014-04-05 10:31:52 +0200

    Approach ball results in ball in front of foot, not midline.

--- Option/ApproachBall/runPolicy.cc
+++ Option/ApproachBall/runPolicy.cc
@@ -39,7 +39,7 @@ vector<shared_ptr<Option>> ApproachBall::runPolicy(Writer<StringBuffer>& writer)
   writer.String("distSpeed").Double(speedDueToDistance);
 
   // NOTE atan2 has flipped x/y on purpose
-  double ballAngleRads = -atan2(ballPos->x(), ballPos->y());
+  double ballAngleRads = -atan2(ballPos->x() - 0.04, ballPos->y());
 
   double speedScaleDueToAngle = Math::lerp(fabs(ballAngleRads), Math::degToRad(d_lowerTurnLimitDegs->getValue()), Math::degToRad(d_upperTurnLimitDegs->getValue()), 1.0, 0.0);
 
