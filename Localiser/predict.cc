05a4b89d700a47362aa5aff9fac5656fcbcd5ee0
Sander van Dijk <sgvandijk@gmail.com>
2014-04-06 21:55:03 +0100

    Localizer uses new odomoetry transformation in prediction

--- Localiser/predict.cc
+++ Localiser/predict.cc
@@ -9,49 +9,41 @@ void Localiser::predict()
 
   if (d_haveLastAgentTransform)
   {
+    // Particle represents WA
+    // Predict new A': WA' = WA * AA'
     auto deltaAgentTransform = odometryState->getTransform() * d_lastAgentTransform.inverse();
 
-    auto deltaAgentMat = deltaAgentTransform.matrix();
+    auto deltaAgentMat4 = deltaAgentTransform.matrix();
+    Matrix3d deltaAgentMat;
+    deltaAgentMat <<
+      deltaAgentMat4.block<2,2>(0,0) , deltaAgentMat4.col(3).head<2>(),
+      0, 0, 1;
 
-    d_filter->predict([deltaAgentMat,this](FilterState const& state) -> FilterState
-                      {
-                        FilterState newState;
-                        newState <<
-                          (deltaAgentMat.block<2,2>(0,0) * state.head<2>() + deltaAgentMat.col(3).head<2>()),
-                          deltaAgentMat.block<2,2>(0,0) * state.tail<2>();
-
-                        newState(0) += d_positionErrorRng();
-                        newState(1) += d_positionErrorRng();
-
-                        auto theta = atan2(newState(3), newState(2));
-                        theta += d_angleErrorRng();
-                        newState(3) = sin(theta);
-                        newState(2) = cos(theta);
-                        return newState;
-                      });
-    // New state:
-    // Location update: 
-    /*
-    auto deltaQuaternion = orientationState->getQuaternion() * d_lastQuaternion.inverse();
-    auto transformMatrix = deltaQuaternion.matrix();
-
-//     auto rotationTransform = AngleAxisd(deltaTheta, Vector3d(0,0,1));
-    auto agentTranslation = odometryState->getTransform().translation() - d_lastTranslation;
-
-    d_filter->predict([deltaTheta,&agentTranslation,this](Vector3d const& state) -> Vector3d
-                      {
-                        AgentPosition pos(state);
-                        auto worldAgentTransform = pos.worldAgentTransform();
-
-                        auto worldTranslation = worldAgentTransform.rotation() * agentTranslation;
-
-                        return Vector3d(
-                          state.x() + worldTranslation.x() + d_positionErrorRng(),
-                          state.y() + worldTranslation.y() + d_positionErrorRng(),
-                          state(2) + deltaTheta + d_angleErrorRng());
-                     });
-    */
-
+    d_filter->predict(
+      [deltaAgentMat,this](FilterState const& state) -> Filter::State
+      {
+        Matrix3d worldAgentMat;
+        worldAgentMat <<
+          state(3), state(2), state.x(),
+          -state(2), state(3), state.y(),
+          0        , 0       , 1;
+        
+        
+        auto newWorldAgentMat = worldAgentMat * deltaAgentMat;
+        
+        FilterState newState;
+        newState <<
+          newWorldAgentMat.col(2).head<2>(), newWorldAgentMat.col(1).head<2>();
+        
+        newState(0) += d_positionErrorRng();
+        newState(1) += d_positionErrorRng();
+        
+        auto theta = atan2(newState(3), newState(2));
+        theta += d_angleErrorRng();
+        newState(3) = sin(theta);
+        newState(2) = cos(theta);
+        return newState;
+      });
   }
 
   d_lastAgentTransform = odometryState->getTransform();
