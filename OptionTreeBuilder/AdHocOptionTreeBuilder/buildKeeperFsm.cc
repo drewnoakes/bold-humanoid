7f731788f926b7685c44e0775e880ec9cfe5cd8c
Sander van Dijk <sgvandijk@gmail.com>
2014-04-10 02:59:34 +0100

    Keeper clears ball if right in front

--- OptionTreeBuilder/AdHocOptionTreeBuilder/buildKeeperFsm.cc
+++ OptionTreeBuilder/AdHocOptionTreeBuilder/buildKeeperFsm.cc
@@ -8,6 +8,8 @@ shared_ptr<FSMOption> AdHocOptionTreeBuilder::buildKeeperFsm(Agent* agent, share
   auto lookAtBall = make_shared<LookAtBall>("lookAtBall", agent->getCameraModel(), agent->getHeadModule());
   auto bigStepLeft = make_shared<MotionScriptOption>("bigStepLeft", agent->getMotionScriptModule(), "./motionscripts/step-left-big.json");
   auto bigStepRight = make_shared<MotionScriptOption>("bigStepRight", agent->getMotionScriptModule(), "./motionscripts/step-right-big.json");
+  auto leftSideKick = make_shared<MotionScriptOption>("leftSideKickScript", agent->getMotionScriptModule(), "./motionscripts/kick-side-left.json");
+  auto rightSideKick = make_shared<MotionScriptOption>("rightSideKickScript", agent->getMotionScriptModule(), "./motionscripts/kick-side-right.json");
 
   auto fsm = tree->addOption(make_shared<FSMOption>(agent->getVoice(), "keeper"));
 
@@ -19,6 +21,8 @@ shared_ptr<FSMOption> AdHocOptionTreeBuilder::buildKeeperFsm(Agent* agent, share
   auto lookAtBallState = fsm->newState("lookAtBall", {stopWalking, lookAtBall});
   auto bigStepLeftState = fsm->newState("bigStepLeft", {bigStepLeft});
   auto bigStepRightState = fsm->newState("bigStepRight", {bigStepRight});
+  auto leftSideKickState = fsm->newState("leftSideKick", {leftSideKick});
+  auto rightSideKickState = fsm->newState("rightSideKick", {rightSideKick});
 
   setPlayerActivityInStates(agent, PlayerActivity::Waiting, { standUpState, lookForBallState, lookForBallState, lookAtBallState, bigStepLeftState, bigStepRightState });
 
@@ -61,6 +65,30 @@ shared_ptr<FSMOption> AdHocOptionTreeBuilder::buildKeeperFsm(Agent* agent, share
     ->whenTerminated();
 
   bigStepRightState
+    ->transitionTo(lookForBallState, "done")
+    ->whenTerminated();
+
+  lookAtBallState
+    ->transitionTo(leftSideKickState, "clear-left")
+    ->when([]()
+           {
+             auto ball = State::get<AgentFrameState>()->getBallObservation();
+             return ball && Range<double>(0.0, 0.17).contains(ball->y()) && Range<double>(-0.2, 0).contains(ball->x());
+           });
+
+  lookAtBallState
+    ->transitionTo(rightSideKickState, "clear-right")
+    ->when([]()
+           {
+             auto ball = State::get<AgentFrameState>()->getBallObservation();
+             return ball && Range<double>(0.0, 0.17).contains(ball->y()) && Range<double>(0, 0.2).contains(ball->x());
+           });
+
+  leftSideKickState
+    ->transitionTo(lookForBallState, "done")
+    ->whenTerminated();
+
+  rightSideKickState
     ->transitionTo(lookForBallState, "done")
     ->whenTerminated();
 
