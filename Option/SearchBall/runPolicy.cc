926ad079a4f93039c4dc5769c9cb01ce6962226f
Drew Noakes <git@drewnoakes.com>
2014-05-04 13:18:16 +0100

    Formatting and variable names.

--- Option/SearchBall/runPolicy.cc
+++ Option/SearchBall/runPolicy.cc
@@ -14,21 +14,21 @@ vector<shared_ptr<Option>> SearchBall::runPolicy(Writer<StringBuffer>& writer)
   double currentPanAngleDegs = Math::radToDeg(body->getJoint(JointId::HEAD_PAN)->angleRads);
   double currentTiltAngleDegs = Math::radToDeg(body->getJoint(JointId::HEAD_TILT)->angleRads);
 
-  static Setting<double>* turnSpeed = Config::getSetting<double>("options.search-ball.turn-speed");
-  static Setting<double>* d_topAngle = Config::getSetting<double>("options.search-ball.max-target-height");
-  static Setting<double>* d_bottomAngle = Config::getSetting<double>("options.search-ball.min-target-height");
-  static Setting<double>* d_sideAngle = Config::getSetting<double>("options.search-ball.max-target-side");
-  static Setting<double>* d_speedX = Config::getSetting<double>("options.search-ball.speed-x");
-  static Setting<double>* d_speedY = Config::getSetting<double>("options.search-ball.speed-y");
+  static Setting<double>* turnSpeedSetting = Config::getSetting<double>("options.search-ball.turn-speed");
+  static Setting<double>* maxTargetHeightSetting = Config::getSetting<double>("options.search-ball.max-target-height");
+  static Setting<double>* minTargetHeightSetting = Config::getSetting<double>("options.search-ball.min-target-height");
+  static Setting<double>* maxTargetSideSetting = Config::getSetting<double>("options.search-ball.max-target-side");
+  static Setting<double>* speedXSetting = Config::getSetting<double>("options.search-ball.speed-x");
+  static Setting<double>* speedYSetting = Config::getSetting<double>("options.search-ball.speed-y");
 
-  double a = turnSpeed->getValue();
-  double maxTargetHeight = d_topAngle->getValue();
-  double minTargetHeight = d_bottomAngle->getValue();
-  double maxTargetSide = d_sideAngle->getValue();
-  double speedX = d_speedX->getValue();
-  double speedY = d_speedY->getValue();
+  double turnSpeed = turnSpeedSetting->getValue();
+  double maxTargetHeight = maxTargetHeightSetting->getValue();
+  double minTargetHeight = minTargetHeightSetting->getValue();
+  double maxTargetSide = maxTargetSideSetting->getValue();
+  double speedX = speedXSetting->getValue();
+  double speedY = speedYSetting->getValue();
 
-  // make sure head isfully turned in the direction we are turning to maximize our chances of seeing the ball
+  // make sure head is fully turned in the direction we are turning to maximize our chances of seeing the ball
   if (d_isLeftTurn ? currentPanAngleDegs <= (maxTargetSide - speedX) : currentPanAngleDegs >= (-maxTargetSide + speedX))
   {
     // utilize this opportunity to reset ourselves to be looking for the top first
@@ -40,8 +40,11 @@ vector<shared_ptr<Option>> SearchBall::runPolicy(Writer<StringBuffer>& writer)
   else
   {
     // if we are too far one way, force the need to correct movement
-    if (currentTiltAngleDegs >= (maxTargetHeight - 2))d_searchTop = false;
-    if (currentTiltAngleDegs <= (minTargetHeight + 2))d_searchTop = true;
+    if (currentTiltAngleDegs >= (maxTargetHeight - 2))
+      d_searchTop = false;
+
+    if (currentTiltAngleDegs <= (minTargetHeight + 2))
+      d_searchTop = true;
 
     // start searching once we have maxed out to one side
     if (d_searchTop)
@@ -72,7 +75,7 @@ vector<shared_ptr<Option>> SearchBall::runPolicy(Writer<StringBuffer>& writer)
     }
 
     // finaly we need to turn ourselves
-    d_walkModule->setTurnAngle(d_isLeftTurn ? a : -a);
+    d_walkModule->setTurnAngle(d_isLeftTurn ? turnSpeed : -turnSpeed);
   }
 
   // return nothing
