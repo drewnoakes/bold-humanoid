54f9ccc38d119957d57c467f2a436bc92026c627
Drew Noakes <git@drewnoakes.com>
2014-05-10 00:56:45 +0100

    Add ActionOption class.

---
+++ Option/ActionOption/actionoption.cc
@@ -0,0 +1,28 @@
+#include "actionoption.hh"
+
+using namespace bold;
+using namespace rapidjson;
+using namespace std;
+
+vector<shared_ptr<Option>> ActionOption::runPolicy(Writer<StringBuffer>& writer)
+{
+  writer.String("run").Bool(d_needsRunning);
+
+  if (d_needsRunning)
+  {
+    d_action();
+    d_needsRunning = false;
+  }
+
+  return {};
+}
+
+void ActionOption::reset()
+{
+  d_needsRunning = true;
+}
+
+double ActionOption::hasTerminated()
+{
+  return !d_needsRunning;
+}
---
+++ Option/ActionOption/actionoption.hh
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "../option.hh"
+
+namespace bold
+{
+  class ActionOption : public Option
+  {
+  public:
+    ActionOption(std::string const& id, std::function<void()> action)
+    : Option(id, "ActionOption"),
+      d_action(action),
+      d_needsRunning(true)
+    {}
+
+    virtual std::vector<std::shared_ptr<Option>> runPolicy(rapidjson::Writer<rapidjson::StringBuffer>& writer) override;
+
+    virtual void reset() override;
+
+    virtual double hasTerminated() override;
+
+  private:
+    std::function<void()> d_action;
+    bool d_needsRunning;
+  };
+}
