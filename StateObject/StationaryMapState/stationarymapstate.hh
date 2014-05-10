42946570a8bcca313ba6fde1f42f9a79d984afaa
Drew Noakes <git@drewnoakes.com>
2014-05-10 23:30:31 +0100

    StationaryMapState sorts estimates by counts. Add getters.

--- StateObject/StationaryMapState/stationarymapstate.hh
+++ StateObject/StationaryMapState/stationarymapstate.hh
@@ -3,6 +3,7 @@
 #include "../stateobject.hh"
 #include "../../stats/average.hh"
 
+#include <vector>
 #include <Eigen/Geometry>
 
 namespace bold
@@ -13,13 +14,25 @@ namespace bold
     StationaryMapState(std::vector<Average<Eigen::Vector3d>> ballEstimates, std::vector<Average<Eigen::Vector3d>> goalEstimates)
     : d_ballEstimates(ballEstimates),
       d_goalEstimates(goalEstimates)
-    {}
+    {
+      // Sort estimates such that those with greater numbers of observations appear first
+      std::sort(d_ballEstimates.begin(), d_ballEstimates.end(), compareAverages);
+      std::sort(d_goalEstimates.begin(), d_goalEstimates.end(), compareAverages);
+    }
 
     void writeJson(rapidjson::Writer<rapidjson::StringBuffer>& writer) const override;
+
+    std::vector<Average<Eigen::Vector3d>> const& getBallEstimates() const { return d_ballEstimates; };
+    std::vector<Average<Eigen::Vector3d>> const& getGoalEstimates() const { return d_goalEstimates; };
 
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
 
   private:
+    static bool compareAverages(Average<Eigen::Vector3d> const& a, Average<Eigen::Vector3d> const& b)
+    {
+      return a.getCount() > b.getCount();
+    }
+
     std::vector<Average<Eigen::Vector3d>> d_ballEstimates;
     std::vector<Average<Eigen::Vector3d>> d_goalEstimates;
   };
