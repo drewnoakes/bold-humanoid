feafa57627b6566ed7d458d96b743c117d009bd3
Drew Noakes <git@drewnoakes.com>
2014-06-06 13:54:53 +0100

    Consolidate BodyControlState.

--- StateObject/BodyControlState/bodycontrolstate.cc
+++ StateObject/BodyControlState/bodycontrolstate.cc
@@ -1,9 +1,25 @@
 #include "bodycontrolstate.hh"
 
+#include "../../BodyControl/bodycontrol.hh"
 #include "../../JointId/jointid.hh"
 
 using namespace bold;
 using namespace rapidjson;
+using namespace std;
+
+BodyControlState::BodyControlState(shared_ptr<BodyControl> bodyControl, ulong motionCycleNumber)
+: d_motionCycleNumber(motionCycleNumber)
+{
+  for (uchar j = (uchar)JointId::MIN; j <= (uchar)JointId::MAX; j++)
+  {
+    auto joint = bodyControl->getJoint((JointId)j);
+
+    d_jointStates[j - 1].value = joint->getValue();
+    d_jointStates[j - 1].pGain = joint->getPGain();
+    d_jointStates[j - 1].iGain = joint->getIGain();
+    d_jointStates[j - 1].dGain = joint->getDGain();
+  }
+}
 
 void BodyControlState::writeJson(Writer<StringBuffer>& writer) const
 {
