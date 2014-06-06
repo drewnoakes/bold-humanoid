914b8ce496294ec24e3d961e844aa0f2a5316f41
Sander van Dijk <sgvandijk@gmail.com>
2014-06-06 16:12:05 +0100

    Use new resampling method and line junctions

--- Localiser/update.cc
+++ Localiser/update.cc
@@ -23,20 +23,20 @@ void Localiser::update()
         if (cnt > 2)
           break;
 
-        Vector2d observed2d(observed.head<2>());
+        Vector2d observed2d{observed.head<2>()};
         double bestScore = 0;
 
         for (Vector3d const& candidate : FieldMap::getGoalPostPositions())
         {
-          Vector3d candidate3d(candidate.x(), candidate.y(), 0);
-          Vector3d candidateAgent3d(agentWorld3d * candidate3d);
-          Vector2d candidateAgent2d(candidateAgent3d.x(), candidateAgent3d.y());
+          Vector3d candidate3d{candidate.x(), candidate.y(), 0};
+          Vector3d candidateAgent3d{agentWorld3d * candidate3d};
+          Vector2d candidateAgent2d{candidateAgent3d.head<2>()};
 
           // very naive scoring system for now...
 
           double distance = (candidateAgent2d - observed2d).norm();
 
-          double score = exp(-distance*distance);
+          double score = exp(-distance*distance / 10);
 
           if (score > bestScore)
             bestScore = score;
@@ -49,48 +49,42 @@ void Localiser::update()
     jointModel.addModel(goalPostModel);
   }
 
-  if (d_useLines->getValue() &&  agentFrame->getObservedLineSegments().size() > 0)
+  if (d_useLines->getValue() && agentFrame->getObservedLineJunctions().size() > 0)
   {
-    log::verbose("Localiser::update") << "Using line model";
+    log::verbose("Localiser::update") << "Using line junction model";
     auto lineModel = [&](Vector4d const& state) {
       AgentPosition pos(state);
 
       Affine3d agentWorld3d(pos.agentWorldTransform());
       Affine3d worldAgent3d(pos.worldAgentTransform());
 
-      // TODO avoid 2d->3d->2d conversion here by creating a worldToAgent2d transform
-
-      // IDEA further positions have more error, so should carry lesser reward? result: bias slightly towards closer matches
-
-      double scoreSum = 0;
-
-      //
-      // Score observed lines
-      //
-
-      for (LineSegment3d const& observed : agentFrame->getObservedLineSegments())
+      double scoreProd = 1.0;
+      for (auto const& observedLineJunction : agentFrame->getObservedLineJunctions())
       {
-        LineSegment2d observed2d(observed.to<2>());
-
+        Vector2d observedLineJunction2d{observedLineJunction.position};
         double bestScore = 0;
-
-        for (LineSegment3d const& candidate : FieldMap::getFieldLines())
+        for (auto const& candidate : FieldMap::getFieldLineJunctions())
         {
-          LineSegment2d candidateAgent = LineSegment3d(agentWorld3d * candidate.p1(), agentWorld3d * candidate.p2()).to<2>();
+          if (candidate.type != observedLineJunction.type)
+            continue;
+
+          Vector3d candidate3d{candidate.position.x(), candidate.position.y(), 0};
+          Vector3d candidateAgent3d{agentWorld3d * candidate3d};
+          Vector2d candidateAgent2d{candidateAgent3d.head<2>()};
 
           // very naive scoring system for now...
 
-          double distance1 = (observed2d.p1() - Math::linePointClosestToPoint(candidateAgent, observed2d.p1())).norm();
-          double distance2 = (observed2d.p2() - Math::linePointClosestToPoint(candidateAgent, observed2d.p2())).norm();
-          double distance = distance1 + distance2;
-          double score = exp(-distance * distance);
+          double distance = (candidateAgent2d - observedLineJunction2d).norm();
+
+          double score = exp(-distance*distance / 10);
 
           if (score > bestScore)
             bestScore = score;
         }
-        scoreSum += bestScore;
+        scoreProd *= bestScore;
       }
-      return scoreSum / agentFrame->getObservedLineSegments().size();
+
+      return scoreProd;
     };
     jointModel.addModel(lineModel);
   }
