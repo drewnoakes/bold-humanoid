d48d5e618af37b69b0f4a220c1ce995847948374
Sander van Dijk <sgvandijk@gmail.com>
2014-06-15 13:31:23 +0100

    RangePixelLabel implementation

---
+++ PixelLabel/RangePixelLabel/rangepixellabel.hh
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "../pixellabel.hh"
+
+namespace bold
+{
+  class RangePixelLabel : public PixelLabel
+  {
+  public:
+    RangePixelLabel(std::string name, Colour::hsvRange hsvRange);
+
+    Colour::hsvRange getHSVRange() const;
+    void setHSVRange(Colour::hsvRange range);
+
+    float labelProb(Colour::hsv const& pixelColour) const override;
+    Colour::hsv modalColour() const override;
+
+  private:
+    Colour::hsvRange d_hsvRange;
+  };
+
+  
+
+  inline RangePixelLabel::RangePixelLabel(std::string name, Colour::hsvRange hsvRange)
+    : PixelLabel{name},
+    d_hsvRange{std::move(hsvRange)}
+  {}
+  
+  Colour::hsvRange RangePixelLabel::getHSVRange() const
+  {
+    return d_hsvRange;
+  }
+
+  void RangePixelLabel::setHSVRange(Colour::hsvRange range)
+  {
+    d_hsvRange = std::move(range);
+  }
+
+  float RangePixelLabel::labelProb(Colour::hsv const& pixelColour) const
+  {
+    return d_hsvRange.contains(pixelColour) ? 1.0f : 0.0f;
+  }
+
+  Colour::hsv RangePixelLabel::modalColour() const
+  {
+    return d_hsvRange.toHsv();
+  }
+}
