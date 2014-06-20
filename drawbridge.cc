767de725da6504d444fe840e3fe24f6999eaa7e8
Drew Noakes <git@drewnoakes.com>
2014-06-20 23:55:31 +0100

    Drawbridge console features.

--- drawbridge.cc
+++ drawbridge.cc
@@ -4,6 +4,7 @@
 #include "../Config/config.hh"
 #include "../UDPSocket/udpsocket.hh"
 #include "../util/log.hh"
+#include "../version.hh"
 
 using namespace bold;
 using namespace std;
@@ -96,15 +97,111 @@ int websocket_callback(libwebsocket_context *context, libwebsocket *wsi, libwebs
   }
 }
 
-int main()
+void printUsage()
 {
-  static constexpr uint MaxMessageSize = 1024*1024;
+  cout << endl;
+  cout << "Options:" << endl;
+  cout << endl;
+  cout << ccolor::fore::lightblue << "  -c <file> " << ccolor::fore::white << "use specified configuration file (or --config)" << endl;
+  cout << ccolor::fore::lightblue << "  -v        " << ccolor::fore::white << "verbose logging (or --verbose)" << endl;
+  cout << ccolor::fore::lightblue << "  -h        " << ccolor::fore::white << "show these options (or --help)" << endl;
+  cout << ccolor::fore::lightblue << "  --version " << ccolor::fore::white << "print git version details at time of build" << endl;
+  cout << ccolor::reset;
+}
 
+vector<string> banners = {
+  " _____     ______     ______     __     __     ______     ______     __     _____     ______     ______   \n/\\  __-.  /\\  == \\   /\\  __ \\   /\\ \\  _ \\ \\   /\\  == \\   /\\  == \\   /\\ \\   /\\  __-.  /\\  ___\\   /\\  ___\\  \n\\ \\ \\/\\ \\ \\ \\  __<   \\ \\  __ \\  \\ \\ \\/ \".\\ \\  \\ \\  __<   \\ \\  __<   \\ \\ \\  \\ \\ \\/\\ \\ \\ \\ \\__ \\  \\ \\  __\\  \n \\ \\____-  \\ \\_\\ \\_\\  \\ \\_\\ \\_\\  \\ \\__/\".~\\_\\  \\ \\_____\\  \\ \\_\\ \\_\\  \\ \\_\\  \\ \\____-  \\ \\_____\\  \\ \\_____\\\n  \\/____/   \\/_/ /_/   \\/_/\\/_/   \\/_/   \\/_/   \\/_____/   \\/_/ /_/   \\/_/   \\/____/   \\/_____/   \\/_____/",
+  "\n888~-_   888~-_        e      Y88b         / 888~~\\  888~-_   888 888~-_    e88~~\\  888~~ \n888   \\  888   \\      d8b      Y88b       /  888   | 888   \\  888 888   \\  d888     888___\n888    | 888    |    /Y88b      Y88b  e  /   888 _/  888    | 888 888    | 8888 __  888   \n888    | 888   /    /  Y88b      Y88bd8b/    888  \\  888   /  888 888    | 8888   | 888   \n888   /  888_-~    /____Y88b      Y88Y8Y     888   | 888_-~   888 888   /  Y888   | 888   \n888_-~   888 ~-_  /      Y88b      Y  Y      888__/  888 ~-_  888 888_-~    \"88__/  888___",
+  "\n,-.  ,-.   ,.  ,   . ,-.  ,-.  , ,-.   ,-. ,--.\n|  \\ |  ) /  \\ | . | |  ) |  ) | |  \\ /    |   \n|  | |-<  |--| | ) ) |-<  |-<  | |  | | -. |-  \n|  / |  \\ |  | |/|/  |  ) |  \\ | |  / \\  | |   \n`-'  '  ' '  ' ' '   `-'  '  ' ' `-'   `-' `--'",
+  "\n.%%%%%...%%%%%....%%%%...%%...%%..%%%%%...%%%%%...%%%%%%..%%%%%....%%%%...%%%%%%.\n.%%..%%..%%..%%..%%..%%..%%...%%..%%..%%..%%..%%....%%....%%..%%..%%......%%.....\n.%%..%%..%%%%%...%%%%%%..%%.%.%%..%%%%%...%%%%%.....%%....%%..%%..%%.%%%..%%%%...\n.%%..%%..%%..%%..%%..%%..%%%%%%%..%%..%%..%%..%%....%%....%%..%%..%%..%%..%%.....\n.%%%%%...%%..%%..%%..%%...%%.%%...%%%%%...%%..%%..%%%%%%..%%%%%....%%%%...%%%%%%.\n.................................................................................",
+  " _____   ______         _  _  _ ______ ______  _____ _____    ______ _______ \n(____ \\ (_____ \\   /\\  | || || (____  (_____ \\(_____|____ \\  / _____|_______)\n _   \\ \\ _____) ) /  \\ | || || |____)  )____) )  _   _   \\ \\| /  ___ _____   \n| |   | (_____ ( / /\\ \\| ||_|| |  __  (_____ (  | | | |   | | | (___)  ___)  \n| |__/ /      | | |__| | |___| | |__)  )    | |_| |_| |__/ /| \\____/| |_____ \n|_____/       |_|______|\\______|______/     |_(_____)_____/  \\_____/|_______)",
+  "______   ______ _______ _  _  _ ______   ______ _____ ______   ______ _______\n |     \\ |_____/ |_____| |  |  | |_____] |_____/   |   |     \\ |  ____ |______\n |_____/ |    \\_ |     | |__|__| |_____] |    \\_ __|__ |_____/ |_____| |______",
+  "   __   ___    _   _   __ ___   ___   __ __    __   ___\n  /  \\ / o | .' \\ ///7/ // o.) / o | / //  \\ ,'_/  / _/\n / o |/  ,' / o /| V V // o \\ /  ,' / // o |/ /_n / _/ \n/__,'/_/`_\\/_n_/ |_n_,'/___,'/_/`_\\/_//__,' |__,'/___/ ",
+  " __  __          __  __   __  __  __\n|  \\|__) /\\ |  ||__)|__)||  \\/ _ |_ \n|__/| \\ /--\\|/\\||__)| \\ ||__/\\__)|__"
+};
+
+void printBanner()
+{
+  if (log::isStdOutRedirected())
+  {
+    cout << banners[rand() % banners.size()] << endl << endl;
+  }
+  else
+  {
+    cout << ccolor::bold << ccolor::fore::lightmagenta
+      << banners[rand() % banners.size()]
+      << endl << endl << ccolor::reset;
+  }
+}
+
+int main(int argc, char **argv)
+{
+  srand(time(0));
+
+  // TODO: use getopt
+  auto nextArg = [&](int* i) -> char*
+  {
+    if (*i == argc - 1)
+    {
+      // No more arguments. Error!
+      log::error() << "Insufficient arguments";
+      exit(-1);
+    }
+    int j = *i + 1;
+    *i = j;
+    return argv[j];
+  };
+
+  string configurationFile = "configuration-agent.json";
   log::minLevel = LogLevel::Info;
 
-  Config::initialise("configuration-metadata.json", "configuration-agent.json");
+  for (int i = 1; i < argc; ++i)
+  {
+    string arg(argv[i]);
+    if (arg == "-h" || arg == "--help")
+    {
+      printBanner();
+      printUsage();
+      return 0;
+    }
+    else if (arg == "-c" || arg == "--config")
+    {
+      configurationFile = nextArg(&i);
+    }
+    else if (arg == "-v" || arg == "--verbose")
+    {
+      log::minLevel = LogLevel::Verbose;
+    }
+    else if (arg == "--version")
+    {
+      cout << ccolor::fore::lightblue << "SHA1:        " << ccolor::reset << Version::GIT_SHA1 << endl
+#if EIGEN_ALIGN
+           << ccolor::fore::lightblue << "Eigen align: " << ccolor::reset << "Yes" << endl
+#else
+        << ccolor::fore::lightblue << "Eigen align: " << ccolor::reset << "No" << endl
+#endif
+        << ccolor::fore::lightblue << "Build type:  " << ccolor::reset << Version::BUILD_TYPE << endl
+#if INCLUDE_ASSERTIONS
+        << ccolor::fore::lightblue << "Assertions:  " << ccolor::reset << "Yes" << endl
+#else
+           << ccolor::fore::lightblue << "Assertions:  " << ccolor::reset << "No" << endl
+#endif
+        << ccolor::fore::lightblue << "Commit date: " << ccolor::reset << Version::GIT_DATE << " (" << Version::describeTimeSinceGitDate() << ")" << endl
+        << ccolor::fore::lightblue << "Message:     " << ccolor::reset << Version::GIT_COMMIT_SUBJECT << endl;
+      return 0;
+    }
+    else
+    {
+      log::error() << "Unknown argument: " << arg;
+      printUsage();
+      return -1;
+    }
+  }
 
-  cout << "I'm alive!!!" << endl;
+  Config::initialise("configuration-metadata.json", configurationFile);
+
+  // TODO support --random option for front-end testing and demonstration purposes
+  // TODO only log output when number of robots changes, or a client connects/disconnects
 
   int udpPort = Config::getStaticValue<int>("drawbridge.udp-port");
   int wsPort = Config::getStaticValue<int>("drawbridge.websocket-port");
@@ -142,8 +239,14 @@ int main()
   }
   log::info("WebSockets") << "Listening on TCP port " << wsPort;
 
+  //
+  // Start tireless loop
+  //
+
   while (true)
   {
+    static constexpr uint MaxMessageSize = 1024*1024;
+
     //
     // Listen for UDP packet
     //
