6468d629ee3b6431c4e603a7a8074e51b1c37e03
Drew Noakes <git@drewnoakes.com>
2014-06-23 23:00:08 +0100

    New approach at calculating orientation angles quaternion.

--- StateObject/OrientationState/orientationstate.cc
+++ StateObject/OrientationState/orientationstate.cc
@@ -2,16 +2,29 @@
 
 #include "../../Math/math.hh"
 
+#include <iomanip>
+
 using namespace bold;
 using namespace Eigen;
 using namespace rapidjson;
 
-OrientationState::OrientationState(Eigen::Quaterniond quaternion)
-: d_quaternion(quaternion),
-  d_pitch(Math::normaliseRads(Math::pitchFromQuaternion(quaternion))),
-  d_roll(Math::normaliseRads(Math::rollFromQuaternion(quaternion))),
-  d_yaw(Math::normaliseRads(Math::yawFromQuaternion(quaternion)))
-{}
+OrientationState::OrientationState(Quaterniond quaternion)
+: d_quaternion(quaternion)
+{
+  Affine3d rotation(d_quaternion);
+  Vector3d axisX1 = rotation.matrix().col(0).head<3>();
+  d_yaw = atan2(axisX1.y(), axisX1.x());
+
+  rotation = AngleAxisd(-d_yaw, Vector3d::UnitZ()) * rotation;
+
+  Vector3d axisY = rotation.matrix().col(1).head<3>();
+  d_pitch = atan2(axisY.z(), axisY.y());
+
+  rotation = AngleAxisd(-d_pitch, Vector3d::UnitX()) * rotation;
+
+  Vector3d axisX2 = rotation.matrix().col(0).head<3>();
+  d_roll = atan2(axisX2.z(), axisX2.x());
+}
 
 void OrientationState::writeJson(Writer<StringBuffer>& writer) const
 {
@@ -27,68 +40,9 @@ void OrientationState::writeJson(Writer<StringBuffer>& writer) const
     writer.Double(swapNaN(d_quaternion.w(), 0), "%.5f");
     writer.EndArray();
 
-    auto const& q1 = d_quaternion;
-
-    double sqw = q1.w()*q1.w();
-    double sqx = q1.x()*q1.x();
-    double sqy = q1.y()*q1.y();
-    double sqz = q1.z()*q1.z();
-    double unit = sqx + sqy + sqz + sqw; // if normalised is one, otherwise is correction factor
-    double test = q1.x()*q1.y() + q1.z()*q1.w();
-
-    double heading;
-    double attitude;
-    double bank;
-
-    if (test > 0.499*unit) { // singularity at north pole
-      heading = 2 * atan2(q1.x(),q1.w());
-      attitude = M_PI/2;
-      bank = 0;
-    }
-    else if (test < -0.499*unit) { // singularity at south pole
-      heading = -2 * atan2(q1.x(),q1.w());
-      attitude = -M_PI/2;
-      bank = 0;
-    }
-    else
-    {
-      heading = atan2(2*q1.y()*q1.w()-2*q1.x()*q1.z() , sqx - sqy - sqz + sqw);
-      attitude = asin(2*test/unit);
-      bank = atan2(2*q1.x()*q1.w()-2*q1.y()*q1.z() , -sqx + sqy - sqz + sqw);
-    }
-
-    writer.String("pitch").Double(swapNaN(attitude, 0));
-    writer.String("roll").Double(swapNaN(bank, 0));
-    writer.String("yaw").Double(swapNaN(heading, 0));
-//    writer.String("pitch").Double(Math::pitchFromQuaternion(d_quaternion));
-//    writer.String("roll").Double(Math::rollFromQuaternion(d_quaternion));
-//    writer.String("yaw").Double(Math::yawFromQuaternion(d_quaternion));
-
-    /*
-    writer.String("angle-axis");
-    AngleAxisd aa(d_quaternion);
-    writer.StartArray();
-    writer.Double(aa.angle());
-    writer.Double(aa.axis().x());
-    writer.Double(aa.axis().y());
-    writer.Double(aa.axis().z());
-    writer.EndArray();
-
-    writer.String("euler");
-    Affine3d af(d_quaternion);
-    Matrix3d m(af.matrix().block<3,3>(0, 0));
-    writer.StartArray();
-    writer.Double(m(0));
-    writer.Double(m(1));
-    writer.Double(m(2));
-    writer.Double(m(3));
-    writer.Double(m(4));
-    writer.Double(m(5));
-    writer.Double(m(6));
-    writer.Double(m(7));
-    writer.Double(m(8));
-    writer.EndArray();
-    */
+    writer.String("pitch").Double(swapNaN(d_pitch, 0));
+    writer.String("roll").Double(swapNaN(d_roll, 0));
+    writer.String("yaw").Double(swapNaN(d_yaw, 0));
   }
   writer.EndObject();
 }
