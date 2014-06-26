d4b7a43cbe2c87b14b16ec57d6cda7fbbf99261f
Sander van Dijk <sgvandijk@gmail.com>
2014-06-26 21:29:44 +0100

    rename VectorsEqual arguments to make clearer which one is which

--- test/helpers.hh
+++ test/helpers.hh
@@ -10,56 +10,56 @@
 #include <Eigen/Geometry>
 #include <iostream>
 
-inline ::testing::AssertionResult VectorsEqual(Eigen::Vector3d const& a, Eigen::Vector3d const& b, const float delta = 0.00001) {
-  double d = (a-b).norm();
+inline ::testing::AssertionResult VectorsEqual(Eigen::Vector3d const& expected, Eigen::Vector3d const& actual, const float delta = 0.00001) {
+  double d = (expected-actual).norm();
   if (d < delta)
     return ::testing::AssertionSuccess();
   else
-    return ::testing::AssertionFailure() << "Actual: " << b.transpose() << ", expected: " << a.transpose() << " d = " << d;
+    return ::testing::AssertionFailure() << "Actual: " << actual.transpose() << ", expected: " << expected.transpose() << " d = " << d;
 }
 
-inline ::testing::AssertionResult VectorsEqual(Eigen::Vector2d const& a, Eigen::Vector2d const& b, const double delta = 0.00001) {
-  double d = (a-b).norm();
+inline ::testing::AssertionResult VectorsEqual(Eigen::Vector2d const& expected, Eigen::Vector2d const& actual, const double delta = 0.00001) {
+  double d = (expected-actual).norm();
   if (d < delta)
     return ::testing::AssertionSuccess();
   else
-    return ::testing::AssertionFailure() << "Actual: " << b.transpose() << ", expected: " << a.transpose() << " d = " << d;
+    return ::testing::AssertionFailure() << "Actual: " << actual.transpose() << ", expected: " << expected.transpose() << " d = " << d;
 }
 
 template<typename T, int N>
-inline ::testing::AssertionResult VectorsEqual(Eigen::Matrix<T,N,1> const& a, Eigen::Matrix<T,N,1> const& b, const double delta = 0.00001) {
-  double d = (a-b).norm();
+inline ::testing::AssertionResult VectorsEqual(Eigen::Matrix<T,N,1> const& expected, Eigen::Matrix<T,N,1> const& actual, const double delta = 0.00001) {
+  double d = (expected-actual).norm();
   if (d < delta)
     return ::testing::AssertionSuccess();
   else
-    return ::testing::AssertionFailure() << "Actual: " << b.transpose() << ", expected: " << a.transpose() << " d = " << d;
+    return ::testing::AssertionFailure() << "Actual: " << actual.transpose() << ", expected: " << expected.transpose() << " d = " << d;
 }
 
 template<int N>
-inline ::testing::AssertionResult VectorsEqual(Eigen::Matrix<int,N,1> const& a, Eigen::Matrix<int,N,1> const& b) {
-  double d = (a-b).norm();
+inline ::testing::AssertionResult VectorsEqual(Eigen::Matrix<int,N,1> const& expected, Eigen::Matrix<int,N,1> const& actual) {
+  double d = (expected-actual).norm();
   if (d == 0)
     return ::testing::AssertionSuccess();
   else
-    return ::testing::AssertionFailure() << "Actual: " << b.transpose() << ", expected: " << a.transpose() << " d = " << d;
+    return ::testing::AssertionFailure() << "Actual: " << actual.transpose() << ", expected: " << expected.transpose() << " d = " << d;
 }
 
-inline ::testing::AssertionResult MatricesEqual(Eigen::MatrixXd const& a, Eigen::MatrixXd const& b, double delta = 0.000001) {
-  double d = (a-b).array().abs().sum();
+inline ::testing::AssertionResult MatricesEqual(Eigen::MatrixXd const& expected, Eigen::MatrixXd const& actual, double delta = 0.000001) {
+  double d = (expected-actual).array().abs().sum();
   if (d < delta)
     return ::testing::AssertionSuccess();
   else
-    return ::testing::AssertionFailure() << "Actual: " << std::endl << b << std::endl << "Expected: " << std::endl << a << std::endl << " d = " << d;
+    return ::testing::AssertionFailure() << "Actual: " << std::endl << actual << std::endl << "Expected: " << std::endl << expected << std::endl << " d = " << d;
 }
 
 template<typename T, int dim>
-inline ::testing::AssertionResult LinesEqual(bold::LineSegment<T,dim> const& a, bold::LineSegment<T,dim> const& b, const double delta = 0.000001) {
-  double d1 = (a.p1()-b.p1()).norm();
-  double d2 = (a.p2()-b.p2()).norm();
+inline ::testing::AssertionResult LinesEqual(bold::LineSegment<T,dim> const& expected, bold::LineSegment<T,dim> const& actual, const double delta = 0.000001) {
+  double d1 = (expected.p1()-actual.p1()).norm();
+  double d2 = (expected.p2()-actual.p2()).norm();
   if (d1 <= delta && d2 <= delta)
     return ::testing::AssertionSuccess();
   else
-    return ::testing::AssertionFailure() << "Actual: " << b << ", expected: " << a << " d1=" << d1 << " d2=" << d2;
+    return ::testing::AssertionFailure() << "Actual: " << actual << ", expected: " << expected << " d1=" << d1 << " d2=" << d2;
 }
 
 //PrintTo(const T&, ostream*)
@@ -84,9 +84,9 @@ inline std::ostream& operator<<(std::ostream& stream, Eigen::Vector3d const& v)
   return stream << "(" << v.x() << ", " << v.y() << ", " << v.z() << ")";
 }
 
-inline bool operator==(Eigen::Vector2i const& a, Eigen::Vector2i const& b)
+inline bool operator==(Eigen::Vector2i const& expected, Eigen::Vector2i const& actual)
 {
-  return a.x() == b.x() && a.y() == b.y();
+  return expected.x() == actual.x() && expected.y() == actual.y();
 }
 
 
