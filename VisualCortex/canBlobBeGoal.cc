c8c5a14d71f8701cd745161b4a50fc9543f4627b
Drew Noakes <git@drewnoakes.com>
2014-07-02 23:38:13 +0100

    Revert 'Remove broken goalpost width test'
    This reverts commit 3a3c2b6d8c92ef654360ac8369837bfc464f9a89.
    
    This check is not perfect, but since removing it I have started to see
    tiny goals on the sides of the ball. The upper threshold can be
    increased to 10 in order to avoid ignoring goals that appear angled in
    the camera frame until such a time as we have a better technique for
    this.

--- VisualCortex/canBlobBeGoal.cc
+++ VisualCortex/canBlobBeGoal.cc
@@ -11,6 +11,31 @@ bool VisualCortex::canBlobBeGoal(Blob const& blob, Vector2d& pos)
     topRun.y
   );
 
+  // Determine width as the average run length
+  // TODO would be interesting to ensure low variance here, as well as just the average
+  ulong widthSum = 0;
+  for (Run const& run : blob.runs)
+    widthSum += run.length();
+  double width = widthSum / blob.runs.size();
+
+  // Ensure the goal is roughly the expected radius (in metres)
+  Vector2d sidePos = basePos + Vector2d(width/2.0, 0);
+
+  auto midPointAgentSpace = d_spatialiser->findGroundPointForPixel(basePos);
+  auto sidePointAgentSpace = d_spatialiser->findGroundPointForPixel(sidePos);
+
+  if (!midPointAgentSpace || !sidePointAgentSpace)
+    return false;
+
+  double radiusAgentSpace = abs((*midPointAgentSpace - *sidePointAgentSpace).norm());
+
+  static double goalRadius = Config::getStaticValue<double>("world.goal-post-diameter") / 2.0;
+
+  double goalMeasuredWidthRatio = radiusAgentSpace / goalRadius;
+  if (goalMeasuredWidthRatio < d_acceptedGoalMeasuredWidthRatio->getValue().min() ||
+      goalMeasuredWidthRatio > d_acceptedGoalMeasuredWidthRatio->getValue().max())
+    return false;
+
   /*
   TODO test whether we're able to estimate the distance of a goal at the opposite end before enabling this
 
