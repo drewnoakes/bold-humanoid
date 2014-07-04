7fe00b8a77c428b79ee6916e809a8b80fac0729a
Sander van Dijk <sgvandijk@gmail.com>
2014-07-04 21:22:50 +0100

    HostogramLabelTeacher all steps operational

--- HistogramLabelTeacher/HistogramLabelTeacherBase.cc
+++ HistogramLabelTeacher/HistogramLabelTeacherBase.cc
@@ -1,13 +1,49 @@
 #include "histogramlabelteacher.ih"
 
-HistogramLabelTeacherBase::HistogramLabelTeacherBase()
-  : d_trainImage{},
+HistogramLabelTeacherBase::HistogramLabelTeacherBase(std::vector<std::string> const& names)
+  : d_yuvTrainImage{},
   d_seedPoint{0,0},
-  d_maxDiff{0},
-  d_snapshotRequested(false)
+  d_snapshotRequested{false},
+  d_labelRequested{false}
 {
-  Config::addAction("histogramlabelteacher.snap-train-image", "Snap Image", [this]()
+  Config::getSetting<int>("histogram-label-teacher.max-flood-diff")->track([this](int val) {
+      d_maxFloodDiff = val;
+      log::info("HistogramLabelTeacherBase") << "Setting maxFloodDiff: " << d_maxFloodDiff;
+    });
+
+  map<int, string> enumOptions;
+  for (unsigned i = 0; i < names.size(); ++i)
+    enumOptions[i] = names[i];
+
+  auto setting = new EnumSetting("histogram-label-teacher.label-to-train", enumOptions, false, "Label to train");
+  setting->changed.connect([names,this](int value) {
+      d_labelToTrain = value;
+    });
+  setting->setValue(0);
+
+  Config::addSetting(setting);
+
+
+  Config::addAction("histogram-label-teacher.snap-train-image", "Snap Image", [this]()
                     {
                       d_snapshotRequested = true;
                     });
+
+  Config::addAction("histogram-label-teacher.set-seed-point", "Set Seed Point", [this](rapidjson::Value* val) {
+      int x, y;
+      val->TryGetIntValue("x", &x);
+      val->TryGetIntValue("y", &y);
+      log::info("HistogramLabelTeacherBase") << "Setting seed point: " << x << " " << y;
+      d_seedPoint = Vector2i{x, y};
+
+      d_mask = floodFill();
+    });
+
+  Config::addAction("histogram-label-teacher.train", "Train", [this]() {
+      train(d_labelToTrain, d_mask);
+    });
+
+  Config::addAction("histogram-label-teacher.label", "Label", [this]() {
+      d_labelRequested = !d_labelRequested;
+    });
 }
--- HistogramLabelTeacher/histogramlabelteacher.hh
+++ HistogramLabelTeacher/histogramlabelteacher.hh
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "../PixelLabel/HistogramPixelLabel/histogrampixellabel.hh"
+#include "../Setting/setting.hh"
+
 #include <vector>
 #include <string>
 #include <memory>
@@ -10,26 +12,46 @@ namespace bold
   class HistogramLabelTeacherBase
   {
   public:
-    HistogramLabelTeacherBase();
+    HistogramLabelTeacherBase(std::vector<std::string> const& names);
+    virtual ~HistogramLabelTeacherBase() {}
 
     bool requestedSnapShot() const { return d_snapshotRequested; }
+    bool requestedLabel() const {return d_labelRequested; }
 
-    void setTrainImage(cv::Mat trainImg);
-    cv::Mat getTrainImage() const { return d_trainImage; }
+    void setYUVTrainImage(cv::Mat yuvTrainImg);
+    cv::Mat getYUVTrainImage() const { return d_yuvTrainImage; }
+    cv::Mat getBGRTrainImage() const
+    {
+      if (d_yuvTrainImage.rows == 0)
+        return d_yuvTrainImage;
+
+      cv::Mat bgrImage = cv::Mat{d_yuvTrainImage.rows, d_yuvTrainImage.cols, CV_8UC3};
+      cv::cvtColor(d_yuvTrainImage, bgrImage, CV_YCrCb2BGR);
+      return bgrImage;
+    }
+
+    cv::Mat getMask() const { return d_mask; }
 
     void setSeedPoint(Eigen::Vector2i point);
 
-    void setMaxDiff(uint8_t maxDiff);
-
     cv::Mat floodFill() const;
 
+    unsigned getLabelToTrain() const { return d_labelToTrain; }
+
+    virtual void train(unsigned labelIdx, cv::Mat const& mask) = 0;
+
+    virtual cv::Mat label(unsigned labelIdx) const = 0;
   protected:
-    cv::Mat d_trainImage;
+    cv::Mat d_yuvTrainImage;
+    cv::Mat d_mask;
+
     Eigen::Vector2i d_seedPoint;
-    uint8_t d_maxDiff;
+    float d_maxFloodDiff;
+    unsigned d_labelToTrain;
 
   private:
     bool d_snapshotRequested;
+    bool d_labelRequested;
   };
 
   template<uint8_t CHANNEL_BITS>
@@ -40,17 +62,20 @@ namespace bold
     
     std::vector<HistogramPixelLabel<CHANNEL_BITS>> getLabels() const;
 
-    void train(std::string const& labelName, cv::Mat const& mask);
+    void train(unsigned labelIdx, cv::Mat const& mask) override;
+
+    cv::Mat label(unsigned labelIdx) const override;
 
   private:
     std::vector<HistogramPixelLabel<CHANNEL_BITS>> d_labels;
   };
   
-
+  //////
 
   template<uint8_t CHANNEL_BITS>
   HistogramLabelTeacher<CHANNEL_BITS>::HistogramLabelTeacher(std::vector<std::string> const& names)
-    : d_labels{}
+    : HistogramLabelTeacherBase{names},
+    d_labels{}
   {
     for (auto const& name : names)
       d_labels.emplace_back(name);
@@ -62,9 +87,9 @@ namespace bold
     return d_labels;
   }
 
-  inline void HistogramLabelTeacherBase::setTrainImage(cv::Mat trainImage)
+  inline void HistogramLabelTeacherBase::setYUVTrainImage(cv::Mat yuvTrainImage)
   {
-    d_trainImage = trainImage;
+    d_yuvTrainImage = yuvTrainImage;
     d_snapshotRequested = false;
   }
 
@@ -73,50 +98,65 @@ namespace bold
     d_seedPoint = point;
   }
 
-  inline void HistogramLabelTeacherBase::setMaxDiff(uint8_t maxDiff)
-  {
-    d_maxDiff = maxDiff;
-  }
-
   inline cv::Mat HistogramLabelTeacherBase::floodFill() const
   {
-    cv::Mat mask = cv::Mat::zeros(d_trainImage.rows + 2, d_trainImage.cols + 2, CV_8UC1);
+    cv::Mat mask = cv::Mat::zeros(d_yuvTrainImage.rows + 2, d_yuvTrainImage.cols + 2, CV_8UC1);
 
-    cv::floodFill(d_trainImage, mask,
+    auto mfd = cv::Scalar(d_maxFloodDiff, d_maxFloodDiff, d_maxFloodDiff);
+    cv::floodFill(d_yuvTrainImage, mask,
                   cv::Point{d_seedPoint.x(), d_seedPoint.y()}, cv::Scalar(255), 0,
-                  d_maxDiff, d_maxDiff, 4 | cv::FLOODFILL_MASK_ONLY | (255 << 8));
-    
-    return cv::Mat{mask, cv::Rect(1, 1, d_trainImage.cols, d_trainImage.rows)};
+                  mfd, mfd, 4 | cv::FLOODFILL_MASK_ONLY | (255 << 8));
+
+    return cv::Mat{mask, cv::Rect(1, 1, d_yuvTrainImage.cols, d_yuvTrainImage.rows)};
     
     return mask;
   }
   
   template<uint8_t CHANNEL_BITS>
-  void HistogramLabelTeacher<CHANNEL_BITS>::train(std::string const& name, cv::Mat const& mask)
+  void HistogramLabelTeacher<CHANNEL_BITS>::train(unsigned labelIdx, cv::Mat const& mask)
   {
     ASSERT(mask.type() == CV_8UC1);
-    ASSERT(d_trainImage.cols == mask.cols && d_trainImage.rows == mask.rows);
-
-    auto labelIter = std::find_if(begin(d_labels), end(d_labels), [&name](HistogramPixelLabel<CHANNEL_BITS> const& label) {
-        return label.getName() == name;
-      });
-
-    if (labelIter == end(d_labels))
-      return;
+    ASSERT(d_yuvTrainImage.cols == mask.cols && d_yuvTrainImage.rows == mask.rows);
 
     for (unsigned i = 0; i < mask.rows; ++i)
     {
-      uint8_t const* trainImageRow = d_trainImage.ptr<uint8_t>(i);
+      uint8_t const* trainImageRow = d_yuvTrainImage.ptr<uint8_t>(i);
       uint8_t const* maskRow = mask.ptr<uint8_t>(i);
 
       for (unsigned j = 0; j < mask.cols; ++j)
         if (maskRow[j] != 0)
         {
-          auto hsv = Colour::hsv{trainImageRow[0], trainImageRow[1], trainImageRow[2]};
-          labelIter->addSample(hsv);
+          auto yuv = Colour::YCbCr{trainImageRow[j * 3 + 0], trainImageRow[j * 3 + 1], trainImageRow[j * 3 + 2]};
+          auto bgr = yuv.toBgrInt();
+          auto hsv = bgr2hsv(bgr);
+
+          d_labels[labelIdx].addSample(hsv);
         }
     }
   }
 
+  template<uint8_t CHANNEL_BITS>
+  cv::Mat HistogramLabelTeacher<CHANNEL_BITS>::label(unsigned labelIdx) const
+  {
+    auto labelImg = cv::Mat{d_yuvTrainImage.rows, d_yuvTrainImage.cols, CV_8UC1};
+    for (unsigned i = 0; i < labelImg.rows; ++i)
+    {
+      uint8_t const* trainImageRow = d_yuvTrainImage.ptr<uint8_t>(i);
+      uint8_t* labelRow = labelImg.ptr<uint8_t>(i);
 
+      for (unsigned j = 0; j < labelImg.cols; ++j)
+      {
+        auto yuv = Colour::YCbCr{trainImageRow[j * 3 + 0], trainImageRow[j * 3 + 1], trainImageRow[j * 3 + 2]};
+        auto bgr = yuv.toBgrInt();
+        auto hsv = bgr2hsv(bgr);
+        labelRow[j] = d_labels[labelIdx].labelProb(hsv) * 255;
+      }
+    }
+
+    cv::normalize(labelImg, labelImg, 0, 255, cv::NORM_MINMAX);
+    cv::Mat zero = cv::Mat::zeros(labelImg.rows, labelImg.cols, CV_8UC1);
+    cv::Mat _colourLabelImage;
+    cv::merge(std::vector<cv::Mat>{zero, labelImg, zero}, _colourLabelImage);
+    return _colourLabelImage;
+  }
 }
