84501178988f399562f2fdfbf00b6f0a08e17a0c
Drew Noakes <git@drewnoakes.com>
2014-07-09 18:01:16 +0100

    Minor tweaks to ApproachBall::runPolicy

--- Option/ApproachBall/approachball.cc
+++ Option/ApproachBall/approachball.cc
@@ -18,31 +18,33 @@ vector<shared_ptr<Option>> ApproachBall::runPolicy(Writer<StringBuffer>& writer)
   ASSERT(d_upperTurnLimitDegs->getValue() > d_lowerTurnLimitDegs->getValue());
   ASSERT(d_brakeDistance->getValue() != 0);
 
-  auto ballPos = State::get<AgentFrameState>()->getBallObservation();
+  auto agentFrame = State::get<AgentFrameState>();
 
-  if (!ballPos)
+  if (!agentFrame->isBallVisible())
   {
-    writer.String("ball").Null();
-//     log::warning("ApproachBall::runPolicy") << "No ball observation in AgentFrame yet ApproachBall was run";
+    writer.String("ballPos").Null();
     return {};
   }
 
-  writer.String("ballPos").StartArray().Double(ballPos->x()).Double(ballPos->y()).EndArray(2);
+  Vector2d ballPos(agentFrame->getBallObservation()->head<2>());
 
-  double dist = ballPos->head<2>().norm();
+  writer.String("ballPos").StartArray().Double(ballPos.x()).Double(ballPos.y()).EndArray(2);
 
+  double dist = ballPos.norm();
+
+  // If we're close to the ball, tell team mates that we're attacking
   d_behaviourControl->setPlayerActivity(
     dist < 0.5
       ? PlayerActivity::AttackingGoal
       : PlayerActivity::ApproachingBall);
 
+  // Subtract the stopping distance here so that the bot doesn't stop too suddenly
   if (d_useCustomStopDistance)
   {
     dist -= d_stopDistance;
   }
   else
   {
-    // subtract the stopping distance here so that the bot doesn't stop too suddenly!!!
     static auto stoppingDistance = Config::getSetting<double>("options.approach-ball.stop-distance");
     dist -= stoppingDistance->getValue();
   }
@@ -52,12 +54,12 @@ vector<shared_ptr<Option>> ApproachBall::runPolicy(Writer<StringBuffer>& writer)
 
   writer.String("ballDist").Double(dist);
 
-  double speedDueToDistance = Math::clamp(dist/d_brakeDistance->getValue(), 0.0, 1.0);
+  double speedScaleDueToDistance = Math::clamp(dist/d_brakeDistance->getValue(), 0.0, 1.0);
 
-  writer.String("distSpeed").Double(speedDueToDistance);
+  writer.String("distSpeed").Double(speedScaleDueToDistance);
 
-  // NOTE atan2 has flipped x/y on purpose
-  double ballAngleRads = -atan2(ballPos->x() - 0.04, ballPos->y());
+  Vector2d target(ballPos + Vector2d(0.04, 0));
+  double ballAngleRads = Math::angleToPoint(target);
 
   double speedScaleDueToAngle = Math::lerp(fabs(ballAngleRads),
                                            Math::degToRad(d_lowerTurnLimitDegs->getValue()),
@@ -67,7 +69,7 @@ vector<shared_ptr<Option>> ApproachBall::runPolicy(Writer<StringBuffer>& writer)
 
   writer.String("angleSpeed").Double(speedScaleDueToAngle);
 
-  double xSpeed = Math::lerp(speedDueToDistance * speedScaleDueToAngle,
+  double xSpeed = Math::lerp(speedScaleDueToDistance * speedScaleDueToAngle,
                              d_minForwardSpeed->getValue(),
                              d_maxForwardSpeed->getValue());
 
