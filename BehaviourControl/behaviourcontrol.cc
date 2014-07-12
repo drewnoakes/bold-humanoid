5cc02f41e1188307fb8079c0556ad64189b5f3c7
Drew Noakes <git@drewnoakes.com>
2014-07-12 12:49:52 +0100

    Log warnings when behaviour control values are set to different values within the same thing cycle.

--- BehaviourControl/behaviourcontrol.cc
+++ BehaviourControl/behaviourcontrol.cc
@@ -5,15 +5,64 @@
 #include "../StateObject/BehaviourControlState/behaviourcontrolstate.hh"
 
 using namespace bold;
+using namespace robocup;
 using namespace std;
 
 BehaviourControl::BehaviourControl(Agent& agent)
-: d_agent(agent)
+: d_agent(agent),
+  d_playerRoleSetThisCycle(false),
+  d_playerStatusSetThisCycle(false),
+  d_playerActivitySetThisCycle(false),
+  d_playModeSetThisCycle(false)
 {
   agent.onThinkEnd.connect([this] { updateStateObject(); });
 }
 
-void BehaviourControl::updateStateObject() const
+void BehaviourControl::updateStateObject()
 {
+  d_playerRoleSetThisCycle = false;
+  d_playerStatusSetThisCycle = false;
+  d_playerActivitySetThisCycle = false;
+  d_playModeSetThisCycle = false;
   State::make<BehaviourControlState>(d_agent.getThinkCycleNumber(), d_playerRole, d_playerActivity, d_playerStatus);
+}
+
+void BehaviourControl::setPlayerRole(PlayerRole role)
+{
+  if (role == d_playerRole)
+    return;
+  if (d_playerRoleSetThisCycle)
+    log::warning("BehaviourControl::setPlayerRole") << "PlayerRole set multiple times during think cycle, with values " << getPlayerRoleString(d_playerRole) << " and " << getPlayerRoleString(role);
+  d_playerRole = role;
+  d_playerRoleSetThisCycle = true;
+}
+
+void BehaviourControl::setPlayerStatus(PlayerStatus status)
+{
+  if (status == d_playerStatus)
+    return;
+  if (d_playerStatusSetThisCycle)
+    log::warning("BehaviourControl::setPlayerStatus") << "PlayerStatus set multiple times during think cycle, with values " << getPlayerStatusString(d_playerStatus) << " and " << getPlayerStatusString(status);
+  d_playerStatus = status;
+  d_playerStatusSetThisCycle = true;
+}
+
+void BehaviourControl::setPlayerActivity(PlayerActivity activity)
+{
+  if (activity == d_playerActivity)
+    return;
+  if (d_playerActivitySetThisCycle)
+    log::warning("BehaviourControl::setPlayerActivity") << "PlayerActivity set multiple times during think cycle, with values " << getPlayerActivityString(d_playerActivity) << " and " << getPlayerActivityString(activity);
+  d_playerActivity = activity;
+  d_playerActivitySetThisCycle = true;
+}
+
+void BehaviourControl::setPlayMode(robocup::PlayMode playMode)
+{
+  if (playMode == d_playMode)
+    return;
+  if (d_playModeSetThisCycle)
+    log::warning("BehaviourControl::setPlayMode") << "PlayMode set multiple times during think cycle, with values " << getPlayModeString(d_playMode) << " and " << getPlayModeString(playMode);
+  d_playMode = playMode;
+  d_playModeSetThisCycle = true;
 }
--- BehaviourControl/behaviourcontrol.hh
+++ BehaviourControl/behaviourcontrol.hh
@@ -15,24 +15,27 @@ namespace bold
     BehaviourControl(Agent& agent);
 
     PlayerRole getPlayerRole() const { return d_playerRole; }
-    void setPlayerRole(PlayerRole role) { d_playerRole = role; }
-
     PlayerStatus getPlayerStatus() const { return d_playerStatus; }
-    void setPlayerStatus(PlayerStatus status) { d_playerStatus = status; }
-
     PlayerActivity getPlayerActivity() const { return d_playerActivity; }
-    void setPlayerActivity(PlayerActivity activity) { d_playerActivity = activity; }
-
     robocup::PlayMode getPlayMode() const { return d_playMode; }
-    void setPlayMode(robocup::PlayMode playMode) { d_playMode = playMode; }
+
+    void setPlayerRole(PlayerRole role);
+    void setPlayerStatus(PlayerStatus status);
+    void setPlayerActivity(PlayerActivity activity);
+    void setPlayMode(robocup::PlayMode playMode);
 
   private:
-    void updateStateObject() const;
+    void updateStateObject();
 
     Agent& d_agent;
     robocup::PlayMode d_playMode;
     PlayerRole d_playerRole;
     PlayerActivity d_playerActivity;
     PlayerStatus d_playerStatus;
+
+    bool d_playerRoleSetThisCycle;
+    bool d_playerStatusSetThisCycle;
+    bool d_playerActivitySetThisCycle;
+    bool d_playModeSetThisCycle;
   };
 }
