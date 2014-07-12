5374933ab92b702ccfb8f0729cc2fd720ae0b507
Drew Noakes <git@drewnoakes.com>
2014-07-12 13:59:03 +0100

    Assert that player status/activity are not set multiple times in a think cycle.

--- BehaviourControl/behaviourcontrol.cc
+++ BehaviourControl/behaviourcontrol.cc
@@ -42,7 +42,10 @@ void BehaviourControl::setPlayerStatus(PlayerStatus status)
   if (status == d_playerStatus)
     return;
   if (d_playerStatusSetThisCycle)
+  {
     log::warning("BehaviourControl::setPlayerStatus") << "PlayerStatus set multiple times during think cycle, with values " << getPlayerStatusString(d_playerStatus) << " and " << getPlayerStatusString(status);
+    ASSERT(false);
+  }
   d_playerStatus = status;
   d_playerStatusSetThisCycle = true;
 }
@@ -52,7 +55,10 @@ void BehaviourControl::setPlayerActivity(PlayerActivity activity)
   if (activity == d_playerActivity)
     return;
   if (d_playerActivitySetThisCycle)
+  {
     log::warning("BehaviourControl::setPlayerActivity") << "PlayerActivity set multiple times during think cycle, with values " << getPlayerActivityString(d_playerActivity) << " and " << getPlayerActivityString(activity);
+    ASSERT(false);
+  }
   d_playerActivity = activity;
   d_playerActivitySetThisCycle = true;
 }
@@ -62,7 +68,10 @@ void BehaviourControl::setPlayMode(robocup::PlayMode playMode)
   if (playMode == d_playMode)
     return;
   if (d_playModeSetThisCycle)
+  {
     log::warning("BehaviourControl::setPlayMode") << "PlayMode set multiple times during think cycle, with values " << getPlayModeString(d_playMode) << " and " << getPlayModeString(playMode);
+    ASSERT(false);
+  }
   d_playMode = playMode;
   d_playModeSetThisCycle = true;
 }
