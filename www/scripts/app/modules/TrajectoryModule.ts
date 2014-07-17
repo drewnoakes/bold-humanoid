9a5836a525265e244418e63e5763363a83d4ed16
Drew Noakes <git@drewnoakes.com>
2014-07-17 15:42:12 -0300

    Trajectory module allows plotting ctrl+mod/ctrl/mod.

--- www/scripts/app/modules/TrajectoryModule.ts
+++ www/scripts/app/modules/TrajectoryModule.ts
@@ -1,34 +1,51 @@
 /**
- * @author Drew Noakes http://drewnoakes.com
+ * @author Drew Noakes https://drewnoakes.com
  */
 
 /// <reference path="../../libs/lodash.d.ts" />
 /// <reference path="../../libs/d3.d.ts" />
 
 import constants = require('constants');
+import canvas = require('util/canvas');
 import Checkbox = require('controls/Checkbox');
 import data = require('data');
+import dom = require("../../libs/domdomdom/domdomdom");
 import BodyFigure = require('controls/BodyFigure');
 import Module = require('Module');
+import Select = require('controls/Select');
 import state = require('state');
-import canvas = require('util/canvas');
 import Trackable = require('util/Trackable');
 
 var chartHeight = 300,
     chartWidth = 430,
     maxDataLength = chartWidth;
 
-var yScale = d3.scale.linear()
+var _yScale = d3.scale.linear()
     .range([0, chartHeight])
     .domain([4096, 0]);
 
-var yScaleMirror = d3.scale.linear()
+var _yScaleMirror = d3.scale.linear()
     .range([0, chartHeight])
     .domain([0, 4096]);
+
+var _yModScale = d3.scale.linear()
+    .range([0, chartHeight])
+    .domain([100, -100]);
+
+var _yModScaleMirror = d3.scale.linear()
+    .range([0, chartHeight])
+    .domain([-100, 100]);
 
 interface ICloseable
 {
     close();
+}
+
+enum Mode
+{
+    All,
+    ControlOnly,
+    ModulationOnly
 }
 
 class TrajectoryModule extends Module
@@ -36,13 +53,17 @@ class TrajectoryModule extends Module
     private data: state.BodyControl[] = [];
 
     private mirrorValues: Trackable<boolean> = new Trackable<boolean>(true);
+    private mode: Trackable<Mode> = new Trackable<Mode>(Mode.All);
     private isRecording: boolean = false;
     private skipFirstDatum: boolean = true;
 
     private subscription: ICloseable;
     private bodyFigure: BodyFigure;
-    private recordButton: HTMLButtonElement;
+    private recordButton: HTMLElement;
     private canvas: HTMLCanvasElement;
+
+    private yScaleNormal: D3.Scale.LinearScale;
+    private yScaleMirror: D3.Scale.LinearScale;
 
     constructor()
     {
@@ -53,37 +74,55 @@ class TrajectoryModule extends Module
 
     public load(width: number)
     {
-        this.recordButton = document.createElement('button');
-        this.recordButton.className = 'record';
-        this.recordButton.textContent = 'record';
+        this.recordButton = dom('button.record', 'record');
         this.recordButton.addEventListener('click', this.toggleIsRecording.bind(this));
 
-        var mirrorCheckbox = new Checkbox('Mirror values', this.mirrorValues);
+        var modeItems = [
+            { value:Mode.All,            text:"Control + Mod" },
+            { value:Mode.ControlOnly,    text:"Control only" },
+            { value:Mode.ModulationOnly, text:"Modulation only" }
+        ];
 
-        var controlContainer = document.createElement('div');
-        controlContainer.className = 'controls';
-        controlContainer.appendChild(this.recordButton);
-        controlContainer.appendChild(mirrorCheckbox.element);
-        this.element.appendChild(controlContainer);
+        var controlContainer =
+            dom('div.controls',
+                this.recordButton,
+                new Select(this.mode, modeItems).element,
+                new Checkbox('Mirror values', this.mirrorValues).element);
 
-        var hoverText = document.createElement('div');
-        hoverText.className = 'hover-value';
-        this.element.appendChild(hoverText);
+        var hoverText = dom('div.hover-value');
 
         this.canvas = document.createElement('canvas');
         this.canvas.width = chartWidth;
         this.canvas.height = chartHeight;
-        this.element.appendChild(this.canvas);
 
-        this.canvas.addEventListener('mousemove', e => hoverText.textContent = Math.round(yScale.invert(e.offsetY)).toString());
+        this.canvas.addEventListener('mousemove', e => hoverText.textContent = Math.round(this.yScaleNormal.invert(e.offsetY)).toString());
         this.canvas.addEventListener('mouseout', e => hoverText.textContent = '');
 
         this.bodyFigure = new BodyFigure({hasHover: true, hasSelection: true});
-        this.element.appendChild(this.bodyFigure.element);
+
+        dom(this.element,
+            controlContainer,
+            hoverText,
+            this.canvas,
+            this.bodyFigure.element);
 
         this.bodyFigure.hoverJointId.track(this.render.bind(this));
         this.bodyFigure.selectedJointIds.track(this.render.bind(this));
         this.bodyFigure.visitJoints((jointId, jointDiv) => jointDiv.textContent = jointId.toString());
+
+        this.closeables.add(this.mode.track(mode => {
+            if (mode === Mode.ModulationOnly)
+            {
+                this.yScaleNormal = _yModScale;
+                this.yScaleMirror = _yModScaleMirror;
+            }
+            else
+            {
+                this.yScaleNormal = _yScale;
+                this.yScaleMirror = _yScaleMirror;
+            }
+            this.render();
+        }));
     }
 
     public unload()
@@ -168,15 +207,21 @@ class TrajectoryModule extends Module
         });
         ctx.stroke();
 
+        var mode = this.mode.getValue(),
+            mirror = this.mirrorValues.getValue();
+
         var drawLine = jointId =>
         {
             jointId--;
             ctx.beginPath();
             _.each(this.data, d =>
             {
-                var y = this.mirrorValues.getValue() && jointId % 2 === 0 && jointId !== 20 ? yScaleMirror : yScale;
+                var y = mirror && jointId % 2 === 0 && jointId !== 20 ? this.yScaleMirror : this.yScaleNormal;
+                var v = 0;
+                if (mode === Mode.All || mode === Mode.ControlOnly) v += d.joints[jointId].v;
+                if (mode === Mode.All || mode === Mode.ModulationOnly) v += d.joints[jointId].m;
                 var px = x(d.cycle),
-                    py = y(d.joints[jointId].v);
+                    py = y(v);
                 ctx.lineTo(px, py);
             });
             ctx.stroke();
