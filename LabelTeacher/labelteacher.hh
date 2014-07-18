08655b81f614521b79fef5fe3545cab8c2b11a14
Sander van Dijk <sgvandijk@gmail.com>
2014-07-18 21:10:14 +0100

    HistogramLabelTeacher to more generic LabelTeacehr

--- LabelTeacher/labelteacher.hh
+++ LabelTeacher/labelteacher.hh
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "../PixelFilterChain/pixelfilterchain.hh"
-#include "../PixelLabel/HistogramPixelLabel/histogrampixellabel.hh"
+#include "../PixelLabel/pixellabel.hh"
 #include "../Setting/setting.hh"
 
 #include <vector>
@@ -10,11 +10,17 @@
 
 namespace bold
 {
-  class HistogramLabelTeacherBase
+  template<uint8_t CHANNEL_BITS>
+  class LabelTeacher
   {
   public:
-    HistogramLabelTeacherBase(std::vector<std::string> const& names);
-    virtual ~HistogramLabelTeacherBase() {}
+    LabelTeacher(std::vector<std::shared_ptr<PixelLabel>> labels);
+    
+    std::vector<std::shared_ptr<PixelLabel>> getLabels() const;
+
+    void train(unsigned labelIdx, cv::Mat const& mask);
+
+    cv::Mat label(unsigned labelIdx) const;
 
     bool requestedSnapShot() const { return d_snapshotRequested; }
     bool requestedLabel() const {return d_labelRequested; }
@@ -42,10 +48,8 @@ namespace bold
 
     unsigned getLabelToTrain() const { return d_labelToTrain; }
 
-    virtual void train(unsigned labelIdx, cv::Mat const& mask) = 0;
-
-    virtual cv::Mat label(unsigned labelIdx) const = 0;
-  protected:
+  private:
+    std::vector<std::shared_ptr<PixelLabel>> d_labels;
     cv::Mat d_yuvTrainImage;
     cv::Mat d_mask;
 
@@ -53,57 +57,46 @@ namespace bold
     float d_maxFloodDiff;
     unsigned d_labelToTrain;
 
-  private:
     bool d_snapshotRequested;
     bool d_labelRequested;
     bool d_fixedRange;
-  };
 
-  template<uint8_t CHANNEL_BITS>
-  class HistogramLabelTeacher : public HistogramLabelTeacherBase
-  {
-  public:
-    HistogramLabelTeacher(std::vector<std::string> const& names);
-    
-    std::vector<HistogramPixelLabel<CHANNEL_BITS>> getLabels() const;
-
-    void train(unsigned labelIdx, cv::Mat const& mask) override;
-
-    cv::Mat label(unsigned labelIdx) const override;
-
-  private:
-    std::vector<HistogramPixelLabel<CHANNEL_BITS>> d_labels;
   };
   
   //////
 
   template<uint8_t CHANNEL_BITS>
-  HistogramLabelTeacher<CHANNEL_BITS>::HistogramLabelTeacher(std::vector<std::string> const& names)
-    : HistogramLabelTeacherBase{names},
-    d_labels{}
+  LabelTeacher<CHANNEL_BITS>::LabelTeacher(std::vector<std::shared_ptr<PixelLabel>> labels)
+    : d_labels{labels},
+    d_yuvTrainImage{},
+    d_seedPoint{0,0},
+    d_snapshotRequested{false},
+    d_labelRequested{false},
+    d_fixedRange{false}
   {
-    for (auto const& name : names)
-      d_labels.emplace_back(name);
   }
 
   template<uint8_t CHANNEL_BITS>
-  std::vector<HistogramPixelLabel<CHANNEL_BITS>> HistogramLabelTeacher<CHANNEL_BITS>::getLabels() const
+  std::vector<std::shared_ptr<PixelLabel>> LabelTeacher<CHANNEL_BITS>::getLabels() const
   {
     return d_labels;
   }
 
-  inline void HistogramLabelTeacherBase::setYUVTrainImage(cv::Mat yuvTrainImage)
+  template<uint8_t CHANNEL_BITS>
+  void LabelTeacher<CHANNEL_BITS>::setYUVTrainImage(cv::Mat yuvTrainImage)
   {
     d_yuvTrainImage = yuvTrainImage;
     d_snapshotRequested = false;
   }
 
-  inline void HistogramLabelTeacherBase::setSeedPoint(Eigen::Vector2i point)
+  template<uint8_t CHANNEL_BITS>
+  void LabelTeacher<CHANNEL_BITS>::setSeedPoint(Eigen::Vector2i point)
   {
     d_seedPoint = point;
   }
 
-  inline cv::Mat HistogramLabelTeacherBase::floodFill() const
+  template<uint8_t CHANNEL_BITS>
+  inline cv::Mat LabelTeacher<CHANNEL_BITS>::floodFill() const
   {
     cv::Mat mask = cv::Mat::zeros(d_yuvTrainImage.rows + 2, d_yuvTrainImage.cols + 2, CV_8UC1);
 
@@ -118,7 +111,7 @@ namespace bold
   }
   
   template<uint8_t CHANNEL_BITS>
-  void HistogramLabelTeacher<CHANNEL_BITS>::train(unsigned labelIdx, cv::Mat const& mask)
+  void LabelTeacher<CHANNEL_BITS>::train(unsigned labelIdx, cv::Mat const& mask)
   {
     ASSERT(mask.type() == CV_8UC1);
     ASSERT(d_yuvTrainImage.cols == mask.cols && d_yuvTrainImage.rows == mask.rows);
@@ -135,13 +128,13 @@ namespace bold
           auto bgr = yuv.toBgrInt();
           auto hsv = bgr2hsv(bgr);
 
-          d_labels[labelIdx].addSample(hsv);
+          d_labels[labelIdx]->addSample(hsv);
         }
     }
   }
 
   template<uint8_t CHANNEL_BITS>
-  cv::Mat HistogramLabelTeacher<CHANNEL_BITS>::label(unsigned labelIdx) const
+  cv::Mat LabelTeacher<CHANNEL_BITS>::label(unsigned labelIdx) const
   {
     auto labelImg = cv::Mat{d_yuvTrainImage.rows, d_yuvTrainImage.cols, CV_8UC1};
     for (unsigned i = 0; i < labelImg.rows; ++i)
@@ -154,7 +147,7 @@ namespace bold
         auto yuv = Colour::YCbCr{trainImageRow[j * 3 + 0], trainImageRow[j * 3 + 1], trainImageRow[j * 3 + 2]};
         auto bgr = yuv.toBgrInt();
         auto hsv = bgr2hsv(bgr);
-        labelRow[j] = d_labels[labelIdx].labelProb(hsv) * 255;
+        labelRow[j] = d_labels[labelIdx]->labelProb(hsv) * 255;
       }
     }
 
