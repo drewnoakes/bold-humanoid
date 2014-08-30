9015f84759c5efacf26ee6e9d9ec5676e5bff26d
Drew Noakes <git@drewnoakes.com>
2014-08-30 15:13:44 +0100

    Add unit test for math.lerp

--- www/scripts/test/math-test.ts
+++ www/scripts/test/math-test.ts
@@ -18,3 +18,19 @@ describe("clamp", () =>
         expect(math.clamp(3, 0, 2)).toEqual(2);
     });
 });
+
+describe("lerp", () =>
+{
+    it("interpolates correctly", () =>
+    {
+        expect(math.lerp(0.0, 5, 10)).toEqual(5);
+        expect(math.lerp(1.0, 5, 10)).toEqual(10);
+        expect(math.lerp(0.5, 5, 10)).toEqual(7.5);
+    });
+
+    it("extrapolates correctly", () =>
+    {
+        expect(math.lerp(-1.0, 5, 10)).toEqual(0);
+        expect(math.lerp(2.0, 5, 10)).toEqual(15);
+    });
+});
