c51b88886807dcadb99fe28e1d840d286f5d1df0
Drew Noakes <git@drewnoakes.com>
2014-09-27 22:55:41 +0100

    Gulp task for transpiling SASS, autoprefixing and bundling/uglifying styles.

--- www/gulpfile.js
+++ www/gulpfile.js
@@ -2,15 +2,30 @@
 
 var gulp = require('gulp');
 
+var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
+var minifycss = require('gulp-minify-css');
 var fs = require('fs');
 var header = require('gulp-header');
 var typescript = require('gulp-tsc');
 var amdOptimize = require('amd-optimize');
 
 var outFolder = 'dist';
+
+gulp.task('styles', function ()
+{
+    return gulp.src('styles/*.scss')
+        .pipe(sass())
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+        .pipe(gulp.dest('styles'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(minifycss())
+        .pipe(header(fs.readFileSync('LICENSE')))
+        .pipe(gulp.dest(outFolder));
+});
 
 gulp.task('tsc', function ()
 {
