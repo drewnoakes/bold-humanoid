6ecea889c4f4cbfdc8051f105e2beccaa38a3dad
Drew Noakes <git@drewnoakes.com>
2014-09-28 21:55:20 +0100

    Add some API documentation.

--- OptionTree/optiontree.hh
+++ OptionTree/optiontree.hh
@@ -9,15 +9,29 @@ namespace bold
 {
   class FSMOption;
 
+  /**
+   * The option tree models the complete behaviour of the agent.
+   * It is executed each think cycle after sensor data has been processed.
+   * Consequences of its running include motion, network messages and internal state updates.
+   */
   class OptionTree
   {
   public:
     OptionTree(std::shared_ptr<Option> root);
 
+    /**
+     * Executes the option tree, from the root down.
+     */
     void run();
 
+    /**
+     * Registers an FSM option with the tree such that it may be debugged in Round Table.
+     */
     void registerFsm(std::shared_ptr<FSMOption> fsm);
 
+    /**
+     * Gets all previously registered FSM options.
+     */
     std::vector<std::shared_ptr<FSMOption>> getFSMs() const;
 
   private:
