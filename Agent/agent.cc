f427d02591b54846505a1256dff49e0fde7121ca
Drew Noakes <git@drewnoakes.com>
2014-11-29 02:15:54 +0100

    Aggregate small functions.

--- Agent/agent.cc
+++ Agent/agent.cc
@@ -198,3 +198,42 @@ ulong Agent::getThinkCycleNumber() const
 {
   return getCycleNumber();
 }
+
+void Agent::setOptionTree(shared_ptr<OptionTree> tree)
+{
+  log::verbose("Agent::setOptionTree") << "Setting OptionTree";
+  d_optionTree = tree;
+  d_streamer->setOptionTree(tree);
+}
+
+void Agent::requestStop()
+{
+  if (!isRunning())
+    throw runtime_error("Not running");
+
+  if (d_isShutdownRequested)
+  {
+    log::warning("Agent::requestStop") << "Forcing exit";
+    exit(EXIT_FAILURE);
+  }
+
+  log::info("Agent::requestStop");
+
+  // The option tree picks up this flag and performs a shutdown sequence
+  d_isShutdownRequested = true;
+
+  d_voice->say("Shutting down");
+}
+
+void Agent::onStopped()
+{
+  d_streamer->stop();
+
+  if (d_motionLoop)
+    d_motionLoop->stop();
+
+  if (d_voice)
+    d_voice->stop();
+
+  log::info("Agent::stop") << "Stopped";
+}
