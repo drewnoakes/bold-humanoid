e62281e29e3b94c28c42cb7a6a28067cc02ee550
Drew Noakes <git@drewnoakes.com>
2014-12-16 23:16:38 +0000

    First cut of BufferReader.

---
+++ test/BufferReaderTests.cc
@@ -0,0 +1,38 @@
+#include <gtest/gtest.h>
+
+#include "../util/bufferreader.hh"
+
+using namespace bold;
+using namespace std;
+
+TEST (BufferReaderTests, readInt8)
+{
+  vector<uchar> bytes = {1,2,3,4,5,6};
+  BufferReader reader(bytes.data());
+
+  EXPECT_EQ(1, reader.readInt8());
+  EXPECT_EQ(2, reader.readInt8());
+  EXPECT_EQ(3, reader.readInt8());
+  EXPECT_EQ(4, reader.readInt8());
+  EXPECT_EQ(5, reader.readInt8());
+  EXPECT_EQ(6, reader.readInt8());
+}
+
+TEST (BufferReaderTests, readInt16)
+{
+  vector<uchar> bytes = {1,2,3,4,5,6};
+  BufferReader reader(bytes.data());
+
+  EXPECT_EQ(0x0201, reader.readInt16());
+  EXPECT_EQ(0x0403, reader.readInt16());
+  EXPECT_EQ(0x0605, reader.readInt16());
+}
+
+TEST (BufferReaderTests, readInt32)
+{
+  vector<uchar> bytes = {1,2,3,4,5,6,7,8};
+  BufferReader reader(bytes.data());
+
+  EXPECT_EQ(0x04030201, reader.readInt32());
+  EXPECT_EQ(0x08070605, reader.readInt32());
+}
---
+++ util/bufferreader.hh
@@ -0,0 +1,65 @@
+#pragma once
+
+// <cstdint> provides types of guaranteed widths
+#include <cstdint>
+
+// TODO endianness as a type param? eg: BufferReader<LittleEndian>
+
+// TODO read N bytes
+// TODO read string of N bytes (encoding?)
+// TODO test length and remaining
+
+
+namespace bold
+{
+  typedef unsigned char uchar;
+
+  class BufferReader
+  {
+  public:
+    BufferReader(const uchar* ptr)
+      : d_ptr(ptr)
+    {}
+
+    int8_t readInt8()
+    {
+      return static_cast<int8_t>(*d_ptr++);
+    }
+
+    uint8_t readInt8u()
+    {
+      return static_cast<uint8_t>(*d_ptr++);
+    }
+
+    int16_t readInt16()
+    {
+      auto val = *reinterpret_cast<const int16_t*>(d_ptr);
+      d_ptr += sizeof(int16_t);
+      return val;
+    }
+
+    uint16_t readInt16u()
+    {
+      auto val = *reinterpret_cast<const uint16_t*>(d_ptr);
+      d_ptr += sizeof(uint16_t);
+      return val;
+    }
+
+    int32_t readInt32()
+    {
+      auto val = *reinterpret_cast<const int32_t*>(d_ptr);
+      d_ptr += sizeof(int32_t);
+      return val;
+    }
+
+    uint32_t readInt32u()
+    {
+      auto val = *reinterpret_cast<const uint32_t*>(d_ptr);
+      d_ptr += sizeof(uint32_t);
+      return val;
+    }
+
+  private:
+    const uchar* d_ptr;
+  };
+}
